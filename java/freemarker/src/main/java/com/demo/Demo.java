305f2cc5e3ca37452ec152d1c76daf69d8cba651
Jane <lvlajzhao1983@gamil.com>
2022-04-01 15:31:52 +0800

    add exmaple of how to use freemark

--- java/freemarker/src/main/java/com/demo/Demo.java
+++ java/freemarker/src/main/java/com/demo/Demo.java
@@ -1,2 +1,63 @@
-package com.demo;public class Demo {
+package com.demo;
+
+import freemarker.cache.FileTemplateLoader;
+import freemarker.core.HTMLOutputFormat;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class Demo {
+    static Configuration create(final String basePath) throws IOException {
+        Configuration cfg = new Configuration();
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setOutputFormat(HTMLOutputFormat.INSTANCE);
+        cfg.setAutoEscapingPolicy(Configuration.ENABLE_IF_SUPPORTED_AUTO_ESCAPING_POLICY);
+        cfg.setTemplateLoader(new FileTemplateLoader(new File(basePath)));
+        return cfg;
+    }
+
+    static Template getTemplate(Configuration configuration,  Locale locale, String name) {
+        try {
+            return configuration.getTemplate(name, locale);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    static void write() throws IOException, TemplateException {
+        StringWriter writer = new StringWriter();
+
+        Locale locale = new Locale("en","US");
+        String templateName = "verification.html";
+
+        String basePath = "D:/alasta/experiment-for-new-platform/email-template";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("verificationCode", "123456");
+
+        Configuration configuration = create(basePath);
+
+        Template template = getTemplate(configuration, locale, templateName);
+        template.process(map, writer);
+
+        System.out.println(writer.toString());
+    }
+
+    public static void main(String[] args) {
+        try {
+            write();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+    }
 }

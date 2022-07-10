5565485ec66b64157dba9b678f87a7aec507809d
Jane <lvlajzhao1983@gamil.com>
2022-07-10 16:04:24 +0800

    first multiple thread deml

--- java/muti-thread/first/src/main/java/com/demo/ThreadGroupDemo.java
+++ java/muti-thread/first/src/main/java/com/demo/ThreadGroupDemo.java
@@ -1,21 +1,41 @@
 package com.demo;
 
-public class MultithreadingDemo {
+public class ThreadGroupDemo {
     public static void main(String[] args) {
         ThreadGroup threadGroup = new ThreadGroup("group");
         Thread thread1 = new Thread(threadGroup, () -> {
             String groupName = Thread.currentThread().getThreadGroup().getName();
             String threadName = Thread.currentThread().getName();
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println(groupName + "-" + threadName);
         }, "thread1");
 
         Thread thread2 = new Thread(threadGroup, () -> {
             String groupName = Thread.currentThread().getThreadGroup().getName();
             String threadName = Thread.currentThread().getName();
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println(groupName + "-" + threadName);
         }, "thread2");
 
+        Thread daemon = new Thread(()-> {
+            System.out.println("I am daemon");
+
+        }, "threadDaemon");
+        daemon.setDaemon(true);
+
+
         thread1.start();
         thread2.start();
+        daemon.start();
     }
 }

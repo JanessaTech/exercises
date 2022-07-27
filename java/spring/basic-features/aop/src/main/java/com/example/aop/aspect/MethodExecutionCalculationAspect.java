4212f565c59c8ed20a870534a60fe907a55fe01b
Jane <lvlajzhao1983@gamil.com>
2022-07-27 09:51:26 +0800

    change @Configuration to @Component for /MethodExecutionCalculationAspect

--- java/spring/basic-features/aop/src/main/java/com/example/aop/aspect/MethodExecutionCalculationAspect.java
+++ java/spring/basic-features/aop/src/main/java/com/example/aop/aspect/MethodExecutionCalculationAspect.java
@@ -6,9 +6,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Aspect
-@Configuration
+@Component
 public class MethodExecutionCalculationAspect {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 

5613b7af4f0da12282d534d7947128259d718b9e
Jane <lvlajzhao1983@gamil.com>
2022-11-21 22:21:23 +0800

    add comments

--- db/mysql/jpa/hibernate/src/main/java/com/example/hibernate/controller/GameController.java
+++ db/mysql/jpa/hibernate/src/main/java/com/example/hibernate/controller/GameController.java
@@ -2,9 +2,8 @@ package com.example.hibernate.controller;
 
 import com.example.hibernate.dao.Game;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.StatelessSession;
 import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,15 +20,26 @@ public class GameController {
 
     @GetMapping("/insert")
     String testInsert() {
-        Game game = new Game();
-        game.setName("first game");
 
+        long start = System.currentTimeMillis();
+        String name = "game name";
         Session session = entityManager.unwrap(Session.class);
+        //StatelessSession session = ((Session) entityManager.getDelegate()).getSessionFactory().openStatelessSession();
         Transaction t=session.beginTransaction();
-        session.save(game);
-        session.flush();
-        t.commit();//transaction is commited
+        for (int i = 0; i < 100000; i++) {
+            Game game = new Game();
+            game.setName(name + i);
+            // session.insert(game);  //StatelessSession
+            if ( i % 100 == 0) {
+                session.flush();
+                session.clear();
+            }
+
+        }
+        t.commit();//transaction is committed
         session.close();
+        long end = System.currentTimeMillis();
+        System.out.println("It takes " + (end - start)/1000 + " seconds to insert " + 10000 + "games");
         return "insert games successfully";
     }
 }

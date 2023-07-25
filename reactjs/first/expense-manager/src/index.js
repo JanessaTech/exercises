15e8cc265b019620ef295e5354682435cece7aa3
Jane <lvlajzhao1983@gamil.com>
2023-07-25 14:09:17 +0800

    update

--- reactjs/first/expense-manager/src/index.js
+++ reactjs/first/expense-manager/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HelloWorld from './components/HelloWorld';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <HelloWorld />
     </React.StrictMode>,
     document.getElementById('root')
 );

6326514c14acb8ecea8e082d47e2d87e9c7f37ee
Janessa <lvlajzhao1983@gmail.com>
2023-08-15 14:17:05 +0800

    add banner

--- js/algrithm/tmp/Test.js
+++ js/algrithm/tmp/Test.js
@@ -1,16 +1,7 @@
-const readline = require("readline");
-
-function readmeFromConsole() {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    rl.question("What is your name? ", function (answer) {
-        console.log(`Oh, so your name is ${answer}`);
-        console.log("Closing the interface");
-        rl.close();
-    });
+const person = {
+    name : 'Jane',
+    age : 10
 }
 
-readmeFromConsole()
+const {name} = person
+console.log(`person name :${name}`)

30106db0d7f2bc4c596072df3fd95b94995c5597
Janessa <lvlajzhao1983@gmail.com>
2023-08-15 16:55:52 +0800

    refactor codes

--- nodejs/express-framework/helpers/errors/ErrorClasses.js
+++ nodejs/express-framework/helpers/errors/ErrorClasses.js
@@ -1,26 +1,25 @@
-class UserError extends Error {
-    constructor(type) {
-        super(type.message)
-        this.code = type.code
+
+const errorClasses = {
+    UserError : class UserError extends Error {
+        constructor(type) {
+            super(type.message)
+            this.code = type.code
+        }
+    },
+    InvalidRequest: class InvalidRequest extends Error {
+        constructor(type, errors) {
+            super(type.message)
+            this.code = type.code
+            this.errors = errors
+        }
+    },
+    DefaultError: class DefaultError extends Error {
+        constructor(type) {
+            super(type.message)
+            this.code = type.code
+        }
     }
-}
-class InvalidRequest extends Error {
-    constructor(type, errors) {
-        super(type.message)
-        this.code = type.code
-        this.errors = errors
-    }
+
 }
 
-class DefaultError extends Error {
-    constructor(type) {
-        super(type.message)
-        this.code = type.code
-    }
-}
-
-module.exports = {
-    UserError : UserError,
-    DefaultError : DefaultError,
-    InvalidRequest : InvalidRequest
-}
+module.exports = errorClasses

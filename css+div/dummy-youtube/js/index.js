22d66e85a9d09e88c4ab621c2603b1b4333b7b6b
Janessa <lvlajzhao1983@gmail.com>
2023-09-07 18:51:48 +0800

    update menu

--- css+div/dummy-youtube/js/index.js
+++ css+div/dummy-youtube/js/index.js
@@ -1,13 +1,40 @@
 
 const short_hint = document.querySelector('.hint')
 const short_close = document.querySelector('.close')
+const search_input = document.querySelector('#search input')
+const historywords = document.querySelector('#historywords')
+const tohides = document.querySelectorAll('.toHide')
+const more = document.querySelector('#more')
+const less = document.querySelector('#less')
+
 short_close.addEventListener('mouseenter', () => {
     short_hint.classList.toggle('active')
     short_close.classList.toggle('active')
-    
-
 })
 short_close.addEventListener('mouseleave', () => {
     short_hint.classList.toggle('active')
     short_close.classList.toggle('active')
 })
+search_input.addEventListener('focus', () => {
+    historywords.classList.toggle('active')
+});
+search_input.addEventListener('focusout', () => {
+    historywords.classList.toggle('active')
+});
+more.addEventListener('click', () => {
+    console.log("more is clicked")
+    tohides.forEach(e => {
+        e.classList.remove('hide')
+    })
+    less.classList.remove('hide')
+    more.classList.add('hide')
+})
+less.addEventListener('click', () => {
+    console.log("less is clicked")
+    tohides.forEach(e => {
+        e.classList.add('hide')
+    })
+    less.classList.add('hide')
+    more.classList.remove('hide')
+})
+

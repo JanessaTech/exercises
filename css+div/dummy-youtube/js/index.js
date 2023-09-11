f4850ed767b6bf689115fda5bd0acc760c699fab
Janessa <lvlajzhao1983@gmail.com>
2023-09-11 15:46:22 +0800

    add min-menu on the left side bar

--- css+div/dummy-youtube/js/index.js
+++ css+div/dummy-youtube/js/index.js
@@ -6,6 +6,10 @@ const historywords = document.querySelector('#historywords')
 const tohides = document.querySelectorAll('.toHide')
 const more = document.querySelector('#more')
 const less = document.querySelector('#less')
+const menu = document.querySelector('#menu')
+const min_menu = document.querySelector('#min-menu')
+const burger = document.querySelector('#burger')
+var r = document.querySelector(':root');
 
 short_close.addEventListener('mouseenter', () => {
     short_hint.classList.toggle('active')
@@ -36,5 +40,19 @@ less.addEventListener('click', () => {
     })
     less.classList.add('hide')
     more.classList.remove('hide')
+})
+burger.addEventListener('click', () => {
+    console.log('burger is clicked');
+    var rs = getComputedStyle(r);
+    var ssv = rs.getPropertyValue('--sideSize')
+    if (ssv === '300px') {
+        r.style.setProperty('--sideSize', '100px');
+        menu.classList.toggle('active')
+        min_menu.classList.toggle('active')        
+    } else {
+        r.style.setProperty('--sideSize', '300px');
+        menu.classList.toggle('active')
+        min_menu.classList.toggle('active')
+    }
 })
 

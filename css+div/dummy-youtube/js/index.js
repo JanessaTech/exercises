3f484e3783d564df80a9ddf756264fd5752bb70a
Janessa <lvlajzhao1983@gmail.com>
2023-09-12 16:07:24 +0800

    1. fix bug when toggle memu in firefox 2. minor changes in vedio

--- css+div/dummy-youtube/js/index.js
+++ css+div/dummy-youtube/js/index.js
@@ -10,6 +10,8 @@ const menu = document.querySelector('#menu')
 const min_menu = document.querySelector('#min-menu')
 const burger = document.querySelector('#burger')
 var r = document.querySelector(':root');
+const actions = document.querySelectorAll('.actions')
+const popup = document.querySelector('#popup')
 
 short_close.addEventListener('mouseenter', () => {
     short_hint.classList.toggle('active')
@@ -20,10 +22,11 @@ short_close.addEventListener('mouseleave', () => {
     short_close.classList.toggle('active')
 })
 search_input.addEventListener('focus', () => {
-    historywords.classList.toggle('active')
+    //historywords.classList.toggle('active')
+    historywords.style.display = "block"
 });
 search_input.addEventListener('focusout', () => {
-    historywords.classList.toggle('active')
+    historywords.style.display = "none"
 });
 more.addEventListener('click', () => {
     console.log("more is clicked")
@@ -45,14 +48,31 @@ burger.addEventListener('click', () => {
     console.log('burger is clicked');
     var rs = getComputedStyle(r);
     var ssv = rs.getPropertyValue('--sideSize')
-    if (ssv === '300px') {
+    if (ssv.trim() === '300px') {
+        console.log("=== 300px")
         r.style.setProperty('--sideSize', '100px');
         menu.classList.toggle('active')
         min_menu.classList.toggle('active')        
     } else {
+        console.log("!== 300px")
         r.style.setProperty('--sideSize', '300px');
         menu.classList.toggle('active')
         min_menu.classList.toggle('active')
+    }
+})
+
+actions.forEach(e => e.addEventListener('click', (e)=> {
+    var p = e.target.parentElement
+    var rect = p.getBoundingClientRect();
+    popup.style.top = rect.top + 50;
+    popup.style.left = rect.left - 250;
+    popup.style.display = "block";
+}))
+
+document.addEventListener('click', e =>{
+    var className = e.target.parentElement.className
+    if (className !== 'actions') {
+        popup.style.display = "none";
     }
 })
 

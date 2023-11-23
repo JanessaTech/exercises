4b2950b9225286574d7bc6800f16ef3cfb32757c
Janessa <lvlajzhao1983@gmail.com>
2023-11-23 14:06:50 +0800

    update for hardhat

--- hardred/hardhat.config.js
+++ hardred/hardhat.config.js
@@ -4,3 +4,16 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.19",
 };
+
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const balance = await ethers.provider.getBalance(taskArgs.account);
+
+    console.log(ethers.formatEther(balance), "ETH");
+  });
+  
+task("hello", "Prints 'Hello, World!'", async () => {
+  console.log("Hello, World!");
+});

f5efcd4984232d686add2acc3f6801892b45271d
Janessa <lvlajzhao1983@gmail.com>
2024-06-13 16:59:42 +0800

    updates

--- nextjs/next-second/app/tickets/page.jsx
+++ nextjs/next-second/app/tickets/page.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Suspense } from 'react'
+import Loading from '../loading'
 import TicketList from './TicketList'
 
 export default function Tickets() {
@@ -10,7 +12,11 @@ export default function Tickets() {
             <p><small>Currently open tickets</small></p>
           </div>
         </nav>
-        <TicketList/>
+        
+        <Suspense fallback={<Loading/>}>
+          <TicketList/>
+        </Suspense>
+        
     </main>
   )
 }

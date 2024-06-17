4a86abbe4116edadd8067cd36e1310b8f0e8dbf2
Janessa <lvlajzhao1983@gmail.com>
2024-06-17 22:01:45 +0800

    show how to use layout and routing group

---
+++ nextjs/next.js_course/hello-world/src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function ForgotPassword() {
+  return (
+    <h1>ForgotPassword</h1>
+  )
+}
+
---
+++ nextjs/next.js_course/hello-world/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LineChart from './linechart'
+
+export default function Dashboard() {
+  return (
+    <div>
+      Dashboard
+      <LineChart/>
+    </div>
+  )
+}
+

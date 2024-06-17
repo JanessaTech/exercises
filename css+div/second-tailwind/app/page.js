d1afb10eb4465f080e0b7e7adec08b4f1535ae91
Janessa <lvlajzhao1983@gmail.com>
2024-06-17 11:11:56 +0800

    practice with tailwind

--- css+div/second-tailwind/app/page.js
+++ css+div/second-tailwind/app/page.js
@@ -1,64 +1,121 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import stew from '../public/img/stew.jpg'
+import noodles from '../public/img/noodles.jpg'
+import curry from '../public/img/curry.jpg'
+import Script from 'next/script'
 
 export default function Home() {
   return (
-    <div className="text-gray-600"> 
-        <div>
-            <nav>
-                <div>
-                    <h1 className="font-bold uppercase p-4 border-b border-gray-100">
-                        <Link href="/">Janessa Food</Link>
-                    </h1>
+
+    <div className="text-gray-600">
+            <Script
+            src={'/index.js'}
+                strategy="beforeInteractive" // lazyOnload, afterInteractive
+            />
+            <div className="grid md:grid-cols-3"> 
+                <div className="md:col-span-1 md:flex md:justify-end">
+                    <nav className="text-right">
+                        <div className="flex justify-between items-center">
+                            <h1 className="font-bold uppercase p-4 border-b border-gray-200">
+                                <Link href="/" className="hover:text-gray-700">Janessa Food</Link>
+                            </h1>
+                            <div className="px-4 cursor-pointer md:hidden" id='burger'>
+                                <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                </svg>
+                            </div>
+                        </div>
+                        
+                        <ul className="text-sm mt-6 hidden md:block" id='menu'>
+                            <li className="text-gray-700 font-bold py-1">
+                                <Link href="#" className="px-4 flex justify-end border-r-4 hover:border-primary border-white">
+                                    <span>Home</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 ml-2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                    </svg>
+                                </Link>
+                            </li>
+                            <li className="py-1">
+                                <a href="#" className="px-4 flex justify-end border-r-4 border-white hover:border-primary">
+                                    <span>About</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 ml-2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                                    </svg>
+                                </a>
+                            </li>
+                            <li className="py-1">
+                                <Link href="#" className="px-4 flex justify-end border-r-4 border-white hover:border-primary">
+                                <span>Contact</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 ml-2">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                                </svg>
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-                <ul>
-                    <li className="text-gray-700 font-bold">
-                        <Link href="#">
-                          <span>Home</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span>About</span>
-                        </a>
-                    </li>
-                    <li>
-                        <Link href="#">
-                          <span>Contact</span>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <main className="px-16 py-6">
-            <div>
-                <Link href="#" className="text-primary">Log in</Link>
-                <Link href="#" className="text-primary">Sign up</Link>
+                <main className="px-16 py-6 bg-gray-100 md:col-span-2">
+                    <div className="flex md:justify-end justify-center">
+                        <Link href="#" className="btn text-primary border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out  duration-500">Log in</Link>
+                        <Link href="#" className="btn text-primary ml-2 border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out  duration-500">Sign up</Link>
+                    </div>
+                    <header>
+                        <h2 className="text-gray-700 text-6xl font-semibold">Recipes</h2>
+                        <h3 className="text-2xl font-semibold">For Janessa</h3>
+                    </header>
+                    <div>
+                        <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">Latest Recipes</h4>
+                        <div className="mt-8 grid lg:grid-cols-3 gap-10">
+                        <div className="card hover:shadow-lg">
+                            <Image src={noodles} alt="noodles" className="w-full h-32 sm:h-48 object-cover"></Image>
+                            <div className="m-4">
+                                <span className="font-bold">Veg noodles</span>
+                                <span className="block text-gray-500 text-sm">Recipe by Mario</span>
+                            </div>
+                            <div className="badge">
+                                <svg className="w-5 inline-block mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                <span>25 mins</span>
+                            </div>
+                        </div>
+                        <div className="card hover:shadow-lg">
+                            <Image src={curry} alt="curry" className="w-full h-32 sm:h-48 object-cover"></Image>
+                            <div className="m-4">
+                                <span className="font-bold">Tofu curry</span>
+                                <span className="block text-gray-500 text-sm">Recipe by Mario</span>
+                            </div>
+                            <div className="badge">
+                                <svg className="w-5 inline-block mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                <span>25 mins</span>
+                            </div>
+                        </div>
+                        <div className="card hover:shadow-lg">
+                            <Image src={stew} alt="stew" className="w-full h-32 sm:h-48 object-cover"></Image>
+                            <div className="m-4">
+                                <span className="font-bold">5 Bean Chilli Stew</span>
+                                <span className="block text-gray-500 text-sm">Recipe by Mario</span>
+                            </div>
+                            <div className="badge">
+                                <svg className="w-5 inline-block mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                <span>25 mins</span>
+                            </div>
+                        </div>
+                        </div>
+                        <h4 className="font-bold mt-12 pb-2 border-b border-gray-100">Most popular</h4>
+                        <div className="mt-8">
+                        </div>
+                        <div className="flex justify-center">
+                            <div className='btn bg-secondary-100 text-secondary-200 hover:shadow-inner transform hover:scale-125 hover:bg-opacity-50 transition ease-out duration-300'>Load more</div>
+                        </div>
+                    </div>
+                </main>
             </div>
-            <header>
-                <h2 className="text-gray-700 text-6xl font-semibold">Recipes</h2>
-                <h3 className="text-2xl font-semibold">For Janessa</h3>
-            </header>
-            <div>
-                <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">Latest Recipes</h4>
-                <div className="mt-8">
-                  <div>
-                      <Image src={stew} alt="stew"></Image>
-                      <div>
-                          <span>5 Bean Chilli Stew</span>
-                          <span>Recipe by Mario</span>
-                      </div>
-                  </div>
-                </div>
-                <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">Most popular</h4>
-                <div className="mt-8">
-                </div>
-                <div>
-                    <div className='bg-secondary-100 text-secondary-200'>Load more</div>
-                </div>
-            </div>
-        </main>
     </div>
   )
 }

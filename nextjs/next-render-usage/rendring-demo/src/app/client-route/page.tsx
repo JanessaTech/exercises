714545ee156ee93124a9244beea21f3632e94f82
Janessa <lvlajzhao1983@gmail.com>
2024-06-20 11:41:35 +0800

    how can we make ContextPovider and Server Components work together

--- nextjs/next-render-usage/rendring-demo/src/app/client-route/page.tsx
+++ nextjs/next-render-usage/rendring-demo/src/app/client-route/page.tsx
@@ -1,13 +1,17 @@
 "use client"
+import { useTheme } from '@/components/theme-provider'
 //import { serverSideFunction } from '@/utils/server-utils'
 import React from 'react'
 
 export default function ClientRoutePage() {
+    const theme = useTheme()
     console.log('Client route rendered')
     //const res = serverSideFunction()
   return (
     <>
-        <h1>Client Route Page</h1>
+        <h1 style={{
+            color: theme.colors.secondary
+        }}>Client Route Page</h1>
         {/* <p>{res}</p> */}
     </>
     

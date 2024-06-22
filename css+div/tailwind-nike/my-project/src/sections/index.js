1c5f7f78d45b828703086c7a892c787f12479182
Janessa <lvlajzhao1983@gmail.com>
2024-06-22 10:58:37 +0800

    updates

---
+++ css+div/tailwind-nike/my-project/src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomReviews from "./CustomReviews";
+import Footer from "./Footer";
+import Hero from "./Hero"
+import PopularProducts from "./PopularProducts"
+import Services from "./Services"
+import SpecialOffer from "./SpecialOffer"
+import Subscribe from "./Subscribe"
+import SuperQuality from "./SuperQuality"
+
+export {
+    CustomReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}

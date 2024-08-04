8742212cbafb13f105568f574db94b6cf15be766
Janessa <lvlajzhao1983@gmail.com>
2024-08-04 21:56:26 +0800

    demo for input

--- css+div/tailwind-dive-deep/src/app/page.tsx
+++ css+div/tailwind-dive-deep/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Grid from "@/components/gridDemo/Grid";
+import InPut from "@/components/inputDemo/Input";
 import Size from "@/components/sizeDemo/Size";
 
 export default function Home() {
   return (
     // <Size/>
-    <Grid/>
+    // <Grid/>
+    <InPut/>
   );
 }
---
+++ css+div/tailwind-dive-deep/src/components/inputDemo/Input.tsx
@@ -0,0 +1,12 @@
+
+
+const InPut = () => {
+    return (
+        <div>
+            ddd
+
+        </div>
+    )
+}
+
+export default InPut

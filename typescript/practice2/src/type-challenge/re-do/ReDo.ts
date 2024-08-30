e6bc515dad5361824f3af8dfe82afc649f712d44
Janessa <lvlajzhao1983@gmail.com>
2024-08-30 22:46:56 +0800

    TS practice

--- typescript/practice2/src/type-challenge/re-do/ReDo.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo.ts
@@ -1,27 +1,23 @@
-import { Alike, Expect, Equal, NotEqual } from "../test-utils";
+import { Alike, Expect, Equal, NotEqual, ExpectExtends } from "../test-utils";
 
 type cases = [
-  Expect<Equal<Trunc<0.1>, '0'>>,
-  Expect<Equal<Trunc<0.2>, '0'>>,
-  Expect<Equal<Trunc<1.234>, '1'>>,
-  Expect<Equal<Trunc<12.345>, '12'>>,
-  Expect<Equal<Trunc<-5.1>, '-5'>>,
-  Expect<Equal<Trunc<'.3'>, '0'>>,
-  Expect<Equal<Trunc<'1.234'>, '1'>>,
-  Expect<Equal<Trunc<'-.3'>, '-0'>>,
-  Expect<Equal<Trunc<'-10.234'>, '-10'>>,
-  Expect<Equal<Trunc<10>, '10'>>,
+  Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
+  Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
+  Expect<Equal<Unique<[1, 'a', 2, 'b', 2, 'a']>, [1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>, [string, number, 1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[unknown, unknown, any, any, never, never]>, [unknown, any, never]>>,
 ]
 
+type Include<T, E> = T extends [infer F, ...infer rest]
+? Equal<E, F> extends true
+  ? true
+  : Include<rest, E>
+: false
 
-type Trunc<T extends string | number> = `${T}` extends `${infer left}.${infer right}`
-? left extends `-${infer rest}`
-  ? rest extends ''
-   ? '-0'
-   : `-${rest}`
-  : left extends ''
-    ? '0'
-    : left
-: `${T}`
+type test = Include<[string, never], never>
 
-type test = Trunc<'.3'>
+type Unique<T, acc extends unknown[] = []> = T extends [infer F, ...infer rest]
+? Include<acc, F> extends true
+  ? Unique<rest, acc>
+  : Unique<rest, [...acc, F]>
+: acc

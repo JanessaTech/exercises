1511060bcf31dd3f6d63f459621e71e0c7b484b5
Janessa <lvlajzhao1983@gmail.com>
2024-09-12 21:23:33 +0800

    daily practice for div/css

--- css+div/tailwind-challenge1/src/app/page.tsx
+++ css+div/tailwind-challenge1/src/app/page.tsx
@@ -21,42 +21,41 @@ export default function Home() {
       <div className="grid md:grid-cols-3">
         <div className="md:col-span-1">
           <div className="flex justify-between md:justify-end items-center">
-            <div className="py-6 border-b-[1px] border-zinc-200 px-3">
-              <span className="text-xl font-semibold uppercase">Janessa food</span>
+            <div className="py-3 border-b-[1px] border-zinc-200">
+              <span className="text-xl font-semibold uppercase px-2">Janessa food</span>
             </div>
-            <div>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
-              className="w-6 md:hidden cursor-pointer hover:text-zinc-600">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
+            <div className="md:hidden cursor-pointer hover:text-zinc-600" onClick={handleClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                className="w-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
             </div>
           </div>
-          <ul>
+          <ul className={`${showMenu ? '' : 'hidden'}`}>
             <li className="cursor-pointer" onClick={() => handleMenu(0)}>
-              <div className={`flex justify-end border-r-4 hover:border-red-600 my-3 ${selected === 0 ? 'border-red-600' : 'border-white'}`}>
-                <span className={`hover:font-semibold ${selected === 0 ? 'font-semibold' : ''}`}>Home</span>
+              <div className={`flex justify-end items-center border-r-4 border-red-600 my-2 ${selected === 0 ? 'border-red-600' : 'border-white'}`}>
+                <span className={`${selected === 0 ? 'font-semibold text-red-600' : 'text-inherit'}`}>Home</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
-                className="w-6 mx-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                className={`w-6 mx-2 ${selected === 0 ? 'text-red-600' : ''}`}>
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
               </div>
             </li>
             <li className="cursor-pointer" onClick={() => handleMenu(1)}>
-              <div className={`flex justify-end border-r-4 hover:border-red-600 my-3 ${selected === 1 ? 'border-red-600' : 'border-white'}`}>
-                <span className={`hover:font-semibold ${selected === 1 ? 'font-semibold' : ''}`}>Email</span>
+              <div className={`flex justify-end items-center border-r-4 border-red-600 my-2 ${selected === 1 ? 'border-red-600' : 'border-white'}`}>
+                <span className={`${selected === 1 ? 'font-semibold text-red-600' : 'text-inherit'}`}>Question</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
-                className="w-6 mx-2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                className={`w-6 mx-2 ${selected === 1 ? 'text-red-600' : ''}`}>
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
                 </svg>
-
               </div>
             </li>
           </ul>
         </div>
-        <div className="md:col-span-2 bg-green-300 h-30">
-          ddd
+        <div className="md:col-span-2 h-20 bg-green-300">
         </div>
       </div>
+
     </div>
   );
 }

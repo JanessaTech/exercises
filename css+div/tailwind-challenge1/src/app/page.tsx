360c6c2ba0190b808bd1daac954606aa7beafc1c
Janessa <lvlajzhao1983@gmail.com>
2024-09-12 08:04:40 +0800

    daily practice for css/div

--- css+div/tailwind-challenge1/src/app/page.tsx
+++ css+div/tailwind-challenge1/src/app/page.tsx
@@ -20,88 +20,41 @@ export default function Home() {
     <div className="container">
       <div className="grid md:grid-cols-3">
         <div className="md:col-span-1">
-          <div className="flex md:justify-end justify-between items-center">
-            <div className="py-3 border-b-[1px] border-gray-100">
-              <span className="text-3xl font-bold">Janessa Food</span>
+          <div className="flex justify-between md:justify-end items-center">
+            <div className="py-6 border-b-[1px] border-zinc-200 px-3">
+              <span className="text-xl font-semibold uppercase">Janessa food</span>
             </div>
-            <div className="md:hidden" onClick={handleClick}>
+            <div>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
-              className="w-6 hover:text-gray-600 cursor-pointer">
+              className="w-6 md:hidden cursor-pointer hover:text-zinc-600">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
               </svg>
             </div>
           </div>
-          <ul className={`${showMenu ? '': 'hidden'} mt-3`}>
+          <ul>
             <li className="cursor-pointer" onClick={() => handleMenu(0)}>
-              <div className={`flex justify-end border-r-[4px]  hover:text-sky-600 ${selected === 0 ? 'text-sky-600 border-sky-600' : 'border-white'}`}>
+              <div className={`flex justify-end border-r-4 hover:border-red-600 my-3 ${selected === 0 ? 'border-red-600' : 'border-white'}`}>
+                <span className={`hover:font-semibold ${selected === 0 ? 'font-semibold' : ''}`}>Home</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
-                className="w-6 mx-1">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                className="w-6 mx-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
-                <span className="mx-1">Home</span>
               </div>
             </li>
             <li className="cursor-pointer" onClick={() => handleMenu(1)}>
-              <div className={`flex justify-end border-r-[4px]  hover:text-sky-600 ${selected === 1 ? 'text-sky-600 border-sky-600' : 'border-white'}`}>
+              <div className={`flex justify-end border-r-4 hover:border-red-600 my-3 ${selected === 1 ? 'border-red-600' : 'border-white'}`}>
+                <span className={`hover:font-semibold ${selected === 1 ? 'font-semibold' : ''}`}>Email</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
-                className="w-6 mx-1">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                className="w-6 mx-2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                 </svg>
-                <span className="mx-1">About</span>
+
               </div>
             </li>
           </ul>
-          
         </div>
-        <div className="md:col-span-2 m-10">
-          <div className="flex md:justify-end justify-center">
-            <Link href="#" className="px-3 py-1 border-[2px] text-red-400 border-red-400 rounded-full hover:text-white hover:bg-red-500 transition ease-in-out duration-300 mx-1">Login</Link>
-            <Link href="#" className="px-3 py-1 border-[2px] text-red-400 border-red-400 rounded-full hover:text-white hover:bg-red-500 transition ease-in-out duration-300">Logout</Link>
-          </div>
-          <div>
-            <span className="font-semibold text-3xl block">My recipes</span>
-            <span className="text-gray-400">For Janessa</span>
-          </div>
-          <div className="py-3 mt-10border-b-[1px] border-gray-100">
-            <span className="font-semibold">Latest recipes</span>
-          </div>
-          <div className="grid md:grid-cols-3 py-3 gap-3">
-            <div className="rounded-md shadow-md hover:shadow-xl overflow-hidden">
-              <img src="/img/stew.jpg" alt="stew" className="w-full object-over"/>
-              <div className="p-3">
-                <span className="font-bold block">5 Bean Chilli Stew</span>
-                <span className="text-gray-400 text-sm">Recipe by Mario</span>
-              </div>
-            </div>
-            <div className="rounded-md shadow-md hover:shadow-xl overflow-hidden">
-              <img src="/img/stew.jpg" alt="stew" className="w-full object-over"/>
-              <div className="p-3">
-                <span className="font-bold block">5 Bean Chilli Stew</span>
-                <span className="text-gray-400 text-sm">Recipe by Mario</span>
-              </div>
-            </div>
-            <div className="rounded-md shadow-md hover:shadow-xl overflow-hidden">
-              <img src="/img/stew.jpg" alt="stew" className="w-full object-over"/>
-              <div className="p-3">
-                <span className="font-bold block">5 Bean Chilli Stew</span>
-                <span className="text-gray-400 text-sm">Recipe by Mario</span>
-              </div>
-            </div>
-            <div className="rounded-md shadow-md hover:shadow-xl overflow-hidden">
-              <img src="/img/stew.jpg" alt="stew" className="w-full object-over"/>
-              <div className="p-3">
-                <span className="font-bold block">5 Bean Chilli Stew</span>
-                <span className="text-gray-400 text-sm">Recipe by Mario</span>
-              </div>
-            </div>
-            
-          </div>
-          <div className="my-4">
-              <span className="font-bold">Most poplular</span>
-          </div>
-          <div className="flex justify-center">
-            <button className="px-3 py-2 rounded-full bg-gray-400 hover:bg-gray-500">Load more</button>
-          </div>
+        <div className="md:col-span-2 bg-green-300 h-30">
+          ddd
         </div>
       </div>
     </div>

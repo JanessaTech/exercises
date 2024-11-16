238709809f0d5fdacb6cea46bbe97f03449c0c94
Janessa <lvlajzhao1983@gmail.com>
2024-11-16 18:29:49 +0800

    update solution for leetcode 100

--- js/algrithm/tree/Same_Tree_100.js
+++ js/algrithm/tree/Same_Tree_100.js
@@ -12,12 +12,10 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if (!p && q) return false
-    if (p && !q) return false
-    if (!p && !q) return true
-    if (p && q && p.val !== q.val) return false
-    const isSameLeft = isSameTree(p.left, q.left)
-    if (!isSameLeft) return isSameLeft
-    const isSameRight = isSameTree(p.right, q.right)
-    return isSameLeft && isSameRight
+    if (!q && !p) return true
+    if ((!p && q) || (p && !q)) return false
+    if (p && q && q.val !== p.val) return false
+    const left = isSameTree(p.left, q.left)
+    const right = isSameTree(p.right, q.right)
+    return left && right
 };

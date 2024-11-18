f78758d3a1a0795b32ea5ab591858dfe9edd31bc
Janessa <lvlajzhao1983@gmail.com>
2024-11-18 21:54:31 +0800

    update solution for 437

--- js/algrithm/tree/Path_Sum_III_437.js
+++ js/algrithm/tree/Path_Sum_III_437.js
@@ -1,33 +1,26 @@
 const pathSum = (root, targetSum) => {
-	let output = 0;
-	let map = {};
-
-	const traverse = (root, pathSum) => {
-		if (!root) return null;
-
-		pathSum += root.val; // current path sum
-
-		// Case 1: starts from root node
-		if (pathSum === targetSum) output++;
-
-		// Case 2: starts from middle of tree
-		// frequency of targetSum in the current path (up to current node)
-		if (map[pathSum - targetSum]) output += map[pathSum - targetSum];
-
-		// memoize current path sum (root to current node) and it's frequency
-		if (map[pathSum]) map[pathSum]++;
-		else map[pathSum] = 1;
-
-		if (root.left) traverse(root.left, pathSum);
-		if (root.right) traverse(root.right, pathSum);
-
-		// remove the current path sum
-		// to note that path is not available/visited
-		map[pathSum]--;
-	};
-
-	traverse(root, 0);
-	return output;
+	var cnt = 0
+    const map = new Map([])
+    const dfs = function(node, acc) {
+        if (!node) return
+        acc += node.val
+        if (acc === targetSum) {
+            cnt++;
+        }
+        if (map[acc - targetSum]) {
+            cnt += map[acc - targetSum]
+        }
+        if (map[acc]) {
+            map[acc]++
+        } else {
+            map[acc] = 1
+        }
+        dfs(node.left, acc)
+        dfs(node.right, acc)
+        map[acc]--
+    }
+    dfs(root, 0)
+    return cnt
 };
 
 //check https://leetcode.com/problems/path-sum-iii/solutions/1745666/explanation-detailed-javascript-solution/

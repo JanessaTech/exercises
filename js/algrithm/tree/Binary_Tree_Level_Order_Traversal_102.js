c0aeccb5353bc631132d8714fc88540266c13e67
Janessa <lvlajzhao1983@gmail.com>
2024-11-18 10:05:56 +0800

    update solution for 102

--- js/algrithm/tree/Binary_Tree_Level_Order_Traversal_102.js
+++ js/algrithm/tree/Binary_Tree_Level_Order_Traversal_102.js
@@ -13,22 +13,23 @@
 var levelOrder = function(root) {
     const ans = []
     const queue = []
-    if (root !== null) queue.push(root)
-
-    while (queue.length > 0) {
+    if (!root) return ans
+    queue.push(root)
+    var level = 0
+    while (queue.length) {
         const size = queue.length
-        const subAns = []
+        ans[level] = []
         for (let i = 0; i < size; i++) {
             const cur = queue.shift()
-            subAns.push(cur.val)
+            ans[level].push(cur.val)
             if (cur.left) {
                 queue.push(cur.left)
-            }
+            } 
             if (cur.right) {
                 queue.push(cur.right)
             }
         }
-        ans.push(subAns)
+        level++
     }
     return ans
 };

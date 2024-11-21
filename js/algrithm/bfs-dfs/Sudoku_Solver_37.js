b518a50c578e902e46cc4136ab4c82ee2ec5db6d
Janessa <lvlajzhao1983@gmail.com>
2024-11-21 09:56:25 +0800

    update solution for 37

---
+++ js/algrithm/bfs-dfs/Sudoku_Solver_37.js
@@ -0,0 +1,46 @@
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solveSudoku = function(board) {
+    const isValid = function(row, col, k) {
+        for (let i = 0; i < 9; i++) {
+            if (board[row][i] === k) return false
+        }
+        for (let j = 0; j < 9; j++) {
+            if (board[j][col] === k) return false
+        }
+        const si = Math.floor(row / 3) * 3
+        const sj = Math.floor(col / 3) * 3
+        for (let i = si; i < si + 3; i++) {
+            for (let j = sj; j < sj + 3; j++) {
+                if (board[i][j] === k) return false 
+            }
+        }
+        return true
+    }
+
+    const dfs = function(row, col) {
+        if (row === 9) return true
+        if (col === 9) {
+            return dfs(row + 1, 0)
+        }
+        const ch = board[row][col]
+        if (ch === '.') {
+            for (let i = 1; i <= 9; i++) {
+                if (isValid(row, col, i + '')) {
+                    board[row][col] = i + ''
+                    if (dfs(row, col)) {
+                        return true
+                    }
+                    board[row][col] = '.'
+                }
+            }
+        } else {
+            return dfs(row, col + 1)
+        }
+        return false
+
+    }
+    dfs(0, 0)
+};

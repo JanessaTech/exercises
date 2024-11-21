eb002f008bb439827837fab7b52a2e78be26734f
Janessa <lvlajzhao1983@gmail.com>
2024-11-21 15:33:49 +0800

    update solution for 79

--- js/algrithm/bfs-dfs/Word_Search_79.js
+++ js/algrithm/bfs-dfs/Word_Search_79.js
@@ -76,7 +76,7 @@ const word = "a"
 const res  = exist(board, word)
 console.log('res = ', res) */
 
-var exist = function(board, word) {
+/*var exist = function(board, word) {
     const getFirst = function() {
         const first = word.charAt(0)
         const ans = []
@@ -149,6 +149,86 @@ var exist = function(board, word) {
     return found
 };
 
+
+const board = [["A","B","C","E"],
+               ["S","F","C","S"],
+               ["A","D","E","E"]]
+const word = "SEE"
+
+const res  = exist(board, word)
+console.log('res = ', res) */
+var exist = function(board, word) {
+    const getFirst = function() {
+        const first = word.charAt(0)
+        const ans = []
+        for (let i  = 0; i < board.length; i++) {
+            for (let j = 0; j < board[0].length; j++) {
+                if (board[i][j] === first) {
+                    ans.push([i, j])
+                }
+            }
+        }
+        return ans
+    }
+
+    const iniVisited = function() {
+        const visited = []
+        for (let i = 0; i < board.length; i++) {
+            visited.push(Array(board[0].length).fill(false))
+        }
+        return visited
+    }
+
+    const resetVisited = function(visited) {
+        for (let i = 0; i < visited.length; i++) {
+            for (let j = 0; j < visited[0].length; j++) {
+                visited[i][j] = false
+            }
+        }
+    }
+
+    const isValid = function(row, col) {
+        if (row < 0 || row >= board.length) return false
+        if (col < 0 || col >= board[0].length) return false
+        return true
+    }
+
+    const dfs = function(row, col, pos) {
+        const target = word.charAt(pos)
+        const cur = board[row][col]
+        if (cur === target) {
+            if (pos === word.length - 1) {
+                found = true
+                return
+            }
+            visited[row][col] = true
+            for (let dir of dirs) {
+                const nr = row + dir[0]
+                const nc = col + dir[1]
+                if (isValid(nr, nc) && !visited[nr][nc]) {
+                    dfs(nr, nc, pos + 1)
+                }
+            }
+             visited[row][col] = false
+        }
+    }
+
+    const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+    var found = false
+    const firsts = getFirst()
+    const visited = iniVisited()
+
+    for (const first of firsts) {
+        dfs(first[0], first[1], 0)
+        if (!found) {
+            resetVisited(visited)
+        } else {
+            break
+        }
+    }
+    return found
+};
 
 const board = [["A","B","C","E"],
                ["S","F","C","S"],

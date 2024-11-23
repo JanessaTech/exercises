9119bcb94e9ab3dd3bb104fbe5021fd23a7ed177
Janessa <lvlajzhao1983@gmail.com>
2024-11-24 07:21:18 +0800

    update solution for 35

---
+++ js/algrithm/binary _search/Search_Insert_Position_35.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    var lo = 0
+    var hi = nums.length - 1
+    while (lo <= hi) {
+        const mid = Math.floor((lo + hi) / 2)
+        if (nums[mid] === target) return mid
+        if (nums[mid] < target) {
+            lo = mid + 1
+        } else {
+            hi = mid - 1
+        }
+    }
+    return lo
+};

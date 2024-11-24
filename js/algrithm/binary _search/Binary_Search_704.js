3ed4955345a26fbc4cf96b977720b8857710dad4
Janessa <lvlajzhao1983@gmail.com>
2024-11-24 09:42:08 +0800

    update solution for 704

---
+++ js/algrithm/binary _search/Binary_Search_704.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    var lo = 0, hi = nums.length - 1
+    while (lo <= hi) {
+        const mid = Math.floor((lo + hi) / 2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] < target) {
+            lo = mid + 1
+        } else {
+            hi = mid - 1
+        }
+    }
+    return - 1
+};

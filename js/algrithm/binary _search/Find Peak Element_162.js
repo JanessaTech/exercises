1ffc749e23cd73d64f8df1cd22f629d2abd076ed
Janessa <lvlajzhao1983@gmail.com>
2024-11-28 10:45:47 +0800

    update solution for 162

--- js/algrithm/binary _search/Find Peak Element_162.js
+++ js/algrithm/binary _search/Find Peak Element_162.js
@@ -3,24 +3,20 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    if (nums.length === 1) return 0
     const n = nums.length
+    if (n === 1) return 0
     if (nums[0] > nums[1]) return 0
-    if (nums[n - 1] > nums[n - 2]) return n - 1
+    if (nums[n - 1] > nums[n - 2]) return n -2
     var lo = 1
-    var hi = n - 2
+    var hi = n -2
     while (lo <= hi) {
         const mid = Math.floor((lo + hi) / 2)
         if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid
-        if (nums[mid] < nums[mid - 1] ) {
+        else if (nums[mid - 1] > nums[mid]) {
             hi = mid - 1
-        }
-        if (nums[mid] < nums[mid + 1]) {
+        } else if (nums[mid + 1] > nums[mid]) {
             lo = mid + 1
         }
     }
     return -1
-
 };
-
-// https://leetcode.com/problems/find-peak-element/

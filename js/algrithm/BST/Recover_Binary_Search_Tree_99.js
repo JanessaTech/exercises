84e15e321a586efc55b2dbeed2798daba751ba1e
Janessa <lvlajzhao1983@gmail.com>
2024-12-11 17:10:32 +0800

    update solution for 99

---
+++ js/algrithm/BST/Recover_Binary_Search_Tree_99.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var recoverTree = function(root) {
+    let pre = null
+    let first = null
+    let second = null
+
+    const dfs = function(node) {
+        if (node === null) return
+        dfs(node.left)
+        if (!first && pre && pre.val > node.val) first = pre
+        if (first && pre && pre.val > node.val) second = node
+        pre = node
+        dfs(node.right)
+    }
+
+    const swap = function(first, second) {
+        const tmp = first.val
+        first.val = second.val
+        second.val = tmp
+    }
+
+    dfs(root)
+    swap(first, second)
+};

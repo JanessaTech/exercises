509356c1de02c7f09ee4b5c08b988e038d7cae7f
Janessa <lvlajzhao1983@gmail.com>
2024-12-12 17:28:56 +0800

    update solution for 133

--- js/algrithm/Graph/Clone_Graph_133.js
+++ js/algrithm/Graph/Clone_Graph_133.js
@@ -11,23 +11,49 @@
  * @return {_Node}
  */
 var cloneGraph = function(node) {
-    const visited = new Map([])
-    const clone = function(node) {
-        if (node === null) return null
-        const clonedNode = new _Node(node.val)
-        visited.set(node.val, clonedNode)
-        for (neighbor of node.neighbors) {
+    const visited = new Map()
+    //const copy = dfs(node, visited)
+    const copy = bfs(node)
+    return copy
+};
+
+function dfs(node, visited) {
+    if (node === null) return null
+    const newNode = new _Node(node.val)
+    visited.set(node.val, newNode)
+    for (let neighbor of node.neighbors) {
+        let clonedNeighbor = undefined
+        if (visited.has(neighbor.val)) {
+            clonedNeighbor = visited.get(neighbor.val)
+        } else {
+            clonedNeighbor = dfs(neighbor, visited)
+        }
+        newNode.neighbors.push(clonedNeighbor)
+    }
+    return newNode
+}
+
+function bfs(node) {
+    if (!node) return null
+    const visited = new Map()
+    const queue = []
+    queue.push(node)
+    const copy = new _Node(node.val)
+    visited.set(node.val, copy)
+
+    while (queue.length > 0) {
+        const cur = queue.pop()
+        for (let neighbor of cur.neighbors) {
             let clonedNeighbor = undefined
             if (visited.has(neighbor.val)) {
                 clonedNeighbor = visited.get(neighbor.val)
             } else {
-                clonedNeighbor = clone(neighbor)
+                clonedNeighbor = new _Node(neighbor.val)
+                visited.set(neighbor.val, clonedNeighbor)
+                queue.push(neighbor)
             }
-            clonedNode.neighbors.push(clonedNeighbor)
+            visited.get(cur.val).neighbors.push(clonedNeighbor)
         }
-        return clonedNode
     }
-
-    const copy = clone(node)
-    return copy 
-};
+    return copy
+}

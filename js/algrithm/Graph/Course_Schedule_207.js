099bc47f434c0c028fbf94967eb4aed8e9e7c45a
Janessa <lvlajzhao1983@gmail.com>
2024-12-16 18:29:22 +0800

    update solution for 207

--- js/algrithm/Graph/Course_Schedule_207.js
+++ js/algrithm/Graph/Course_Schedule_207.js
@@ -8,6 +8,41 @@ var canFinish = function(numCourses, prerequisites) {
     return res
 };
 
+function dfs(numCourses, prerequisites) {
+    const digraph = []
+    const visited = Array(numCourses).fill(false)
+    const memo = Array(numCourses).fill(false)
+    for (let pre of prerequisites) {
+        if (!digraph[pre[1]]) {
+            digraph[pre[1]] = []
+        }
+        digraph[pre[1]].push(pre[0])
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        if (!_dfs(digraph, visited, i, memo)) {
+            return false
+        }
+    }
+    return true
+}
+
+function dfs(digraph, visited, i, memo) {
+    if (visited[i]) return false
+    if (memo[i]) return true
+    visited[i] = true
+    if (digraph[i]) {
+        for (let next of digraph[i]) {
+            if (!dfs(digraph, visited, next, memo)) {
+                return false
+            }
+        }
+    }
+    visited[i] = false
+    memo[i] = true
+    return true
+}
+
 function bfs(numCourses, prerequisites) {
     const queue = []
     const digraph = new Map()

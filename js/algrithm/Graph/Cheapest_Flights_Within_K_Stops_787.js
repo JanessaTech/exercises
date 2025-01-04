b64b2246a6c5158656731ad7ecf1a0853e000366
Janessa <lvlajzhao1983@gmail.com>
2025-01-04 11:33:15 +0800

    update solution for 787

--- js/algrithm/Graph/Cheapest_Flights_Within_K_Stops_787.js
+++ js/algrithm/Graph/Cheapest_Flights_Within_K_Stops_787.js
@@ -7,11 +7,14 @@
  * @return {number}
  */
 var findCheapestPrice = function(n, flights, src, dst, k) {
-    return bellman_ford(n, flights, src, dst, k)
+    const res = bfs(n, flights, src, dst, k)
+    //const res = bellman(n, flights, src, dst, k)
+    return res
  }
-var findCheapestPrice1 = function(n, flights, src, dst, k) {
+
+ function bfs(n, flights, src, dst, k) {
     const digraph = createDigraph(n, flights)
-    const dists = Array(n).fill(Number.MAX_VALUE)
+    const dists = Array(n).fill(Infinity)
     dists[src] = 0
 
     const queue = [[src, 0]]
@@ -27,14 +30,15 @@ var findCheapestPrice1 = function(n, flights, src, dst, k) {
                     dists[to] = newPrice
                     queue.push([to, newPrice])
                 }
+                
             }
         }
         level++
     }
-    return dists[dst] === Number.MAX_VALUE ? -1 : dists[dst]
-};
+    return dists[dst] === Infinity ? -1 : dists[dst]
+ }
 
-function createDigraph(n, flights) {
+ function createDigraph(n, flights) {
     const digraph = Array(n).fill(undefined).map((_, i) => [])
     for (let flight of flights) {
         const from = flight[0]
@@ -45,19 +49,21 @@ function createDigraph(n, flights) {
     return digraph
 }
 
-function bellman_ford(n, flights, src, dst, K) {
-    let M = Array(n).fill(undefined).map((_, i) => i === src? 0: Infinity)
-    for (let i = 0; i < K + 1; i++) {
+ function bellman(n, flights, src, dst, k) {
+    let M = Array(n).fill(Infinity)
+    M[src] = 0
+    for (let i = 0; i < k + 1; i++) {
         let N = [...M]
         for (let [from, to, price] of flights) {
             N[to] = Math.min(N[to], M[from] + price)
         }
         M = [...N]
     }
-    return M[dst] !== Infinity ? M[dst] : -1
-}
+    return M[dst] !== Infinity ? M[dst] :-1
+ }
 
-const n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1
+const n = 5, flights =
+[[1,2,10],[2,0,7],[1,3,8],[4,0,10],[3,4,2],[4,2,10],[0,3,3],[3,1,6],[2,4,5]], src = 0, dst = 4, k = 1
 
 const res = findCheapestPrice(n, flights, src, dst, k)
 console.log(res)

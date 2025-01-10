f998a700c16bc074d62cb2d2e2fd4dc83098689c
Janessa <lvlajzhao1983@gmail.com>
2025-01-10 16:49:34 +0800

    update solution for 46

--- js/algrithm/bfs-dfs/Permutations_46.js
+++ js/algrithm/bfs-dfs/Permutations_46.js
@@ -5,21 +5,22 @@
 var permute = function(nums) {
     const visited = Array(nums.length).fill(false)
     const ans = []
-    const dfs = function(level, path) {
-        if (level === nums.length) {
+    const dfs = function(path) {
+        if (path.length === nums.length) {
             ans.push(path.slice())
         } else {
             for (let i = 0; i < nums.length; i++) {
                 if (!visited[i]) {
                     visited[i] = true
                     path.push(nums[i])
-                    dfs(level + 1, path)
+                    dfs(path)
                     path.pop()
                     visited[i] = false
                 }
             }
         }
     }
-    dfs(0, [])
-    return ans 
+
+    dfs([])
+    return ans
 };

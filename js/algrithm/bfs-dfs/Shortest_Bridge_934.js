ffb1bad696afeacb93f6d08509f66915bf212bfa
Janessa <lvlajzhao1983@gmail.com>
2025-01-12 20:56:18 +0800

    update solution for 934

--- js/algrithm/bfs-dfs/Shortest_Bridge_934.js
+++ js/algrithm/bfs-dfs/Shortest_Bridge_934.js
@@ -3,73 +3,62 @@
  * @return {number}
  */
 var shortestBridge = function(grid) {
+    const n = grid.length
+    const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    const queue = []
+
     const init = function() {
         const visited = []
-        for (let i = 0; i < grid.length; i++) {
-            visited.push(Array(grid[0].length).fill(false))
+        for (let i = 0; i < n; i++) {
+            visited.push(Array(n).fill(false))
         }
         return visited
     }
-    
-    const isValid = function(row, col) {
-        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return false
-        return true
-    }
-
-    const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-    const [visited] = init()
-    var found = false
-
-    const dfs = function(row, col, visited) {
-        if (!isValid(row, col) || visited[row][col] || grid[row][col] !== 1) return
-        visited[row][col] = true
-        grid[row][col] = 2
-        for (const dir of dirs) {
-            const nr = row + dir[0]
-            const nc = col + dir[1]
-            dfs(nr, nc, visited)
-        }
-    }
-
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === 1 && !found) {
-                dfs(i, j, visited)
-                found = true
+    const getStarter = function() {
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                if (grid[i][j] === 1) return [i, j]
             }
         }
     }
 
-    const queue = []
-    var level = 0
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === 2) {
-                queue.push([i, j])
-                visited[i][j] = true
+    const dfs = function(i, j, visited){
+        visited[i][j] = true
+        queue.push([i, j])
+        grid[i][j] = 2
+        for (let dir of dirs) {
+            const nr = dir[0] + i
+            const nc = dir[1] + j
+            if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc] && grid[nr][nc] === 1) {
+                dfs(nr, nc, visited)
             }
         }
     }
+
+
+    const starter = getStarter()
+    const visited = init()
+    let level = 0
+    dfs(starter[0], starter[1], visited)
 
     while (queue.length) {
         const size = queue.length
         for (let i = 0; i < size; i++) {
-            const cur = queue.shift()
-            if (grid[cur[0]][cur[1]] === 1) {
-                return level - 1
-            }
-            for (const dir of dirs) {
-                const nr = cur[0] + dir[0]
-                const nc = cur[1] + dir[1]
-                if (isValid(nr, nc) && !visited[nr][nc]) {
+            const [row, col] = queue.shift()
+            if (grid[row][col] === 1) return level - 1
+            for (let dir of dirs) {
+                const nr = dir[0] + row
+                const nc = dir[1] + col
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc]) {
                     queue.push([nr, nc])
                     visited[nr][nc] = true
                 }
-            }
+            } 
         }
         level++
     }
     return -1
+    
 };
 
 const grid =  [[0,1],

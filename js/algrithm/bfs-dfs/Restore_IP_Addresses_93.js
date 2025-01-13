0f7c23ecdd17b6bd22d7e303caad73c10dfa61e8
Janessa <lvlajzhao1983@gmail.com>
2025-01-13 18:38:36 +0800

    update solution for 131

--- js/algrithm/bfs-dfs/Restore_IP_Addresses_93.js
+++ js/algrithm/bfs-dfs/Restore_IP_Addresses_93.js
@@ -7,17 +7,15 @@ var restoreIpAddresses = function(s) {
     if (s.length < 4) return []
     const nums = s.split('').map((v) => parseInt(v))
 
-    const isValid = function(sub) {
+    const validate = function(sub) {
         if (sub.length === 0) return false
-        if (sub.length > 3) return false
         if (sub.length > 1 && sub[0] === 0) return false
-
+        if (sub.length > 3) return false
         var sum = 0
-        for (let i = 0; i < sub.length; i++) {
-            sum += sum * 10 + sub[i]
+        for (let s of sub) {
+            sum = sum * 10 + s
         }
         if (sum > 255) return false
-
         return true
     }
 
@@ -27,19 +25,18 @@ var restoreIpAddresses = function(s) {
             const part2 = nums.slice(seps[0], seps[1])
             const part3 = nums.slice(seps[1], seps[2])
             const part4 = nums.slice(seps[2])
-
             const res = part1.join('') + '.' + part2.join('') + '.' + part3.join('') + '.' + part4.join('')
             ans.push(res)
         } else {
-            for (let i = start; i <= s.length; i++) {
+            for (let i = start; i < s.length; i++) {
                 const part1 = nums.slice(start, i)
                 const part2 = nums.slice(i)
-                const valid1 = isValid(part1)
-                if(seps.length === 2) {
-                    const valid2 = isValid(part2)
+                const valid1 = validate(part1)
+                if (seps.length === 2) {
+                    const valid2 = validate(part2)
                     if (valid1 && valid2) {
                         seps.push(i)
-                        dfs(i, seps)
+                        dfs(i , seps)
                         seps.pop()
                     }
                 } else {
@@ -51,12 +48,13 @@ var restoreIpAddresses = function(s) {
                 }
             }
         }
+
     }
 
     dfs(0, [])
     return ans
 };
 
-const s = "101023"
+const s = "25525511135"
 const res = restoreIpAddresses(s)
 console.log(res)

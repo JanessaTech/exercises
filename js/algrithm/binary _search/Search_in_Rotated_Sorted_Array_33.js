fcb15a1ebf55055880e9d6fba462198b20ce2938
Janessa <lvlajzhao1983@gmail.com>
2025-01-14 15:04:35 +0800

    update solution for 33

--- js/algrithm/binary _search/Search_in_Rotated_Sorted_Array_33.js
+++ js/algrithm/binary _search/Search_in_Rotated_Sorted_Array_33.js
@@ -4,26 +4,25 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    var lo = 0
-    var hi = nums.length - 1
+    let lo = 0, hi = nums.length - 1
     while (lo <= hi) {
         const mid = Math.floor((lo + hi) / 2)
         if (nums[mid] === target) {
             return mid
         }
         if (nums[lo] <= nums[mid]) {
-            if (target <= nums[mid] && target >= nums[lo]) {
+            if (target >= nums[lo] && target < nums[mid]) {
                 hi = mid - 1
             } else {
                 lo = mid + 1
             }
         } else {
-            if (target >= nums[mid] && target <= nums[hi]) {
+            if (target > nums[mid] && target <= nums[hi]) {
                 lo = mid + 1
             } else {
                 hi = mid - 1
             }
         }
-    } 
+    }
     return -1
 };

79371bbeabf441e013353942705c734c133eab1a
Janessa <lvlajzhao1983@gmail.com>
2025-01-14 16:55:37 +0800

    update solution for 162

--- js/algrithm/binary _search/Find Peak Element_162.js
+++ js/algrithm/binary _search/Find Peak Element_162.js
@@ -3,20 +3,17 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    const n = nums.length
-    if (n === 1) return 0
-    if (nums[0] > nums[1]) return 0
-    if (nums[n - 1] > nums[n - 2]) return n - 1
-    var lo = 1
-    var hi = n -2
-    while (lo <= hi) {
-        const mid = Math.floor((lo + hi) / 2)
-        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid
-        else if (nums[mid - 1] > nums[mid]) {
-            hi = mid - 1
-        } else if (nums[mid + 1] > nums[mid]) {
-            lo = mid + 1
-        }
-    }
-    return -1
+   const n = nums.length
+   if (n === 1) return 0
+   if (nums[0] > nums[1]) return 0
+   if (nums[n - 1] > nums[n - 2]) return n - 1
+   let lo = 1, hi = n - 2
+   while (lo <= hi) {
+    const mid = Math.floor((lo + hi) / 2)
+    if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid
+    else if (nums[mid] < nums[mid + 1]) lo = mid + 1
+    else if (nums[mid] < nums[mid - 1]) hi = mid - 1
+    else lo++
+   }
+   return -1
 };

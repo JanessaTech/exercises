6c1610f83338f8898800d2dc3a5fc1c28634897f
Janessa <lvlajzhao1983@gmail.com>
2025-01-15 10:51:44 +0800

    update solution for 125

--- js/algrithm/two-pointer/Valid_Palindrome_125.js
+++ js/algrithm/two-pointer/Valid_Palindrome_125.js
@@ -3,9 +3,8 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.toLowerCase().replace(/[^a-z0-9]/gi, '')
-
-    for (let i = 0, j = s.length - 1; i < j;i++, j--) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '')
+    for (let i = 0,j = s.length - 1; i < j; i++, j--) {
         if (s.charAt(i) !== s.charAt(j)) return false
     }
     return true

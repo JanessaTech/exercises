3c334e500e87eacf48da263e56698e17a229a340
Janessa <lvlajzhao1983@gmail.com>
2025-01-19 22:42:23 +0800

    update solution for 1202

--- js/algrithm/Graph/Smallest_String_With_Swaps_1202.js
+++ js/algrithm/Graph/Smallest_String_With_Swaps_1202.js
@@ -4,58 +4,59 @@
  * @return {string}
  */
 var smallestStringWithSwaps = function(s, pairs) {
-    const res = []
-    const uf = new UF(s.length)
-    pairs.forEach(([v1, v2]) => uf.union(v1, v2))
-    uf.disJointSets().forEach((ids, key) => {
-        const chars = ids.map(id => s[id])
-        chars.sort()
-        let i = 0
-        ids.forEach(id => {
-            res[id] = chars[i++]
-        })
-    })
-    return res.join('')
-};
+    const n = s.length
+    const id = Array(n).fill(undefined).map((_, i) => i)
+    const sz = Array(n).fill(1)
 
-class UF {
-    constructor(n) {
-        this.id = Array(n).fill(null).map((_,ind) => ind)
-        this.sz = Array(n).fill(1)
-        this.n = n
+    const find = (x) => {
+        if (id[x] === x) return x
+        id[x] = id[id[x]]
+        return find(id[x])
     }
 
-    root(i) {
-        if (i === this.id[i]) return i
-        this.id[i] = this.root(this.id[i])
-        return this.id[i]
+    const isConnected = (i, j) => {
+        return find(i) === find(j)
     }
 
-    isConnected(i, j) {
-        return this.root(i) === this.root(j)
-    }
-
-    union(i, j) {
-        let pi = this.root(i)
-        let pj = this.root(j)
-        if (pi === pj) return
-        if (this.sz[pi] < this.sz[pj]) {
-            this.id[pi] = pj
-            this.sz[pj] = this.sz[pj] + this.sz[pi]
+    const union = (i, j) => {
+        let pi = find(i)
+        let pj = find(j)
+        if (sz[pi] < sz[pj]) {
+            id[pi] = pj
+            sz[pj] += sz[pi]
         } else {
-            this.id[pj] = pi
-            this.sz[pi] = this.sz[pi] + this.sz[pj]
+            id[pj] = pi
+            sz[pi] += sz[pj]
         }
     }
 
-    disJointSets() {
+    const disjointSets = () => {
         const map = new Map()
-        for (let i = 0; i < this.n; i++) {
-            const rt = this.root(i)
-            const ids = map.has(rt) ? map.get(rt) : []
-            ids.push(i)
-            map.set(rt, ids)
+        for (let i = 0; i < n; i++) {
+            const rt = find(i)
+            if (!map.has(rt)) map.set(rt, [])
+            map.get(rt).push(i)
         }
         return map
     }
-}
+
+    for (let pair of pairs) {
+        let i = pair[0]
+        let j = pair[1]
+        if (!isConnected(i, j)) {
+            union(i, j)
+        }
+    }
+
+    const ans = []
+    disjointSets().forEach((ids, root) => {
+        const chars = ids.map((id) => s.charAt(id))
+        chars.sort()
+        let i = 0
+        ids.forEach((id) => {
+            ans[id] = chars[i++]
+        })
+    })
+
+    return ans.join('')
+};
--- js/algrithm/tmp/Test.js
+++ js/algrithm/tmp/Test.js
@@ -1,63 +1,9 @@
-var exist = function(board, word) {
-    const m = board.length
-    const n = board[0].length
-    const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-    const init = function() {
-        const visited = []
-        for (let i = 0; i < m; i++) {
-            visited.push(Array(n).fill(false))
-        }
-        return visited
-    }
-    const reset = function(visited) {
-        for (let i = 0; i < m; i++) {
-            for (let j = 0; j < n; j++) {
-                visited[i][j] = true
-            }
-        }
-    }
-
-    const getStarters = function(ch) {
-        const ans = []
-        for (let i = 0; i < m; i++) {
-            for (let j = 0; j < n; j++) {
-                if (board[i][j] === ch){
-                    ans.push([i, j])
-                }
-            }
-        }
-        return ans
-    }
-
-    const dfs = function(i, j, pos, visited) {
-        if (pos === word.length) return true
-        const target = word.charAt(pos)
-        const cur = board[i][j]
-        if (target === cur) return true
-        visited[i][j] = true
-        for (let dir of dirs) {
-            let nr = dir[0] + i
-            let nc = dir[1] + j
-            if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
-                if (dfs(nr, nc, pos + 1, visited)) return true
-            }
-        }
-        visited[i][j] = false
-        return false
-    }
-
-    const starters = getStarters(word.charAt(0))
-    for (let starter of starters) {
-        const visited = init()
-        if (dfs(starter[0], starter[1], 0, visited)) return true  
-    }
-
-    return false
-    
-};
-
-const board = [["a"]]
-const word = "a"
-const res = exist(board, word)
-console.log(res)
+const map = new Map()
+map.set('a', [])
+map.get('a').push(1)
+map.get('a').push(2)
+console.log(map)
+map.forEach((value, key) => {
+    console.log('key = ', key)
+    console.log('value = ', value)
+})

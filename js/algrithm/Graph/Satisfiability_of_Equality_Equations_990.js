ce73b9c5a37c38dbcd3188359a0f02077d99ba5a
Janessa <lvlajzhao1983@gmail.com>
2025-01-21 10:18:59 +0800

    update solution for 990

--- js/algrithm/Graph/Satisfiability_of_Equality_Equations_990.js
+++ js/algrithm/Graph/Satisfiability_of_Equality_Equations_990.js
@@ -4,12 +4,11 @@
  */
 var equationsPossible = function(equations) {
     const A = 'a'.charCodeAt(0)
-    const id = Array(26).fill(0).map((_, i) => i)
+    const id = Array(26).fill(undefined).map((_, i) => i)
     const sz = Array(26).fill(1)
+
     const find = (x) => {
-        if (id[x] === x) {
-            return x
-        }
+        if (x === id[x]) return x
         id[x] = id[id[x]]
         return find(id[x])
     }
@@ -19,8 +18,9 @@ var equationsPossible = function(equations) {
     }
 
     const union = (i, j) => {
-        let pi = find(i)
-        let pj = find(j)
+        const pi = find(i)
+        const pj = find(j)
+        if (pi === pj) return
         if (sz[pi] < sz[pj]) {
             id[pi] = pj
             sz[pj] += sz[pi]
@@ -30,19 +30,18 @@ var equationsPossible = function(equations) {
         }
     }
 
-    
     for (let eq of equations) {
         if (eq.charAt(1) === '=') {
-            let i = find(eq.charCodeAt(0) - A)
-            let j = find(eq.charCodeAt(3) - A)
+            const i = eq.charCodeAt(0) - A
+            const j = eq.charCodeAt(3) - A
             union(i, j)
         }
     }
 
     for (let eq of equations) {
         if (eq.charAt(1) === '!') {
-            let i = eq.charCodeAt(0) - A
-            let j = eq.charCodeAt(3) - A
+            const i = eq.charCodeAt(0) - A
+            const j = eq.charCodeAt(3) - A
             if (isConnected(i, j)) return false
         }
     }

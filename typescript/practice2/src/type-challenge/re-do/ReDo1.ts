f13b5ed5867c75de402211b30559eabcb8e1fa2c
Janessa <lvlajzhao1983@gmail.com>
2025-01-22 11:21:07 +0800

    redo all TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,15 +1,12 @@
 import {Expect, Equal, Alike, NotEqual} from "../test-utils";
 
 type cases = [
-  Expect<Equal<LastIndexOf<[1, 2, 3, 2, 1], 2>, 3>>,
-  Expect<Equal<LastIndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 7>>,
-  Expect<Equal<LastIndexOf<[0, 0, 0], 2>, -1>>,
-  Expect<Equal<LastIndexOf<[string, 2, number, 'a', number, 1], number>, 4>>,
-  Expect<Equal<LastIndexOf<[string, any, 1, number, 'a', any, 1], any>, 5>>,
+  Expect<Equal<LengthOfString<''>, 0>>,
+  Expect<Equal<LengthOfString<'kumiko'>, 6>>,
+  Expect<Equal<LengthOfString<'reina'>, 5>>,
+  Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
 ]
 
-type LastIndexOf<T, U> = T extends [...infer R, infer L]
-? Equal<L, U> extends true
-  ? R['length']
-  : LastIndexOf<R, U>
-: -1
+type LengthOfString<S extends string, acc extends unknown[] = []> = S extends `${infer F}${infer R}`
+? LengthOfString<R, [...acc, F]>
+: acc['length']

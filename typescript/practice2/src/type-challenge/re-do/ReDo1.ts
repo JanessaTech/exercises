f95e46aaec9a3258d391c02b15961e15111cb50b
Janessa <lvlajzhao1983@gmail.com>
2025-01-24 07:17:44 +0800

    redo all TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,16 +1,26 @@
 import {Expect, Equal, Alike, NotEqual} from "../test-utils";
 
+
 type cases = [
-  Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
-  Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
-  Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
-  Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
-  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
-  Expect<Equal<Replace<'', '', ''>, ''>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
+  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
+  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
+  Expect<Equal<ReplaceAll<'', '', ''>, ''>>,
 ]
 
-type Replace<S extends string, From extends string, To extends string> = From extends ''
+
+type ReplaceAll<S extends string, From extends string, To extends string, acc extends string= ''> = From extends ''
 ? S
-: S extends `${infer F}${From}${infer L}`
-  ? `${F}${To}${L}`
-  : S
+: S extends ''
+  ? acc
+  : S extends `${infer L}${From}${infer R}` 
+    ? ReplaceAll<R, From, To, `${acc}${L}${To}`>
+    : ReplaceAll<'', From, To, `${acc}${S}`>
+
+
+type test = ReplaceAll<'foobar', 'bar', 'foo'>

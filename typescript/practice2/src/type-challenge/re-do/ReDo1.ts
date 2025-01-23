1bbb24a05eff69f37d102006c78d0cafb0cda693
Janessa <lvlajzhao1983@gmail.com>
2025-01-23 21:38:45 +0800

    redo all TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,16 +1,16 @@
 import {Expect, Equal, Alike, NotEqual} from "../test-utils";
 
 type cases = [
-  Expect<Equal<ReplaceFirst<[1, 2, 3], 3, 4>, [1, 2, 4]>>,
-  Expect<Equal<ReplaceFirst<['A', 'B', 'C'], 'C', 'D'>, ['A', 'B', 'D']>>,
-  Expect<Equal<ReplaceFirst<[true, true, true], true, false>, [false, true, true]>>,
-  Expect<Equal<ReplaceFirst<[string, boolean, number], boolean, string>, [string, string, number]>>,
-  Expect<Equal<ReplaceFirst<[1, 'two', 3], string, 2>, [1, 2, 3]>>,
-  Expect<Equal<ReplaceFirst<['six', 'eight', 'ten'], 'eleven', 'twelve'>, ['six', 'eight', 'ten']>>,
+  Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
+  Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'', '', ''>, ''>>,
 ]
 
-type ReplaceFirst<T extends readonly unknown[], S, R, acc extends unknown[] = []> = T extends [infer F, ...infer rest]
-? F extends S
-  ? ReplaceFirst<[], S, R, [...acc, R, ...rest]>
-  : ReplaceFirst<rest, S, R, [...acc, F]>
-: acc
+type Replace<S extends string, From extends string, To extends string> = From extends ''
+? S
+: S extends `${infer F}${From}${infer L}`
+  ? `${F}${To}${L}`
+  : S

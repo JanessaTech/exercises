5050f16bbacc837c832c4ce27c8daabc2a208f94
Janessa <lvlajzhao1983@gmail.com>
2025-01-23 11:36:02 +0800

    redo all TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,30 +1,32 @@
 import {Expect, Equal, Alike, NotEqual} from "../test-utils";
 
+type Result1 = | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type Result2 = | 0 | 1 | 2
+type Result3 =
+  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+  | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20
+  | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30
+  | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40
+  | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50
+  | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60
+  | 61 | 62 | 63 | 64 | 65 | 66 | 67 | 68 | 69 | 70
+  | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80
+  | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90
+  | 91 | 92 | 93 | 94 | 95 | 96 | 97 | 98 | 99 | 100
+  | 101 | 102 | 103 | 104 | 105 | 106 | 107 | 108 | 109 | 110
+  | 111 | 112 | 113 | 114 | 115 | 116 | 117 | 118 | 119 | 120
+  | 121 | 122 | 123 | 124 | 125 | 126 | 127 | 128 | 129 | 130
+  | 131 | 132 | 133 | 134 | 135 | 136 | 137 | 138 | 139 | 140
 type cases = [
-  Expect<Equal<AllCombinations<''>, ''>>,
-  Expect<Equal<AllCombinations<'A'>, '' | 'A'>>,
-  Expect<Equal<AllCombinations<'AB'>, '' | 'A' | 'B' | 'AB' | 'BA'>>,
-  Expect<Equal<AllCombinations<'ABC'>, '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'>>,
-  Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
+  Expect<Equal<NumberRange<2, 9>, Result1>>,
+  Expect<Equal<NumberRange<0, 2>, Result2>>,
+  Expect<Equal<NumberRange<0, 140>, Result3>>
 ]
 
-type StringToUnion<S extends string> = S extends `${infer F}${infer R}`
-? F | StringToUnion<R>
-: never
+type Range<N extends number, acc extends unknown[] = [], res = never> = acc['length'] extends N
+? res | acc['length']
+: Range<N, [...acc, unknown], res | acc['length']>
 
-type ArrayToString<A extends unknown[]> = A extends [infer F, ...infer R]
-? `${string &F}${ArrayToString<R>}`
-: ''
+type NumberRange<L, H> = L | Exclude<Range<number &H>, Range<number &L>>
 
-type Comb<C, path extends unknown[] = [], ans = never, A = C> = [C] extends [never]
-? ans | ArrayToString<path>
-: C extends any
-  ? Comb<Exclude<A, C>, [...path, C],  ans | ArrayToString<path>>
-  : never
-
-type AllCombinations<S> = Comb<StringToUnion<string &S>>
-
-type SU = StringToUnion<'AB'>
-type AS = ArrayToString<['A', 'B']>
-type test1 = AllCombinations<'AB'>
-type test2 = Comb<StringToUnion<'AB'>>
+type range = Range<10>

f17f7a0c534cf008fd76fd36cc849adaa4215f6e
Janessa <lvlajzhao1983@gmail.com>
2025-01-23 20:50:34 +0800

    redo all TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,16 +1,15 @@
 import {Expect, Equal, Alike, NotEqual} from "../test-utils";
 
-
-
-
-
-
 type cases = [
-  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
-  Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
-  Expect<Equal<Pop<[]>, []>>,
+  Expect<Equal<PublicType<{ a: number }>, { a: number }>>,
+  Expect<Equal<PublicType<{ _b: string | bigint }>, {}>>,
+  Expect<Equal<PublicType<{ readonly c?: number }>, { readonly c?: number }>>,
+  Expect<Equal<PublicType<{ d: string, _e: string }>, { d: string }>>,
+  Expect<Equal<PublicType<{ _f: () => bigint[] }>, {}>>,
+  Expect<Equal<PublicType<{ g: '_g' }>, { g: '_g' }>>,
+  Expect<Equal<PublicType<{ __h: number, i: unknown }>, { i: unknown }>>,
 ]
 
-type Pop<T extends any[]> = T extends [...infer R, infer L]
-? R
-: []
+type PublicType<T extends object> = {
+  [P in keyof T as P extends `_${any}` ? never : P]: T[P]
+}

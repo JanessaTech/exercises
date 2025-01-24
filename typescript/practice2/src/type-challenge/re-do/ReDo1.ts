db0e5c61fb3179c69fde8a30ce9cc184c8ad1b06
Janessa <lvlajzhao1983@gmail.com>
2025-01-24 14:50:32 +0800

    redo all TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,25 +1,19 @@
 import {Expect, Equal, Alike, NotEqual} from "../test-utils";
 
-interface Cat {
-  type: 'cat'
-  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
-}
-
-interface Dog {
-  type: 'dog'
-  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
-  color: 'brown' | 'white' | 'black'
-}
-
-type Animal = Cat | Dog
-
 type cases = [
-  Expect<Equal<LookUp<Animal, 'dog'>, Dog>>,
-  Expect<Equal<LookUp<Animal, 'cat'>, Cat>>,
+  Expect<Equal<Without<[1, 2], 1>, [2]>>,
+  Expect<Equal<Without<[1, 2, 4, 1, 5], [1, 2]>, [4, 5]>>,
+  Expect<Equal<Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]>, []>>,
 ]
 
-type LookUp<U extends {type : string}, T> = U extends any
-? U['type'] extends T
-  ? U
-  : never
-: never
+type Inlcude<L, E> = L extends [infer F, ...infer R]
+? Equal<E, F> extends true
+  ? true
+  : Inlcude<R, E>
+: false
+
+type Without<T, U, acc extends unknown[]= [], filter = U extends unknown[] ? U : [U]> = T extends [infer F, ...infer R]
+? Inlcude<filter, F> extends true
+  ? Without<R, U, acc>
+  : Without<R, U, [...acc, F]>
+: acc

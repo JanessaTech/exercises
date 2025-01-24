b8e7059fde8636bd93679bbb3a34d6907c65db2f
Janessa <lvlajzhao1983@gmail.com>
2025-01-24 10:38:19 +0800

    redo all TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,15 +1,22 @@
 import {Expect, Equal, Alike, NotEqual} from "../test-utils";
 
 type cases = [
-  Expect<Equal<TrimRight<'str'>, 'str'>>,
-  Expect<Equal<TrimRight<'str '>, 'str'>>,
-  Expect<Equal<TrimRight<'str     '>, 'str'>>,
-  Expect<Equal<TrimRight<'     str     '>, '     str'>>,
-  Expect<Equal<TrimRight<'   foo bar  \n\t '>, '   foo bar'>>,
-  Expect<Equal<TrimRight<''>, ''>>,
-  Expect<Equal<TrimRight<'\n\t '>, ''>>,
+  Expect<Equal<Trim<'str'>, 'str'>>,
+  Expect<Equal<Trim<' str'>, 'str'>>,
+  Expect<Equal<Trim<'     str'>, 'str'>>,
+  Expect<Equal<Trim<'str   '>, 'str'>>,
+  Expect<Equal<Trim<'     str     '>, 'str'>>,
+  Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
+  Expect<Equal<Trim<''>, ''>>,
+  Expect<Equal<Trim<' \n\t '>, ''>>,
 ]
 
-type TrimRight<S extends string> = S extends `${infer R}${' ' | '\n' | '\t'}`
+type TrimRight<S> = S extends `${infer R}${' ' | '\n' | '\t'}`
 ? TrimRight<R>
 : S
+
+type TrimLeft<S> =  S extends `${' ' | '\n' | '\t'}${infer R}`
+? TrimLeft<R>
+: S
+
+type Trim<S extends string> = TrimLeft<TrimRight<S>>

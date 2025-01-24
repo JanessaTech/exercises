e0e8fdff7124018b59139713f335202efe0d75e8
Janessa <lvlajzhao1983@gmail.com>
2025-01-24 15:12:49 +0800

    redo all TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,19 +1,20 @@
 import {Expect, Equal, Alike, NotEqual} from "../test-utils";
 
 type cases = [
-  Expect<Equal<Without<[1, 2], 1>, [2]>>,
-  Expect<Equal<Without<[1, 2, 4, 1, 5], [1, 2]>, [4, 5]>>,
-  Expect<Equal<Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]>, []>>,
+  Expect<Equal<Zip<[], []>, []>>,
+  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
 ]
 
-type Inlcude<L, E> = L extends [infer F, ...infer R]
-? Equal<E, F> extends true
-  ? true
-  : Inlcude<R, E>
-: false
-
-type Without<T, U, acc extends unknown[]= [], filter = U extends unknown[] ? U : [U]> = T extends [infer F, ...infer R]
-? Inlcude<filter, F> extends true
-  ? Without<R, U, acc>
-  : Without<R, U, [...acc, F]>
+type Zip<T, U, acc extends unknown[] = []> = T extends [infer T1, ...infer TR]
+? U extends [infer U1, ...infer UR]
+  ? Zip<TR, UR, [...acc, [T1, U1]]>
+  : acc
 : acc
+
+
+// type Zip<T, U, acc extends unknown[] = []> = [T, U] extends [[infer T1, ...infer TR], [infer U1, ...infer UR]]
+// ? Zip<TR, UR, [...acc, [T1, U1]]>
+// : acc

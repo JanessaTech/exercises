41bc566523f02e28c947d4f4e4131b776dc3b977
Janessa <lvlajzhao1983@gmail.com>
2025-02-08 18:24:54 +0800

    redo TS chanllenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,71 +1,14 @@
 import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
-type NodeA = {
-  type: 'A'
-  name: string
-  flag: number
-}
-
-type NodeB = {
-  type: 'B'
-  id: number
-  flag: number
-}
-
-type NodeC = {
-  type: 'C'
-  name: string
-  flag: number
-}
-
-type ReplacedNodeA = {
-  type: 'A'
-  name: number
-  flag: string
-}
-
-type ReplacedNodeB = {
-  type: 'B'
-  id: number
-  flag: string
-}
-
-type ReplacedNodeC = {
-  type: 'C'
-  name: number
-  flag: string
-}
-
-type NoNameNodeA = {
-  type: 'A'
-  flag: number
-  name: never
-}
-
-type NoNameNodeC = {
-  type: 'C'
-  flag: number
-  name: never
-}
-
-type Nodes = NodeA | NodeB | NodeC
-type ReplacedNodes = ReplacedNodeA | ReplacedNodeB | ReplacedNodeC
-type NodesNoName = NoNameNodeA | NoNameNodeC | NodeB
-
 type cases = [
-  Expect<Equal<ReplaceKeys<Nodes, 'name' | 'flag', { name: number, flag: string }>, ReplacedNodes>>,
-  Expect<Equal<ReplaceKeys<Nodes, 'name', { aa: number }>, NodesNoName>>,
+  Expect<Equal<StringToUnion<''>, never>>,
+  Expect<Equal<StringToUnion<'t'>, 't'>>,
+  Expect<Equal<StringToUnion<'hello'>, 'h' | 'e' | 'l' | 'l' | 'o'>>,
+  Expect<Equal<StringToUnion<'coronavirus'>, 'c' | 'o' | 'r' | 'o' | 'n' | 'a' | 'v' | 'i' | 'r' | 'u' | 's'>>,
 ]
 
-type ReplaceKeys<U, T, Y> = U extends any
-  ? {
-    [P in keyof U]: P extends T
-      ? P extends keyof Y
-        ? Y[P]
-        : never
-      : U[P]
-  }
-  : never
 
-
+type StringToUnion<T extends string> = T extends `${infer F}${infer R}`
+? F | StringToUnion<R>
+: never
 

893626206e951ba58f0a4cb3fa73e14c3f6d8583
Janessa <lvlajzhao1983@gmail.com>
2025-02-09 12:29:55 +0800

    redo TS chanllenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,23 +1,31 @@
 import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
 type cases = [
-  Expect<Equal<Trunc<0.1>, '0'>>,
-  Expect<Equal<Trunc<0.2>, '0'>>,
-  Expect<Equal<Trunc<1.234>, '1'>>,
-  Expect<Equal<Trunc<12.345>, '12'>>,
-  Expect<Equal<Trunc<-5.1>, '-5'>>,
-  Expect<Equal<Trunc<'.3'>, '0'>>,
-  Expect<Equal<Trunc<'1.234'>, '1'>>,
-  Expect<Equal<Trunc<'-.3'>, '-0'>>,
-  Expect<Equal<Trunc<'-10.234'>, '-10'>>,
-  Expect<Equal<Trunc<10>, '10'>>,
+  Expect<Equal<AllCombinations<''>, ''>>,
+  Expect<Equal<AllCombinations<'A'>, '' | 'A'>>,
+  Expect<Equal<AllCombinations<'AB'>, '' | 'A' | 'B' | 'AB' | 'BA'>>,
+  Expect<Equal<AllCombinations<'ABC'>, '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'>>,
+  Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
 ]
 
-type Trunc<T extends string | number> = `${T}` extends `${infer L}.${infer R}`
-? L extends '-'
-  ? '-0'
-  : L extends ''
-    ? '0'
-    : L
-: `${T}`
+type StringToUnion<S> = S extends `${infer F}${infer R}`
+? F | StringToUnion<R>
+: never
 
+type ArrayToString<A> = A extends any
+? A extends [infer F, ...infer R]
+  ? `${string &F}${ArrayToString<R>}`
+  : ''
+: never
+
+type Com<U, path extends unknown[] = [], acc = never, A = U> = [U] extends [never]
+  ? ArrayToString<acc | path>
+  : U extends any
+    ? Com<Exclude<A, U>, [...path, U], acc | path>
+    : never
+  
+
+type AllCombinations<S> = Com<StringToUnion<S>>
+
+type test = StringToUnion<'AB'>
+type test1 = Com<StringToUnion<'AB'>>

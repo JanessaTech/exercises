a0c9a8e69d4735e5dc408d692dd04d111fb8dc12
Janessa <lvlajzhao1983@gmail.com>
2025-02-13 11:07:58 +0800

    redo TS chanllenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,26 +1,30 @@
 import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
-type Foo = {
-  a: number
-  b: string
-}
-type Bar = {
-  b: number
-  c: boolean
-}
-
 type cases = [
-  Expect<Equal<Merge<Foo, Bar>, {
-    a: number
-    b: number
-    c: boolean
-  }>>,
+  Expect<Equal<AllCombinations<''>, ''>>,
+  Expect<Equal<AllCombinations<'A'>, '' | 'A'>>,
+  Expect<Equal<AllCombinations<'AB'>, '' | 'A' | 'B' | 'AB' | 'BA'>>,
+  Expect<Equal<AllCombinations<'ABC'>, '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'>>,
+  Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
 ]
 
-type Merge<F, S extends {[K in any]: any}> = {
-  [P in keyof(F & S)]: P extends keyof F
-    ? P extends keyof S 
-      ? S[P]
-      : F[P]
-    : S[P]
-}
+type StringToUnion<S> = S extends `${infer F}${infer R}`
+  ? F | StringToUnion<R>
+  : never
+
+type ArrayToString<A> = A extends any
+?  A extends [infer F, ...infer R]
+  ?  `${string &F}${ArrayToString<R>}`
+  : ''
+: never
+
+type Com<U, path extends unknown[] = [], acc = never, A = U> = [U] extends [never]
+? ArrayToString<acc | path>
+: U extends any
+  ? Com<Exclude<A, U>, [...path, U], acc | path>
+  : never
+
+type AllCombinations<S> =  Com<StringToUnion<S>>
+
+type test = StringToUnion<'AB'>
+type test1 = Com<StringToUnion<'AB'>>

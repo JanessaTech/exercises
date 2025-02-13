fd71ab55a4931df3eb22cc7eab08879ca6792f42
Janessa <lvlajzhao1983@gmail.com>
2025-02-13 11:30:28 +0800

    redo TS chanllenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,42 +1,23 @@
 import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
-type Foo = {
-  [key: string]: any
-  foo(): void
-}
-
-type Bar = {
-  [key: number]: any
-  bar(): void
-  0: string
-}
-
-const foobar = Symbol('foobar')
-type FooBar = {
-  [key: symbol]: any
-  [foobar](): void
-}
-
-type Baz = {
-  bar(): void
-  baz: string
-}
-
 type cases = [
-  Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
-  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void, 0: string }>>,
-  Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
-  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void, baz: string }>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
+  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
+  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
+  Expect<Equal<ReplaceAll<'', '', ''>, ''>>,
 ]
 
-type Filter<T> = string extends T
-? never
-: number extends T
-  ? never
-  : symbol extends T
-    ? never
-    : T
+type ReplaceAll<S extends string, From extends string, To extends string, acc extends string = ''> = From extends ''
+? S
+: S extends ''
+  ? acc
+  : S extends `${infer L}${From}${infer R}`
+    ? ReplaceAll<R, From, To, `${acc}${L}${To}`>
+    : ReplaceAll<'', From, To, `${acc}${S}`>
 
-type RemoveIndexSignature<T> = {
-  [P in keyof T as Filter<P>]: T[P]
-}
+type test = ReplaceAll<'foobar', 'bar', 'foo'>

17595a9e56ed1f991f611a85e5e303029c1de486
Janessa <lvlajzhao1983@gmail.com>
2025-02-13 16:14:09 +0800

    update UTs for EtherWallet

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 // import "hardhat/console.sol";
 
 contract Redo {
-    address payable owner;
+    address payable public owner;
 
     constructor() {
         owner = payable(msg.sender);
@@ -15,21 +15,17 @@ contract Redo {
         require(msg.sender == owner, 'Not owner');
         _;
     }
+
     function getBalance() public view returns(uint256) {
         return address(this).balance;
     }
 
-    function withdraw(uint256 _amount) public isOwner {
+    function withdraw(uint256 amount) public isOwner {
         uint256 balance = address(this).balance;
-        require(balance >= _amount, 'The _amount is too large');
-        bool sent = payable(address(msg.sender)).send(_amount);
+        require(balance > amount, 'The amount is too large');
+        bool sent = payable(address(msg.sender)).send(amount);
         require(sent, 'Failed to withdraw');
     }
 
-    function send() public payable{
-        bool sent = payable(address(this)).send(msg.value);
-        require(sent, 'Failed to send eth');
-    }
-    
-    receive() external payable{}
+    receive() external payable {}
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -1,3 +1,45 @@
 const {expect} = require("chai")
 const { ethers } = require("hardhat");
 const {loadFixture} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+
+describe('Redo', function () {
+    async function deployWalletFixture() {
+        const [account1, account2, ...others] = await ethers.getSigners()
+        const Redo = await ethers.getContractFactory('Redo', account2)
+        const redo = await Redo.deploy()
+        return {redo, account1, account2}
+    }
+
+    describe('withdraw', function () {
+        it('It should get correct balance', async function () {
+            const {redo, account1, account2} = await loadFixture(deployWalletFixture)
+            const tx = {
+                to: redo.getAddress(),
+                value: 3000
+            }
+            await account1.sendTransaction(tx)
+            const balance = await redo.getBalance()
+            expect(balance).to.equal(3000)
+        })
+        it('It should withdraw successfully', async function () {
+            const {redo, account1, account2} = await loadFixture(deployWalletFixture)
+            const tx = {
+                to: redo.getAddress(),
+                value : 3000
+            }
+            await account1.sendTransaction(tx)
+            await redo.withdraw(1000)
+            const balance = await redo.getBalance()
+            expect(balance).to.equal(2000)
+        })
+        it('It should fail to withdraw', async function () {
+            const {redo, account1, account2} = await loadFixture(deployWalletFixture)
+            const tx = {
+                to: redo.getAddress(),
+                value: 3000
+            }
+            await account1.sendTransaction(tx)
+            await expect(redo.connect(account1).withdraw(1000)).to.be.revertedWith('Not owner')
+        })
+    })
+})

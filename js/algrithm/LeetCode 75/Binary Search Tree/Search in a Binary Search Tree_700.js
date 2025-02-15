958b62663fd5dbea227f1c1051148c994e2a23d1
Janessa <lvlajzhao1983@gmail.com>
2025-02-15 20:57:48 +0800

    update solutions for 700

---
+++ js/algrithm/LeetCode 75/Binary Search Tree/Search in a Binary Search Tree_700.js
@@ -0,0 +1,6 @@
+var searchBST = function(root, val) {
+    if (root === null) return null
+    if (root.val === val) return root
+    else if (root.val > val) return searchBST(root.left, val)
+    return searchBST(root.right, val)
+};

ca95e3e4eddd86a1f7e8dd4275b87c74d7fc5e01
Janessa <lvlajzhao1983@gmail.com>
2025-02-15 16:37:40 +0800

    update solutions for 238

--- js/algrithm/LeetCode 75/Array_String/Product of Array Except Self_238.js
+++ js/algrithm/LeetCode 75/Array_String/Product of Array Except Self_238.js
@@ -2,12 +2,12 @@ var productExceptSelf = function(nums) {
     const res = Array(nums.length)
     res[0] = 1
     for (let i = 1; i < nums.length; i++) {
-        res[i] = nums[i - 1] * res[i - 1]
+        res[i] = res[i - 1] * nums[i - 1]
     }
     let right = 1
     for (let i = nums.length - 1; i >= 0; i--) {
-        res[i] = res[i] * right
-        right = right * nums[i]
+        res[i] *= right
+        right *= nums[i]
     }
     return res
 };

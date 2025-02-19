009a8a9d8e88041baad8427c0066d912474137a1
Janessa <lvlajzhao1983@gmail.com>
2025-02-19 10:13:43 +0800

    update solutin for 1129

--- js/algrithm/huahua130/Graph/Shortest_Path_with_Alternating_Colors_1129.js
+++ js/algrithm/huahua130/Graph/Shortest_Path_with_Alternating_Colors_1129.js
@@ -5,20 +5,20 @@
  * @return {number[]}
  */
 var shortestAlternatingPaths = function(n, redEdges, blueEdges) {
-    const digraph = createDigraph(n,  redEdges, blueEdges)
+    const digraph = createDigraph(n, redEdges, blueEdges)
     const dists = Array(n).fill(-1)
 
-    const queue = [[0, 1], [0, -1]]
+    const queue = [[0, -1], [0, 1]]
     let level = 0
 
     while (queue.length) {
         const size = queue.length
         for (let i = 0; i < size; i++) {
             const [cur, color] = queue.shift()
-            dists[cur] = dists[cur] === -1 ? level : dists[cur]
+            dists[cur] = dists[cur] === -1 ? level :dists[cur]
             for (let next of digraph[cur]) {
                 const [v, col, visited] = next
-                if (col === color || visited) continue
+                if (visited || col === color) continue
                 queue.push([v, -1 * color])
                 next[2] = true
             }

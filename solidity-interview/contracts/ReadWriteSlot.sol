f51a72e2aaa3792bacc073f0ea3f44c3cbed9a4d
Janessa <lvlajzhao1983@gmail.com>
2025-02-24 22:03:50 +0800

    redo ReadWriteSlot

--- solidity-interview/contracts/ReadWriteSlot.sol
+++ solidity-interview/contracts/ReadWriteSlot.sol
@@ -5,29 +5,25 @@ pragma solidity ^0.8.20;
 // import "hardhat/console.sol";
 
 library StorageLib {
-    struct AddressSlot {
+    struct StorageSlot {
         string value;
     }
 
-    function getAddressSlot(bytes32 slot)
-        internal
-        pure
-        returns(AddressSlot storage pointer) {
-            assembly {
-                pointer.slot := slot
-            }
+    function get(bytes32 slot) internal pure returns(StorageSlot storage pointer) {
+        assembly {
+            pointer.slot := slot
         }
-}
-
-contract ReadWriteSlot {
-
-    bytes32 constant test_slot = keccak256('hahaha');
-    function write(string memory _name) external {
-        StorageLib.AddressSlot storage data = StorageLib.getAddressSlot(test_slot);
-        data.value = _name;
     }
-    function read() external view returns(string memory) {
-        StorageLib.AddressSlot storage data = StorageLib.getAddressSlot(test_slot);
+}
+contract ReadWriteSlot {
+    bytes32 private constant test_slot  = keccak256("aaaaa");
+    function write(string memory name) public {
+        StorageLib.StorageSlot storage data = StorageLib.get(test_slot);
+        data.value = name;
+        
+    }
+    function read() public view returns(string memory) {
+        StorageLib.StorageSlot storage data = StorageLib.get(test_slot);
         return data.value;
     }
 }

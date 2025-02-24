d39e0bf52c8fa83de96796592261228de6c90bb0
Janessa <lvlajzhao1983@gmail.com>
2025-02-24 15:22:56 +0800

    update solution for 1268

--- js/algrithm/tmp/Test.js
+++ js/algrithm/tmp/Test.js
@@ -1,2 +1,10 @@
-const map = {}
-console.log(map[1])
+const list = [[3, 4], [1,3], [1, 2],  [2, 2]]
+list.sort((a, b) => {
+    if (a[0] === b[[0]]) {
+        return a[1] - b[1]
+    } else {
+        return a[0] - b[0]
+    }
+})
+
+console.log(list)

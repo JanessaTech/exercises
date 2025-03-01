b3a7f7a41e0b066be9052400fd12e33ae69d6854
Janessa <lvlajzhao1983@gmail.com>
2025-03-01 20:46:40 +0800

    redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,48 +1,70 @@
 import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
-declare const a: Chainable
-
-const result1 = a
-  .option('foo', 123)
-  .option('bar', { value: 'Hello World' })
-  .option('name', 'type-challenges')
-  .get()
-
-const result2 = a
-  .option('name', 'another name')
-  // @ts-expect-error
-  .option('name', 'last name')
-  .get()
-
-const result3 = a
-  .option('name', 'another name')
-  // @ts-expect-error
-  .option('name', 123)
-  .get()
-
 type cases = [
-  Expect<Alike<typeof result1, Expected1>>,
-  Expect<Alike<typeof result2, Expected2>>,
-  Expect<Alike<typeof result3, Expected3>>,
+  Expect<Equal<DeepReadonly<X1>, Expected1>>,
+  Expect<Equal<DeepReadonly<X2>, Expected2>>,
 ]
 
-type Expected1 = {
-  foo: number
-  bar: {
-    value: string
+type X1 = {
+  a: () => 22
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 'string'
+        }
+        k: 'hello'
+      }
+      l: [
+        'hi',
+        {
+          m: ['hey']
+        },
+      ]
+    }
   }
-  name: string
 }
 
-type Expected2 = {
-  name: string
+type X2 = { a: string } | { b: number }
+
+type Expected1 = {
+  readonly a: () => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 'string'
+        }
+        readonly k: 'hello'
+      }
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey']
+        },
+      ]
+    }
+  }
 }
 
-type Expected3 = {
-  name: number
-}
+type Expected2 = { readonly a: string } | { readonly b: number }
 
-type Chainable<O = {}> = {
-  option<K extends string, V>(key: K extends keyof O ? never : K, value: V): Chainable<Omit<O, K> & {[P in K]: V}>
-  get(): O
-}
+type DeepArray<A, acc extends unknown[] = []> = A extends [infer F, ...infer R]
+  ? DeepArray<R, [...acc, DeepReadonly<F>]>
+  : acc
+
+type DeepReadonly<T> = T extends any
+  ? T extends {[P in any]: unknown}
+    ? {readonly [K in keyof T]: DeepReadonly<T[K]>}
+    : T extends unknown[]
+      ? readonly [...DeepArray<T>]
+      : T
+  : never
+
+  type test = DeepReadonly<X1>

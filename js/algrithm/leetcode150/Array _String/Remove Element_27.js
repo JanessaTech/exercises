f7c3264c7c3e8d1ae9322f0cc77560589b16dbc2
Janessa <lvlajzhao1983@gmail.com>
2025-03-06 19:55:50 +0800

    update solution for 27

---
+++ js/algrithm/leetcode150/Array _String/Remove Element_27.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let write = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[write++] = nums[i]
+        }
+    }
+    return write
+};

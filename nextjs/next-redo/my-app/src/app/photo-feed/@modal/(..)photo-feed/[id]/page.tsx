46c05309bad2f2bb80072ad8a0e6a514a86045ae
Janessa <lvlajzhao1983@gmail.com>
2025-03-17 09:54:33 +0800

    update photo-feef

--- nextjs/next-redo/my-app/src/app/photo-feed/@modal/(..)photo-feed/[id]/page.tsx
+++ nextjs/next-redo/my-app/src/app/photo-feed/@modal/(..)photo-feed/[id]/page.tsx
@@ -1,36 +1,19 @@
 'use client'
 
-import {  Dialog,  
-    DialogContent,  
-    DialogTrigger,
-} from "@/components/ui/dialog"
 import wonderImages  from "@/app/photo-feed/wonders";
 import type {WonderImage} from "@/app/photo-feed/wonders";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { MyModal } from "@/components/MyModal";
 
 
 export default function InterceptedPhoto({params: {id}}: {params: {id: string}}) {
     const photo: WonderImage = wonderImages.find((p) => p.id === id)!
-    const router = useRouter()
 
-    const handOpenChange = (open: boolean) => {
-        console.log('handOpenChange:', open)
-        if (!open) {
-            router.back()  // backforward when the dialog is closed
-        }
-        
-    }
     return (
         <div>
-            <div>
-                <Dialog onOpenChange={handOpenChange}>  
-                    <DialogTrigger>Show</DialogTrigger>  
-                    <DialogContent>    
-                        <Image src={photo.src} alt={photo.name}></Image>
-                    </DialogContent>
-                </Dialog>
-            </div>
+            <MyModal>
+                <Image src={photo.src} alt={photo.name}></Image>
+            </MyModal>
         </div>
        
     )

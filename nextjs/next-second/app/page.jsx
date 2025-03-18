9ee1a3b0e8d7c00b12029cfea97ead5ea99d068f
Janessa <lvlajzhao1983@gmail.com>
2025-03-18 08:48:30 +0800

    move codes

--- nextjs/next-second/app/page.jsx
+++ nextjs/next-second/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h2>Dashboard</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p>
+      <p className="text-orange-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p>
 
       <div className="flex justify-center my-8">
         <Link href="/tickets">

6c02facd4174092e45393966921ecdd24a4517de
Janessa <lvlajzhao1983@gmail.com>
2025-03-21 19:34:23 +0800

    fix bug

--- blockchain/my-dpps-home/dapp-challenges/next-voting/voting-dapp/src/app/home/page.tsx
+++ blockchain/my-dpps-home/dapp-challenges/next-voting/voting-dapp/src/app/home/page.tsx
@@ -52,7 +52,7 @@ const HomePage:React.FC<HomeProps> = () => {
                 router.push('/')
             }
         })()
-    }, [])
+    }, [contract])
 
     const logout = async (e:React.MouseEvent<HTMLButtonElement>) => {
         await disConnectWallet()
@@ -94,30 +94,22 @@ const HomePage:React.FC<HomeProps> = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
-                        <TableCell className="font-medium">0</TableCell>
-                        <TableCell>Bob</TableCell>
-                        <TableCell>1234566767</TableCell>
-                        <TableCell className="text-right">
-                            <button className="bg-green-700 hover:bg-green-600 text-white py-1 px-2 rounded-full">vote</button>
-                        </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell className="font-medium">1</TableCell>
-                        <TableCell>Alice</TableCell>
-                        <TableCell>1234566767</TableCell>
-                        <TableCell className="text-right">
-                            <button className="bg-green-700 hover:bg-green-600 text-white py-1 px-2 rounded-full">vote</button>
-                        </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell className="font-medium">2</TableCell>
-                        <TableCell>Janessa</TableCell>
-                        <TableCell>1234566767</TableCell>
-                        <TableCell className="text-right">
-                            <button className="bg-green-700 hover:bg-green-600 text-white py-1 px-2 rounded-full">vote</button>
-                        </TableCell>
-                    </TableRow>
+                    {
+                        state.candidates.map((candidate) => {
+                            return (
+                                <TableRow key={candidate.id}>
+                                    <TableCell className="font-medium">{candidate.id}</TableCell>
+                                    <TableCell>{candidate.name}</TableCell>
+                                    <TableCell>{candidate.votedBy}</TableCell>
+                                    <TableCell className="text-right">
+                                        <button className={`hover:bg-green-60 disabled:hover:bg-zinc-400 
+                                                        text-white py-1 px-2 rounded-full ${candidate.votedBy ? 'bg-zinc-500' : 'bg-green-700'}`} 
+                                                disabled={!!candidate.votedBy}>vote</button>
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        })
+                    }
                 </TableBody>
             </Table>
             </div>

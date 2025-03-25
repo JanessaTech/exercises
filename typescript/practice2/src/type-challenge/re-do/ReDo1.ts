cf5396ba76f8fbc65ad3915dcea18059a948cf6b
Janessa <lvlajzhao1983@gmail.com>
2025-03-25 11:00:46 +0800

    redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,50 +1,26 @@
 import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
-declare const a: Chainable
-
-const result1 = a
-  .option('foo', 123)
-  .option('bar', { value: 'Hello World' })
-  .option('name', 'type-challenges')
-  .get()
-
-const result2 = a
-  .option('name', 'another name')
-  // @ts-expect-error
-  .option('name', 'last name')
-  .get()
-
-const result3 = a
-  .option('name', 'another name')
-  // @ts-expect-error
-  .option('name', 123)
-  .get()
-
 type cases = [
-  Expect<Alike<typeof result1, Expected1>>,
-  Expect<Alike<typeof result2, Expected2>>,
-  Expect<Alike<typeof result3, Expected3>>,
+  Expect<Equal<MapTypes<{ stringToArray: string }, { mapFrom: string, mapTo: [] }>, { stringToArray: [] }>>,
+  Expect<Equal<MapTypes<{ stringToNumber: string }, { mapFrom: string, mapTo: number }>, { stringToNumber: number }>>,
+  Expect<Equal<MapTypes<{ stringToNumber: string, skipParsingMe: boolean }, { mapFrom: string, mapTo: number }>, { stringToNumber: number, skipParsingMe: boolean }>>,
+  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date } | { mapFrom: string, mapTo: null }>, { date: null | Date }>>,
+  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date | null }>, { date: null | Date }>>,
+  Expect<Equal<MapTypes<{ fields: Record<string, boolean> }, { mapFrom: Record<string, boolean>, mapTo: string[] }>, { fields: string[] }>>,
+  Expect<Equal<MapTypes<{ name: string }, { mapFrom: boolean, mapTo: never }>, { name: string }>>,
+  Expect<Equal<MapTypes<{ name: string, date: Date }, { mapFrom: string, mapTo: boolean } | { mapFrom: Date, mapTo: string }>, { name: boolean, date: string }>>,
 ]
 
-type Expected1 = {
-  foo: number
-  bar: {
-    value: string
-  }
-  name: string
-}
-
-type Expected2 = {
-  name: string
-}
-
-type Expected3 = {
-  name: number
-}
 
 
-type Chainable<O = {}> = {
-  option<K extends string, V>(key: K extends keyof O ? never : K, value: V): Chainable<Omit<O, K> & {[P in K]: V}>
-  get(): O
+type MapTypes<T, R extends {[K in 'mapFrom' | 'mapTo'] : any}> = {
+  [P in keyof T] : T[P] extends R['mapFrom']
+    ? R extends any
+      ? R extends {mapFrom: T[P]}
+        ? R['mapTo']
+        : never
+      : never
+    : T[P]
 }
+
 

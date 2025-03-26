aa51860b627bbfdacd06beb283284fdaf46a546b
Janessa <lvlajzhao1983@gmail.com>
2025-03-26 11:48:20 +0800

    redo solidity challenge

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -6,28 +6,41 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "hardhat/console.sol";
 
 contract Redo {
-   IERC20 token;
-   uint price;
-   uint quantity;
-   address owner;
+   struct Person {
+      uint id;
+      string name;
+   }
+   Person[] persons;
+   uint idx;
+   mapping(uint => uint) idxMapping;
+   mapping(uint => bool) inserted;
 
-   constructor(address _token, uint _price, uint _quantity, address _owner) {
-      token = IERC20(_token);
-      price = _price;
-      quantity = _quantity;
-      owner = _owner;
+   function create(string memory _name) public {
+      uint _id = idx;
+      idx++;
+      persons.push(Person({id: _id, name:_name}));
+      idxMapping[_id] = persons.length - 1;
+      inserted[_id] = true;
    }
 
-   function buy(uint _quantity) public payable {
-      uint needed = price * _quantity;
-      require(msg.value >= needed, 'not enough eth');
-      require(token.allowance(owner, address(this)) >= _quantity, 'not enough allowance');
-      uint refund = msg.value - needed;
-      bool sent = payable(msg.sender).send(refund);
-      require(sent, 'failed to refund');
-      bool success = token.transferFrom(owner, msg.sender, _quantity);
-      require(success, 'failed to transfer token to buyer');
-      bool done = payable(owner).send(needed);
-      require(done, 'failed to pay owner');
+   function remove(uint _id) public {
+      require(inserted[_id], 'invalid id');
+      uint _idx = idxMapping[_id];
+      Person storage last = persons[persons.length - 1];
+      persons[_idx] = Person({id: last.id, name: last.name});
+      idxMapping[last.id] = _idx;
+      delete idxMapping[_id];
+      delete inserted[_id];
+      persons.pop();
+   }
+
+   function get(uint _id) public view returns(uint id, string memory name) {
+      require(inserted[_id], 'invalid id');
+      Person storage person = persons[idxMapping[_id]];
+      return (person.id, person.name);
+   }
+
+   function getAll() public view returns(Person[] memory) {
+      return persons;
    }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -5,58 +5,35 @@ const {loadFixture, time} = require("@nomicfoundation/hardhat-toolbox/network-he
 
 describe('Redo', function () {
     async function deployRedoFixture() {
-        const [deployer1, deployer2, Bob, Alice, ...others] = await ethers.getSigners()
-        const MyERC20 = await ethers.getContractFactory('MyERC20', deployer1)
-        const name = 'MyERC20'
-        const symbol = 'MyERC20'
-        const erc20 = await MyERC20.deploy(name, symbol)
-        const price = 10
-        const quantity = 1000
-        await erc20.transfer(Bob.getAddress(), quantity)
-
-        const Redo = await ethers.getContractFactory('Redo', deployer2)
-        const redo = await Redo.deploy(erc20.getAddress(), price, quantity, Bob.getAddress())
-
-        await erc20.connect(Bob).approve(redo.getAddress(), quantity)
-        return {redo, erc20, price, quantity, Bob, Alice}
+        const Redo = await ethers.getContractFactory('Redo')
+        const redo = await Redo.deploy()
+        return {redo}
     }
-
-    describe('init', function () {
-        it('init', async function () {
-            const {redo, erc20, Bob, quantity} = await loadFixture(deployRedoFixture)
-            const balance = await erc20.balanceOf(Bob.getAddress())
-            const allowance = await erc20.allowance(Bob.getAddress(), redo.getAddress())
-
-            expect(balance).to.be.equal(quantity)
-            expect(allowance).to.be.equal(quantity)
+    describe('create', function () {
+        it('create', async function () {
+            const {redo} = await loadFixture(deployRedoFixture)
+            await redo.create('person0')
+            const person0 = await redo.get(0)
+            expect(person0.name).to.be.equal('person0')
         })
     })
-    describe('buy', function() {
-        it('it failed to buy when the eth is not enough', async function () {
-            const {redo, Alice, price} = await loadFixture(deployRedoFixture)
-            const quality = 10
-            const amount = 50
-            await expect(redo.connect(Alice).buy(quality, {value: amount})).to.be.revertedWith('not enough eth')
+    describe('remove', function () {
+        it('it failed to remove the person when the id is invalid', async function () {
+            const {redo} = await loadFixture(deployRedoFixture)
+            await expect(redo.remove(0)).to.be.revertedWith('invalid id')
         })
-        it('it failed to buy when the allowance is not enough', async function () {
-            const {redo, price, Alice} = await loadFixture(deployRedoFixture)
-            const quantity = 1001
-            const amount = quantity * price
-            await expect(redo.connect(Alice).buy(quantity, {value: amount})).to.be.revertedWith('not enough allowance')
+        it('it removed the person successfully', async function () {
+            const {redo} = await loadFixture(deployRedoFixture)
+            await redo.create('person0')
+            await redo.create('person1')
+            await redo.create('person2')
+            await redo.remove(1)
 
-        })
-        it('it bought the token successfully', async function () {
-            const {redo, Alice, Bob, quantity, price, erc20} = await loadFixture(deployRedoFixture)
-            const _quantity = 20
-            const amount = _quantity * price
-            await redo.connect(Alice).buy(_quantity, {value: amount})
-            const balanceAlice = await erc20.balanceOf(Alice.getAddress())
-            const balanceBob = await erc20.balanceOf(Bob.getAddress())
-            const allowance = await erc20.allowance(Bob.getAddress(), redo.getAddress())
-
-            expect(balanceAlice).to.be.equal(_quantity)
-            expect(balanceBob).to.be.equal(quantity - _quantity)
-            expect(allowance).to.be.equal(quantity - _quantity)
+            const person0 = await redo.get(0)
+            const person2 = await redo.get(2)
+            await expect(redo.get(1)).to.be.revertedWith('invalid id')
+            expect(person0.name).to.be.equal('person0')
+            expect(person2.name).to.be.equal('person2')
         })
     })
 })

0f40d62a5aa374a237d9ac2865739f2771e54b5e
Janessa <lvlajzhao1983@gmail.com>
2025-04-03 08:11:01 +0800

    clean redo challenge

--- blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/page.tsx
+++ blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/page.tsx
@@ -1,20 +1,11 @@
-'use client'
 
-import { IWeb3Context, useWeb3Context } from "@/components/providers/Web3ContextProvider"
-import { useRouter } from "next/navigation"
 
 const Login = () => {
-  const router = useRouter()
-  const {connectWallet} = useWeb3Context() as IWeb3Context
 
-  const onClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    await connectWallet()
-    router.push('/home')
-  }
 
   return (
     <div>
-      <button className="bg-zinc-600 rounded-3xl px-3 py-2" onClick={onClick}>Collect wallet </button>
+      <button className="bg-zinc-600 rounded-3xl px-3 py-2">Collect wallet </button>
     </div>
   )
 }

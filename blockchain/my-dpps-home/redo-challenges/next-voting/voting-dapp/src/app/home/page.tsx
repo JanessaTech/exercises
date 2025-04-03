17047a879675038f02dcd22ef0d5fab1458a4c1c
Janessa <lvlajzhao1983@gmail.com>
2025-04-03 09:12:17 +0800

    redo challenge

--- blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/home/page.tsx
+++ blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/home/page.tsx
@@ -1,11 +1,58 @@
 'use client'
 
 import { IWeb3Context, useWeb3Context } from "@/components/providers/Web3ContextProvider"
+import { AuthState, authState } from "@/lib/Atoms"
+import { Contract } from "ethers"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import { useRecoilState } from "recoil"
 
+type CandidateType = {
+    id: number;
+    name: string;
+    votedBy: string
+}
+type HomeStateType = {
+    isEnded: boolean;
+    candidates: CandidateType[]
+}
+
+const defaultHomeState: HomeStateType = {
+    isEnded: false,
+    candidates: []
+}
 
 type HomeProps = {}
 const Home:React.FC<HomeProps> = () => {
     const {connectWallet, disconnectWallet, state: {address, contract}} = useWeb3Context() as IWeb3Context
+    const [auth, setAuth] = useRecoilState<AuthState>(authState)
+    const router = useRouter()
+    const [state, setState] = useState<HomeStateType>(defaultHomeState)
+
+    useEffect(() => {
+        (async () => {
+            if (auth.connected) {
+                if (contract) {
+                    await updateState(contract)
+                } else {
+                    await connectWallet()
+                }
+            } else {
+                router.push('/')
+            }
+        })()
+    }, [contract])
+
+    const updateState = async (contract: Contract) => {
+        const rawCandidates: {[P in any]: any}[] = await contract.getCandidates()
+        const rows: CandidateType[] = []
+        rawCandidates.forEach((e) => {
+            rows.push({id: Number(e[0]), name: e[1], votedBy: e[2]})
+        })
+        const isEnded = await contract.isEnded()
+        setState({isEnded: isEnded, candidates: rows})
+    }
+    console.log(state)
     return (
         <div>
             <div>address: {address}</div>

6d3e2fa6b996a9c40fefd255d93d61e179ea9189
Janessa <lvlajzhao1983@gmail.com>
2025-04-05 11:00:07 +0800

    update solution for 994

--- js/algrithm/LeetCode 75/Graphs - BFS/Rotting Oranges_994.js
+++ js/algrithm/LeetCode 75/Graphs - BFS/Rotting Oranges_994.js
@@ -10,38 +10,36 @@ var orangesRotting = function(grid) {
     for (let i = 0; i < m; i++) {
         visited.push(Array(n).fill(false))
     }
-
     const queue = []
-    let nonempty = 0
+    let goods = 0
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (grid[i][j] === 2) {
                 queue.push([i, j])
                 visited[i][j] = true
-                nonempty++
             }
-            if (grid[i][j] === 1) nonempty++
+            if (grid[i][j] === 1) goods++
         }
     }
 
+    let rotted = 0
     let minutes = 0
-    let cnt = 0
     while (queue.length) {
         const size = queue.length
         for (let i = 0; i < size; i++) {
             const [row, col] = queue.shift()
-            cnt++
             for (let dir of dirs) {
                 const nr = row + dir[0]
                 const nc = col + dir[1]
                 if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc] && grid[nr][nc] === 1) {
                     queue.push([nr, nc])
                     visited[nr][nc] = true
+                    rotted++
                 }
             }
         }
         minutes++
     }
 
-    return cnt === nonempty ? Math.max(minutes - 1, 0) : -1
+    return rotted === goods ? Math.max(minutes - 1, 0) : -1
 };

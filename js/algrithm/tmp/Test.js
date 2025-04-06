9c47faea835318ba1c51ec7f41de283cd9379dd4
Janessa <lvlajzhao1983@gmail.com>
2025-04-06 21:16:06 +0800

    redo leetcode challenge

--- js/algrithm/tmp/Test.js
+++ js/algrithm/tmp/Test.js
@@ -1,5 +1,18 @@
 
 
-const p = ['bc', 'ba']
-p.sort()
-console.log(p)
+const search = (nums, target) => {
+    let lo = 0, hi = nums.length - 1
+    while (lo < hi) {
+        const mid = Math.floor((lo + hi) / 2)
+        if (target <= nums[mid]) {
+            hi = mid
+        } else {
+            lo = mid + 1
+        }
+    }
+    return nums[lo]
+}
+
+const nums = [4, 5, 6, 9]
+const res = search(nums, 8.9)
+console.log(res)

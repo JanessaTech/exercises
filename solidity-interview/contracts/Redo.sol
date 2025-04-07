76a8d7905e6f3ed03b710ada5f61b7ae38697f77
Janessa <lvlajzhao1983@gmail.com>
2025-04-07 09:40:55 +0800

    redo solidty challenge

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -16,26 +16,15 @@ contract Redo {
     mapping(uint => bool) inserted;
 
     function create(string memory _name) public {
-        uint _id = idx;
-        idx++;
-        persons.push(Person({id: _id, name: _name}));
-        idxMapping[_id] = persons.length - 1;
-        inserted[_id] = true;
+
     }
     function remove(uint _id) public {
-        require(inserted[_id], "invalid id");
-        uint _idx = idxMapping[_id];
-        Person storage last = persons[persons.length - 1];
-        persons[_idx] = Person({id: last.id, name: last.name});
-        idxMapping[last.id] = _idx;
-        delete idxMapping[_id];
-        delete inserted[_id];
-        persons.pop();
-    }
 
-    function get(uint _id) public view returns (uint id, string memory name) {
-        require(inserted[_id], "invalid id");
-        Person storage person = persons[idxMapping[_id]];
-        return (person.id, person.name);
+    }
+    function get(uint _id) public view returns(uint id, string memory name) {
+
+    }
+    function getAll() public view returns(Person[] memory) {
+        return persons;
     }
 }

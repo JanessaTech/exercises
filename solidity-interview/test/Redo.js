cf6c3cee010399ce47192f2ee5f6f1264d4a9b14
Janessa <lvlajzhao1983@gmail.com>
2025-04-07 10:18:27 +0800

    redo solidty challenge

--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -31,7 +31,16 @@ describe('Redo', function () {
             await expect(redo.remove(1)).to.be.revertedWith('invalid id')
         })
         it('it removed the person by id successfully', async function () {
-
+            const {redo} = await loadFixture(deployRedoFixture)
+            await redo.create('person0')
+            await redo.create('person1')
+            await redo.create('person2')
+            await redo.remove(1)
+            const person0 = await redo.get(0)
+            const person2 = await redo.get(2)
+            await expect(redo.get(1)).to.be.revertedWith('invalid id')
+            expect(person0.name).to.to.equal('person0')
+            expect(person2.name).to.to.equal('person2')
         })
     })
 })

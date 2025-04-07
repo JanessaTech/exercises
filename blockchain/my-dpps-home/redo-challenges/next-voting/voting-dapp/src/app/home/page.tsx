6d5036ed89a2de12549740f1bee036d15b29e896
Janessa <lvlajzhao1983@gmail.com>
2025-04-07 09:37:40 +0800

    redo voting challenge

--- blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/home/page.tsx
+++ blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/home/page.tsx
@@ -4,13 +4,28 @@ import { IWeb3Context, useWeb3Context } from "@/components/providers/Web3Context
 import { AuthState, authState } from "@/lib/Atoms"
 import { Contract } from "ethers"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useRecoilState } from "recoil"
+
+type CandidateType = {
+    id: number,
+    name: string;
+    votedBy: string
+}
+type HomeState = {
+    isEnded: boolean
+    candidates: CandidateType[]
+}
+const defaultHomeState: HomeState = {
+    isEnded: false,
+    candidates: []
+}
 
 type HomeProps = {}
 const Home: React.FC<HomeProps> = () => {
     const {connectWallet, disconnectWallet, state: {address, contract}} = useWeb3Context() as IWeb3Context
     const [auth, setAuth] = useRecoilState<AuthState>(authState)
+    const [state, setState] = useState<HomeState>(defaultHomeState)
     const router = useRouter()
     useEffect(() => {
         (async () => {
@@ -27,8 +42,15 @@ const Home: React.FC<HomeProps> = () => {
     }, [contract])
 
     const updateState = async (contract: Contract) => {
-
+        const rawCandidates: any[] = await contract.getCandidates()
+        const rows: CandidateType[] = []
+        rawCandidates.forEach((e) => {
+            rows.push({id: Number(e[0]), name: e[1], votedBy: e[2]})
+        })
+        const isEnded = await contract.isEnded()
+        setState({isEnded: isEnded, candidates: rows})
     }
+    console.log(state)
     return (
         <div>
             <div>address: {address}</div>

2e31c84a154dd156934f15a071b1f4937ba522b0
Janessa <lvlajzhao1983@gmail.com>
2025-04-10 22:36:12 +0800

    redo solidity challenge

--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -1,8 +1,7 @@
 const {expect} = require("chai")
 const { ethers } = require("hardhat");
 const {loadFixture, time} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { boolean } = require("hardhat/internal/core/params/argumentTypes");
-const { extendEnvironment } = require("hardhat/config");
+
 
 
 describe('Redo', function () {

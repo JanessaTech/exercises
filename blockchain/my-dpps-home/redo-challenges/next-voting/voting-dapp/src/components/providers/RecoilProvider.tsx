d62a955d33d2d8ea44d9e95e89a9dc51355ef03c
Janessa <lvlajzhao1983@gmail.com>
2025-04-22 23:01:32 +0800

    redo voting challenge

---
+++ blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/components/providers/RecoilProvider.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { RecoilRoot } from "recoil"
+
+type RecoilProviderProps = {
+    children: React.ReactNode
+}
+const RecoilProvider:React.FC<RecoilProviderProps> = ({children}) => {
+    return (
+        <RecoilRoot>
+            {children}
+        </RecoilRoot>
+    )
+}
+
+export default RecoilProvider

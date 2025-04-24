861965662d73106ddcfb64f634fb21089923cdb3
Janessa <lvlajzhao1983@gmail.com>
2025-04-24 21:50:20 +0800

    redo tailwind challenge

--- css+div/tailwind-redo/my-app/src/app/page.tsx
+++ css+div/tailwind-redo/my-app/src/app/page.tsx
@@ -15,7 +15,42 @@ export default function Home() {
 
   return (
     <div className="container">
-      
+      <div className="grid md:grid-cols-3 gap-5">
+        <div className="md:col-span-1 w-full h-72 bg-yellow-100">
+          <div className="flex justify-between items-center">
+            <div className="py-3 border-b-[1px] border-zinc-200"><span className="text-3xl uppercase">Janessa Food</span></div>
+            <svg xmlns="http://www.w3.org/2000/svg" 
+              className="w-8 cursor-pointer hover:text-zinc-500 pr-1 md:hidden"
+              fill="none" 
+              viewBox="0 0 24 24" 
+              strokeWidth="1.5" 
+              stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </div>
+          <ul>
+            <li className="group cursor-pointer my-3" onClick={() => setSelect(0)}>
+              <div className={`flex justify-end border-r-4 
+                group-hover:text-red-500 group-hover:border-red-500 ${select === 0 ? 'border-red-500 text-red-500' : 'border-white '}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+                <span className="px-2">Home</span>
+              </div>
+            </li>
+            <li className="group cursor-pointer my-3" onClick={() => setSelect(1)}>
+              <div className={`flex justify-end border-r-4 
+                group-hover:text-red-500 group-hover:border-red-500 ${select === 1 ? 'border-red-500 text-red-500' : 'border-white '}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+                <span className="px-2">Home</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="md:col-span-2 w-full h-60 bg-green-100"></div>
+      </div>
     </div>
   )
 }

3c5270a9f0476cc881eb53f234fa5dd1458cb26b
Janessa <lvlajzhao1983@gmail.com>
2025-04-26 21:27:25 +0800

    redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,10 +1,16 @@
 import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
 type cases = [
-  Expect<Equal<First<[3, 2, 1]>, 3>>,
-  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
-  Expect<Equal<First<[]>, never>>,
-  Expect<Equal<First<[undefined]>, undefined>>,
+  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<If<false, 'a', 2>, 2>>,
+  Expect<Equal<If<boolean, 'a', 2>, 'a' | 2>>,
 ]
 
-type First<T extends any[]> = T extends [infer F, ...infer R] ? F : never
+// @ts-expect-error
+type error = If<null, 'a', 'b'>
+
+type If<C extends boolean, T, F> = C extends any
+? C extends true 
+  ? T
+  : F
+: never

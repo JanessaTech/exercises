9623da105979e882616b6b77eb9b7c3dafc9c98f
Janessa <lvlajzhao1983@gmail.com>
2025-04-26 22:11:38 +0800

    redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -2,18 +2,25 @@ import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
 
 type cases = [
-  Expect<Equal<Absolute<0>, '0'>>,
-  Expect<Equal<Absolute<-0>, '0'>>,
-  Expect<Equal<Absolute<10>, '10'>>,
-  Expect<Equal<Absolute<-5>, '5'>>,
-  Expect<Equal<Absolute<'0'>, '0'>>,
-  Expect<Equal<Absolute<'-0'>, '0'>>,
-  Expect<Equal<Absolute<'10'>, '10'>>,
-  Expect<Equal<Absolute<'-5'>, '5'>>,
-  Expect<Equal<Absolute<-1_000_000n>, '1000000'>>,
-  Expect<Equal<Absolute<9_999n>, '9999'>>,
+  Expect<Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[1, '', false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, 'test', false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', true, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [1], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { 1: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], {}, undefined, null]>, false>>,
+  Expect<Equal<AnyOf<[]>, false>>,
 ]
 
-type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer N}`
-? N
-: `${T}`
+type falsy = [0, '', false, [], {[P in any]: never}, undefined, null]
+
+
+type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
+? F extends falsy[number]
+  ? AnyOf<R>
+  : true
+: false
+
+type test = AnyOf<[0, 'test', false, [], {}]>

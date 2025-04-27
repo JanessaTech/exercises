a6ee21b516f6d4d5ee65bddfa43099f3b45ab74b
Janessa <lvlajzhao1983@gmail.com>
2025-04-27 22:06:14 +0800

    redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,49 +1,23 @@
 import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
 
-declare const a: Chainable
-
-const result1 = a
-  .option('foo', 123)
-  .option('bar', { value: 'Hello World' })
-  .option('name', 'type-challenges')
-  .get()
-
-const result2 = a
-  .option('name', 'another name')
-  // @ts-expect-error
-  .option('name', 'last name')
-  .get()
-
-const result3 = a
-  .option('name', 'another name')
-  // @ts-expect-error
-  .option('name', 123)
-  .get()
-
 type cases = [
-  Expect<Alike<typeof result1, Expected1>>,
-  Expect<Alike<typeof result2, Expected2>>,
-  Expect<Alike<typeof result3, Expected3>>,
+  Expect<Equal<CheckRepeatedTuple<[number, number, string, boolean]>, true>>,
+  Expect<Equal<CheckRepeatedTuple<[number, string]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[1, 2, 3]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[1, 2, 1]>, true>>,
+  Expect<Equal<CheckRepeatedTuple<[]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<string[]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[number, 1, string, '1', boolean, true, false, unknown, any]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[never, any, never]>, true>>,
 ]
-
-type Expected1 = {
-  foo: number
-  bar: {
-    value: string
-  }
-  name: string
-}
-
-type Expected2 = {
-  name: string
-}
-
-type Expected3 = {
-  name: number
-}
-
-type Chainable<O = {}> = {
-  option<K extends string, V>(key: K extends keyof O ? never : K, value: V): Chainable<Omit<O, K> & {[P in K]: V}>
-  get(): O
-}
+type Include<E, L> = L extends [infer F, ...infer R]
+? Equal<E, F> extends true
+  ? true
+  : Include<E, R>
+: false
+type CheckRepeatedTuple<T extends unknown[]> = T extends [infer F, ...infer R]
+? Include<F, R> extends true
+  ? true
+  : CheckRepeatedTuple<R>
+: false

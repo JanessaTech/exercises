ee1d77c9ec9be2dc1ca9a925e907c903fe150658
Janessa <lvlajzhao1983@gmail.com>
2025-04-27 21:56:08 +0800

    redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -1,26 +1,49 @@
 import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
 
+declare const a: Chainable
+
+const result1 = a
+  .option('foo', 123)
+  .option('bar', { value: 'Hello World' })
+  .option('name', 'type-challenges')
+  .get()
+
+const result2 = a
+  .option('name', 'another name')
+  // @ts-expect-error
+  .option('name', 'last name')
+  .get()
+
+const result3 = a
+  .option('name', 'another name')
+  // @ts-expect-error
+  .option('name', 123)
+  .get()
+
 type cases = [
-  Expect<Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[1, '', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, 'test', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', true, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [1], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], {}, undefined, null]>, false>>,
-  Expect<Equal<AnyOf<[]>, false>>,
+  Expect<Alike<typeof result1, Expected1>>,
+  Expect<Alike<typeof result2, Expected2>>,
+  Expect<Alike<typeof result3, Expected3>>,
 ]
 
-type falsy = [0, '', false, [], {[P in any]: never}, undefined, null]
+type Expected1 = {
+  foo: number
+  bar: {
+    value: string
+  }
+  name: string
+}
 
+type Expected2 = {
+  name: string
+}
 
-type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
-? F extends falsy[number]
-  ? AnyOf<R>
-  : true
-: false
+type Expected3 = {
+  name: number
+}
 
-type test = AnyOf<[0, 'test', false, [], {}]>
+type Chainable<O = {}> = {
+  option<K extends string, V>(key: K extends keyof O ? never : K, value: V): Chainable<Omit<O, K> & {[P in K]: V}>
+  get(): O
+}

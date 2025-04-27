78e275e6ddd5dc0de8d6a3b8f7a474b1ec1fe283
Janessa <lvlajzhao1983@gmail.com>
2025-04-27 22:57:33 +0800

    redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo1.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo1.ts
@@ -2,22 +2,69 @@ import {Expect, Equal, Alike, NotEqual, ExpectExtends} from "../test-utils";
 
 
 type cases = [
-  Expect<Equal<CheckRepeatedTuple<[number, number, string, boolean]>, true>>,
-  Expect<Equal<CheckRepeatedTuple<[number, string]>, false>>,
-  Expect<Equal<CheckRepeatedTuple<[1, 2, 3]>, false>>,
-  Expect<Equal<CheckRepeatedTuple<[1, 2, 1]>, true>>,
-  Expect<Equal<CheckRepeatedTuple<[]>, false>>,
-  Expect<Equal<CheckRepeatedTuple<string[]>, false>>,
-  Expect<Equal<CheckRepeatedTuple<[number, 1, string, '1', boolean, true, false, unknown, any]>, false>>,
-  Expect<Equal<CheckRepeatedTuple<[never, any, never]>, true>>,
+  Expect<Equal<DeepReadonly<X1>, Expected1>>,
+  Expect<Equal<DeepReadonly<X2>, Expected2>>,
 ]
-type Include<E, L> = L extends [infer F, ...infer R]
-? Equal<E, F> extends true
-  ? true
-  : Include<E, R>
-: false
-type CheckRepeatedTuple<T extends unknown[]> = T extends [infer F, ...infer R]
-? Include<F, R> extends true
-  ? true
-  : CheckRepeatedTuple<R>
-: false
+
+type X1 = {
+  a: () => 22
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 'string'
+        }
+        k: 'hello'
+      }
+      l: [
+        'hi',
+        {
+          m: ['hey']
+        },
+      ]
+    }
+  }
+}
+
+type X2 = { a: string } | { b: number }
+
+type Expected1 = {
+  readonly a: () => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 'string'
+        }
+        readonly k: 'hello'
+      }
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey']
+        },
+      ]
+    }
+  }
+}
+
+type Expected2 = { readonly a: string } | { readonly b: number }
+type DeepArray<L> = L extends [infer F, ...infer R]
+? [DeepReadonly<F>, ...DeepArray<R>]
+: []
+
+type DeepReadonly<T> = T extends any
+? T extends {[P in any]: unknown}
+  ? { readonly [K in keyof T] : DeepReadonly<T[K]> }
+  : T extends unknown[]
+    ? readonly [...DeepArray<T>]
+    : T
+: never
+
+type test = DeepReadonly<X1>

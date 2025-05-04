3769ee67fa30ba5b07ba1ae19029e58a1053e7da
Janessa <lvlajzhao1983@gmail.com>
2025-05-04 21:42:06 +0800

    redo solidity challenge

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -10,9 +10,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Redo is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
     bytes32 public constant BURNER_ROLE = keccak256('BURNER_ROLE');
-
     constructor(
-        string memory name, 
+        string memory name,
         string memory symbol,
         address admin,
         address minter,
@@ -22,15 +21,13 @@ contract Redo is ERC20, AccessControl {
         _grantRole(MINTER_ROLE, minter);
         _grantRole(BURNER_ROLE, burner);
     }
-
     function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
-
     function burn(address from, uint256 amount) external onlyRole(BURNER_ROLE) {
         _burn(from, amount);
     }
-    function revokMinter(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function revokeMinter(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _revokeRole(MINTER_ROLE, account);
     }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -1,21 +1,68 @@
 const {expect} = require("chai")
 const { ethers } = require("hardhat");
 const {loadFixture, time} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { boolean } = require("hardhat/internal/core/params/argumentTypes");
 
 describe('Redo', function () {
     async function deployRedoFixture() {
-        const [admin, minter, burner, ...others] = await ethers.getSigners()
+        const [admin, nonadmin, minter, burner, Bob, ...others] = await ethers.getSigners()
         const Redo = await ethers.getContractFactory('Redo')
         const name = 'aclerc20'
         const symbol = 'aclerc20'
         const redo = await Redo.deploy(name, symbol, admin.getAddress(), minter.getAddress(), burner.getAddress())
-        return {redo}
+        return {redo, admin, nonadmin, minter, burner, Bob}
     }
     describe('init', function () {
         it('init', async function () {
             const {redo} = await loadFixture(deployRedoFixture)
+        })  
+    })
+    describe('mint', function () {
+        it('it minted successfully', async function () {
+            const {redo, minter, Bob} = await loadFixture(deployRedoFixture)
+            const amount = 1000
+            await expect(redo.connect(minter).mint(Bob.getAddress(), amount)).not.to.be.reverted
+            const balance = await redo.balanceOf(Bob.getAddress())
+            expect(balance).to.be.equal(amount)
         })
-        
+        it('it failed to mint when it is not the minter', async function () {
+            const {redo, Bob}= await loadFixture(deployRedoFixture)
+            const amount = 1000
+            await expect(redo.mint(Bob.getAddress(), amount)).to.be.revertedWithCustomError(redo, 'AccessControlUnauthorizedAccount')
+        })
+    })
+    describe('burn', function () {
+        it('it burned the token successfully', async function () {
+            const {redo, burner,minter, Bob} = await loadFixture(deployRedoFixture)
+            const amount  = 1000
+            await redo.connect(minter).mint(Bob.getAddress(), 1000)
+            await redo.connect(burner).burn(Bob.getAddress(), 300)
+            const balance = await redo.balanceOf(Bob.getAddress())
+            expect(balance).to.be.equal(700)
+        })
+        it('it failed to burn when it is not burner', async function () {
+            const {redo, minter, burner, Bob} = await loadFixture(deployRedoFixture)
+            const amount = 1000
+            await redo.connect(minter).mint(Bob.getAddress(), 1000)
+            await expect(redo.burn(Bob.getAddress(), 300)).to.be.revertedWithCustomError(redo, 'AccessControlUnauthorizedAccount')
+        })
+    })
+
+    describe('revokeMinter', function () {
+        it('it revoked successfully', async function () {
+            const {redo, admin, minter, Bob} = await loadFixture(deployRedoFixture)
+            const amount = 1000
+            await expect(redo.connect(minter).mint(Bob.getAddress(), amount)).not.to.be.reverted
+            await redo.connect(admin).revokeMinter(minter)
+            await expect(redo.connect(minter).mint(Bob.getAddress(), amount)).to.be.revertedWithCustomError(redo, 'AccessControlUnauthorizedAccount')
+
+        })
+        it('it failed to revoke when it is not admin', async function () {
+            const {redo, admin, nonadmin, minter, Bob} = await loadFixture(deployRedoFixture)
+            const amount = 1000
+            await expect(redo.connect(minter).mint(Bob.getAddress(), amount)).not.to.be.reverted
+            await expect(redo.connect(nonadmin).revokeMinter(minter)).to.be.revertedWithCustomError(redo, 'AccessControlUnauthorizedAccount')
+        })
     })
 })
 

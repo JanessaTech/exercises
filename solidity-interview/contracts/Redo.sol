49bb6a5376c2e673269e3775ab75eae250dba554
Janessa <lvlajzhao1983@gmail.com>
2025-05-04 20:55:24 +0800

    redo solidity challenge

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -5,7 +5,33 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Redo {
-    
+contract Redo is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+    bytes32 public constant BURNER_ROLE = keccak256('BURNER_ROLE');
+
+    constructor(
+        string memory name, 
+        string memory symbol,
+        address admin,
+        address minter,
+        address burner
+    ) ERC20(name, symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(MINTER_ROLE, minter);
+        _grantRole(BURNER_ROLE, burner);
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
+    }
+    function revokMinter(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _revokeRole(MINTER_ROLE, account);
+    }
 }
+
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -3,7 +3,20 @@ const { ethers } = require("hardhat");
 const {loadFixture, time} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 describe('Redo', function () {
-    
+    async function deployRedoFixture() {
+        const [admin, minter, burner, ...others] = await ethers.getSigners()
+        const Redo = await ethers.getContractFactory('Redo')
+        const name = 'aclerc20'
+        const symbol = 'aclerc20'
+        const redo = await Redo.deploy(name, symbol, admin.getAddress(), minter.getAddress(), burner.getAddress())
+        return {redo}
+    }
+    describe('init', function () {
+        it('init', async function () {
+            const {redo} = await loadFixture(deployRedoFixture)
+        })
+        
+    })
 })
 
 

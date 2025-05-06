384522e552c9aad7783086cd928fbf1f203527ff
Janessa <lvlajzhao1983@gmail.com>
2025-05-06 16:41:15 +0800

    ERC721 with ERC721URIStorage

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -1,24 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; // 元数据扩展
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Redo {
-    function sumArray(uint256[] memory arr) public pure returns(uint256) {
-        uint256 sum;
-        assembly {
-            let length := mload(arr)
-            let ptr := add(arr, 0x20)
-            for {let i := 0} lt(i, length) { i := add(i, 1)} {
-                sum := add(sum, mload(ptr))
-                ptr := add(ptr, 0x20)
-            }
-        }
-        return sum;
+contract Redo is ERC721, ERC721URIStorage {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721('ERC721','ERC721') {}
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId) 
+        public view override(ERC721, ERC721URIStorage) 
+        returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function _baseURI() internal pure  override returns (string memory) {
+        return "http://test.com/";
+    } 
+
+    function getNextTokenId() external view returns(uint256) {
+        return _nextTokenId;
+    }
+
+    function safeMint(address to, string memory _tokenURI) public {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
     }
 }
-
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -5,15 +5,20 @@ const { boolean } = require("hardhat/internal/core/params/argumentTypes");
 
 describe('Redo', function () {
     async function deployRedoFixture() {
+        const [admin, Bob, ...others] = await ethers.getSigners()
         const Redo = await ethers.getContractFactory('Redo')
         const redo  = await Redo.deploy()
-        return {redo}
+        const uri = 'aaaa'
+        return {redo, Bob, uri}
     }
-    describe('sum', function () {
-        it('sum', async function () {
-            const {redo} = await loadFixture(deployRedoFixture)
-            const sum = await redo.sumArray([1, 2, 3])
-            expect(sum).to.be.equal(sum)
+    describe('mint', function () {
+        it('mint', async function () {
+            const {redo, Bob, uri} = await loadFixture(deployRedoFixture)
+            await redo.safeMint(Bob.getAddress(), uri)
+            const balance = await redo.balanceOf(Bob.getAddress())
+            const url = await redo.tokenURI(0)
+            expect(balance).to.be.equal(1)
+            console.log(url)
         })
     })
     

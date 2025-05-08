ccebb5639b0f4e9064943b7cde300dff0924bf88
Janessa <lvlajzhao1983@gmail.com>
2025-05-08 10:43:02 +0800

    withdraw using checks-effects-interactions pattern

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.20;
 // import "hardhat/console.sol";
 
 contract Redo {
-    mapping(address => uint256) balances;
+
+    mapping(address => uint) balances;
     bool private locking;
+
+    event Deposit(address indexed from, uint256 amount);
+    event Withdraw(address indexed from, uint256 amount);
 
     modifier noReentrant() {
         require(!locking, 'reentrance');
@@ -14,25 +18,22 @@ contract Redo {
         locking = false;
     }
 
-    event Deposit(address indexed from, uint256 amount);
-    event Withdraw(address indexed from, uint256 amount);
-
     function deposit() external payable {
         balances[msg.sender] += msg.value;
         emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw() external noReentrant {
+    function withdraw() external noReentrant{
         uint256 amount = balances[msg.sender];
-        require(amount > 0, 'no enough eth left');
+        require(amount > 0, 'no eth');
 
         balances[msg.sender] = 0;
 
         (bool success, ) = payable(msg.sender).call{
             value: amount,
-            gas:2300
+            gas: 2300
         }("");
-        if (!success) {
+        if(!success) {
             balances[msg.sender] += amount;
             revert('failed to withdraw');
         }

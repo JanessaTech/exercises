3875b81e0fcdba3be6a3ea3153d5f9d7fc5f3248
Janessa <lvlajzhao1983@gmail.com>
2025-05-09 15:07:55 +0800

    upgrade using delegatecall challenge

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -5,20 +5,52 @@ pragma solidity ^0.8.20;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Redo is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256('BURNER_ROLE');
+contract Redo {
+    uint256 _valuePlaceHolder;
+    bytes32 private constant ADMIN_SLOT = keccak256('ADMIN_SLOT');
+    bytes32 private constant IMPLEMENTATION_SLOT = keccak256('IMPLEMENTATION_SLOT');
 
-    constructor(address defaultAdmin, address minter, address burner) ERC20("MyToken", "MTK") {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
-        _grantRole(BURNER_ROLE, burner);
+    constructor(address _implementation) {
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            sstore(slot, _implementation)
+        }
+        slot = ADMIN_SLOT;
+        assembly {
+            sstore(slot, caller())
+        }
+    }
+    function value() external view returns(uint256) {
+        return _valuePlaceHolder;
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+    function admin() public view returns(address adm) {
+        bytes32 slot = ADMIN_SLOT;
+        assembly {
+            adm := sload(slot)
+        }
     }
-    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE) {
-        _burn(to, amount);
+    function implementation() public view returns(address impl) {
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            impl := sload(slot)
+        }
     }
+
+    function upgradeTo(address _newImplementation) public {
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            sstore(slot, _newImplementation)
+        }
+    }
+
+    function _deletegate(address _implementation) private {
+        (bool success, ) = _implementation.delegatecall(msg.data);
+        require(success, 'failed to call delegatecall');
+    }
+
+    fallback() external payable {
+        _deletegate(implementation());
+    }
+    receive() external payable {}
 }

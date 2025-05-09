eb604290a51066cd49f00c336afa7d2d16978191
Janessa <lvlajzhao1983@gmail.com>
2025-05-09 17:02:54 +0800

    Redo solidity challenge

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -3,37 +3,41 @@ pragma solidity ^0.8.20;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Redo{
-    mapping(address => uint256) balances;
-    bool private locking;
+contract Redo is ERC721, ERC721URIStorage {
+    uint256 private nextTokenId;
+    constructor() ERC721("MyToken", "MTK") {}
 
-    event Deposit(address indexed from, uint256 amount);
-    event Withdraw(address indexed from, uint256 amount);
+    // The following functions are overrides required by Solidity.
 
-    modifier noReentrant() {
-        require(!locking, 'reentrance');
-        locking = true;
-        _;
-        locking = false;
-    }
-    function deposit() external payable {
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
-    }
-    function withdraw() external noReentrant {
-        uint256 amount = balances[msg.sender];
-        require(amount > 0, 'no eough eth');
-
-        balances[msg.sender] = 0;
-
-        (bool success, ) = payable(msg.sender).call{
-            value: amount,
-            gas: 2300
-        }("");
-        require(success, 'failed to withdraw');
-        emit Withdraw(msg.sender, amount);
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function getNextTokenId() public view returns(uint256) {
+        return nextTokenId;
+    }
+
+    function safeMint(address to, string memory _tokenURI) public {
+        uint256 tokenId = nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+    }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -5,25 +5,7 @@ const { boolean } = require("hardhat/internal/core/params/argumentTypes");
 const { extendEnvironment } = require("hardhat/config");
 
 describe('Redo', function () {
-    async function deployRedoFixture() {
-        const [admin, Bob, ...others] = await ethers.getSigners()
-        const Bank = await ethers.getContractFactory('Redo')
-        const bank = await Bank.deploy()
-        return {bank, Bob}
-    }
-    describe('deposit & withdraw', function () {
-        it('deposit', async function () {
-            const {bank, Bob} = await loadFixture(deployRedoFixture)
-            const amount = 1000
-            await expect(bank.connect(Bob).deposit({value: amount})).to.emit(bank, 'Deposit').withArgs(Bob.getAddress(), amount)
-        })
-        it('withdraw', async function () {
-            const {bank, Bob} = await loadFixture(deployRedoFixture)
-            const amount = 1000
-            await bank.connect(Bob).deposit({value: amount})
-            await expect(bank.connect(Bob).withdraw()).to.emit(bank, 'Withdraw').withArgs(Bob.getAddress(), amount)
-        })
-    })
+
     
 })
 

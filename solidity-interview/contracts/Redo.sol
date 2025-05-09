c9845a673ea79ae740e1e2c73ec204d29ece9d1c
Janessa <lvlajzhao1983@gmail.com>
2025-05-09 21:34:15 +0800

    Redo solidity challenge

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -9,13 +9,16 @@ contract Redo is ERC1155, Ownable {
     uint256 public constant SWORD = 1;
     uint256 public constant POTION = 2;
     uint256 public constant SHIELD = 3;
-    
+
     constructor(address _owner)
     ERC1155("http://test.com/{id}.json")
     Ownable(_owner) {
         _mint(msg.sender, SWORD, 1000, '');
         _mint(msg.sender, POTION, 2000, '');
         _mint(msg.sender, SHIELD, 3000, '');
+    }
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
     }
 
     function batchMint(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public {

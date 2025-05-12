9ff075ce945a7b507296fc086a99a2a4c638c0bd
Janessa <lvlajzhao1983@gmail.com>
2025-05-12 16:45:03 +0800

    create erc20 with accesscontrol

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -6,20 +6,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Redo is ERC20, AccessControl{
-    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+    bytes32 public constant MINTER_ROLE = keccak256('ADMIN_ROLE');
     bytes32 public constant BURNER_ROLE = keccak256('BURNER_ROLE');
 
-    constructor(address admin, address minter, address burner) ERC20('ERC20', 'ERC20'){
+    constructor(address admin, address minter, address burner) ERC20('ERC20', 'ERC20') {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(MINTER_ROLE, minter);
         _grantRole(BURNER_ROLE, burner);
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
-
-    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE) {
+    function burn(address to, uint256 amount) external onlyRole(BURNER_ROLE) {
         _burn(to, amount);
     }
 }

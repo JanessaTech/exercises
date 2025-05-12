7df8680f11f8a6743ce37a6809a1b4c1e98975c7
Janessa <lvlajzhao1983@gmail.com>
2025-05-12 21:18:11 +0800

    ERC1155 and Ownable

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -8,28 +8,29 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract Redo is ERC1155, Ownable {
     uint256 public constant SWORD = 1;
     uint256 public constant POTION = 2;
-    uint256 public constant SHIELD = 3;
+    uint256 public constant SHEILD = 3;
 
-    constructor(address initialOwner) ERC1155("http://test.com/{id}.json") Ownable(initialOwner) {
-        _mint(msg.sender, SWORD, 1000, '');
-        _mint(msg.sender, POTION, 2000, '');
-        _mint(msg.sender, SHIELD, 3000, '');
+    constructor(address initialOwner) 
+        ERC1155("http://test.com/{id}.json") 
+        Ownable(initialOwner) {
+            _mint(msg.sender, SWORD, 1000, '');
+            _mint(msg.sender, POTION, 3000, '');
+            _mint(msg.sender, SHEILD, 3000, '');
     }
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
 
-    function mintBatch(address to, 
-            uint256[] memory ids, 
-            uint256[] memory values, 
-            bytes memory data) public onlyOwner {
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public onlyOwner {
+        require(ids.length == values.length, 'ids.length != values.length');
         _mintBatch(to, ids, values, data);
     }
+
     function batchTransfer(address[] memory recipients, uint256 id, uint256 amount) public {
         require(balanceOf(msg.sender, id) >= recipients.length * amount);
         for (uint256 i = 0; i < recipients.length; i++) {
-            _safeTransferFrom(msg.sender, recipients[i], id, amount, '');
+            safeTransferFrom(msg.sender, recipients[i], id, amount, '');
         }
     }
 }

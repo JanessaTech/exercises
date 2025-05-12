354519b6301c129349c52f5c5fdf8031ca75bff3
Janessa <lvlajzhao1983@gmail.com>
2025-05-12 16:00:33 +0800

    How tp upgrade a contract

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -6,28 +6,53 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Redo {
-    bool private locking;
-    modifier noReentrant() {
-        require(!locking, 'reentrance');
-        locking = true;
-        _;
-        locking = false;
-    }
-    event Deposit(address indexed from, uint256 amount);
-    event Withdraw(address indexed from, uint256 amount);
+    uint256 public _valuePlaceHolder;
+    bytes32 private constant ADMIN_SLOT = keccak256('ADMIN_SLOT');
+    bytes32 private constant IMPLEMENTATION = keccak256('IMPLEMENTATION');
 
-    mapping(address => uint256) balances;
-    function deposit() external payable {
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
+    constructor(address _implemenation) {
+        bytes32 slot = IMPLEMENTATION;
+        assembly {
+            sstore(slot, _implemenation)
+        }
+        slot = ADMIN_SLOT;
+        assembly {
+            sstore(slot, caller())
+        }
     }
 
-    function withdraw() external noReentrant {
-        uint256 amount = balances[msg.sender];
-        balances[msg.sender] = 0;
-
-        (bool success, ) = payable(msg.sender).call{value: amount, gas: 2300}('');
-        require(success, 'failed to withdraw');
-        emit Withdraw(msg.sender, amount);
+    function admin() public view returns(address adm) {
+        bytes32 slot = ADMIN_SLOT;
+        assembly {
+            adm := sload(slot)
+        }
     }
+    function implementation() public view returns(address impl) {
+        bytes32 slot = IMPLEMENTATION;
+        assembly {
+            impl := sload(slot)
+        }
+    }
+
+    function value() public view returns(uint256) {
+        return _valuePlaceHolder;
+    }
+
+    function upgradeTo(address _newImplementation) public {
+        bytes32 slot = IMPLEMENTATION;
+        assembly {
+            sstore(slot, _newImplementation)
+        }
+    }
+
+    function _deletegate(address _implementation) private {
+        (bool success,) = _implementation.delegatecall(msg.data);
+        require(success, 'failed to call delegatecall');
+    }
+
+    fallback() external payable {
+        _deletegate(implementation());
+    }
+
+    receive() external payable {}
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -7,17 +7,43 @@ const { extendEnvironment } = require("hardhat/config");
 describe('Redo', function () {
     async function deployRedoFixture() {
         const [admin, Bob, ...others] = await ethers.getSigners()
-        const Redo = await ethers.getContractFactory('Redo')
-        const redo  = await Redo.deploy()
-        return {redo, Bob}
+        const LogicV1 = await ethers.getContractFactory('LogicV1')
+        const logicV1 = await LogicV1.deploy()
+        const LogicV2 = await ethers.getContractFactory('LogicV2')
+        const logicV2 = await LogicV2.deploy()
+        const Proxy = await ethers.getContractFactory('Redo')
+        const proxy  = await Proxy.deploy(logicV1.getAddress())
+        return {proxy, admin, Bob, logicV1, logicV2}
     }
-    describe('withdraw & deposit', function () {
-        it('withdraw & deposit', async function () {
-            const {redo, Bob} = await loadFixture(deployRedoFixture)
-            const amount = 1000
-            await redo.connect(Bob).deposit({value: amount})
-            await expect(redo.connect(Bob).withdraw()).to.emit(redo, "Withdraw").withArgs(Bob.getAddress(), amount)
-
+    describe('LogicV1', function () {
+        it('LogicV1', async function () {
+            const {proxy, admin} = await loadFixture(deployRedoFixture)
+            const abi = ['function setValue(uint256) external']
+            const iface = new ethers.Interface(abi)
+            const amount = 10
+            const cdata = iface.encodeFunctionData('setValue(uint256)', [amount])
+            const tx = {
+                to: proxy.getAddress(),
+                data: cdata
+            }
+            await admin.sendTransaction(tx)
+            const val = await proxy.value()
+            expect(val).to.be.equal(amount)
+        })
+        it('LogicV2', async function () {
+            const {proxy, admin, logicV2} = await loadFixture(deployRedoFixture)
+            await proxy.upgradeTo(logicV2.getAddress())
+            const abi = ['function setValue(uint256) external']
+            const iface = new ethers.Interface(abi)
+            const amount = 10
+            const cdata = iface.encodeFunctionData('setValue(uint256)', [amount])
+            const tx = {
+                to: proxy.getAddress(),
+                data: cdata
+            }
+            await admin.sendTransaction(tx)
+            const val = await proxy.value()
+            expect(val).to.be.equal(amount * 2)
         })
     })
 })

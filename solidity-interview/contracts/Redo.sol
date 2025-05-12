61fdfa6bb207d4b9c0056e91981f0b98f8f1d063
Janessa <lvlajzhao1983@gmail.com>
2025-05-12 15:30:00 +0800

    checks-affects-interactions

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -6,16 +6,28 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Redo {
-    function sumArray(uint256[] memory arr) public pure returns(uint256) {
-        uint256 sum;
-        assembly {
-            let length := mload(arr)
-            let ptr := add(arr, 0x20)
-            for { let i := 0} lt(i, length) { i := add(i, 1)} {
-                sum := add(sum, mload(ptr))
-                ptr := add(ptr, 0x20)
-            }
-        }
-        return sum;
+    bool private locking;
+    modifier noReentrant() {
+        require(!locking, 'reentrance');
+        locking = true;
+        _;
+        locking = false;
+    }
+    event Deposit(address indexed from, uint256 amount);
+    event Withdraw(address indexed from, uint256 amount);
+
+    mapping(address => uint256) balances;
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw() external noReentrant {
+        uint256 amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+
+        (bool success, ) = payable(msg.sender).call{value: amount, gas: 2300}('');
+        require(success, 'failed to withdraw');
+        emit Withdraw(msg.sender, amount);
     }
 }

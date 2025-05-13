4e9cd515dda580ecba494a9f6a16ddd54322b040
Janessa <lvlajzhao1983@gmail.com>
2025-05-13 14:48:08 +0800

    Redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo.ts
@@ -1,14 +1,34 @@
 import { Alike, Expect, Equal, NotEqual, ExpectExtends } from "../test-utils";
 
-function foo(arg1: string, arg2: number): void {}
-function bar(arg1: boolean, arg2: { a: 'A' }): void {}
-function baz(): void {}
+type Foo = {
+  name: string
+  age: string
+}
+type Bar = {
+  name: string
+  age: string
+  gender: number
+}
+type Coo = {
+  name: string
+  gender: number
+}
 
 type cases = [
-  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
-  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
-  Expect<Equal<MyParameters<typeof baz>, []>>,
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string, gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string, gender: number }>>,
 ]
-type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer P) => any
-? P
-: never
+
+// type Diff<O, O1> = {
+//   [K in keyof (O & O1) as Exclude<K, keyof (O | O1)>] : K extends keyof O
+//   ? O[K]
+//   : K extends keyof O1
+//     ? O1[K]
+//     : never
+// }
+
+type Diff<O, O1> = {
+  [K in keyof (O & O1) as K extends keyof (O | O1) ? never : K] : (O & O1)[K]
+} 

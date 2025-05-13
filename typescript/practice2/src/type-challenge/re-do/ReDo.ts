73e4efdd11143db088b5052f321223bf99a19661
Janessa <lvlajzhao1983@gmail.com>
2025-05-13 15:21:42 +0800

    Redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo.ts
@@ -1,44 +1,12 @@
 import { Alike, Expect, Equal, NotEqual, ExpectExtends } from "../test-utils";
 
-type Foo = {
-  [key: string]: any
-  foo(): void
-}
-
-type Bar = {
-  [key: number]: any
-  bar(): void
-  0: string
-}
-
-const foobar = Symbol('foobar')
-type FooBar = {
-  [key: symbol]: any
-  [foobar](): void
-}
-
-type Baz = {
-  bar(): void
-  baz: string
-}
-
 type cases = [
-  Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
-  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void, 0: string }>>,
-  Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
-  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void, baz: string }>>,
+  Expect<Equal<StringToUnion<''>, never>>,
+  Expect<Equal<StringToUnion<'t'>, 't'>>,
+  Expect<Equal<StringToUnion<'hello'>, 'h' | 'e' | 'l' | 'l' | 'o'>>,
+  Expect<Equal<StringToUnion<'coronavirus'>, 'c' | 'o' | 'r' | 'o' | 'n' | 'a' | 'v' | 'i' | 'r' | 'u' | 's'>>,
 ]
 
-type Filter<T> = string extends T
-? never
-: symbol extends T
-  ? never
-  : number extends T
-    ? never
-    : T
-
-type RemoveIndexSignature<T> = {
-  [K in keyof T as Filter<K>] : T[K]
-}
-
-type test = RemoveIndexSignature<Bar>
+type StringToUnion<T extends string> = T extends `${infer F}${infer R}`
+? F | StringToUnion<R>
+: never

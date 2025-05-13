b1e4578f13d897b599b76befb30f242df708c136
Janessa <lvlajzhao1983@gmail.com>
2025-05-13 14:52:17 +0800

    Redo TS challenge

--- typescript/practice2/src/type-challenge/re-do/ReDo.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo.ts
@@ -1,34 +1,25 @@
 import { Alike, Expect, Equal, NotEqual, ExpectExtends } from "../test-utils";
 
-type Foo = {
-  name: string
-  age: string
-}
-type Bar = {
-  name: string
-  age: string
-  gender: number
-}
-type Coo = {
-  name: string
-  gender: number
-}
-
 type cases = [
-  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
-  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
-  Expect<Equal<Diff<Foo, Coo>, { age: string, gender: number }>>,
-  Expect<Equal<Diff<Coo, Foo>, { age: string, gender: number }>>,
+  Expect<Equal<IsUnion<string>, false>>,
+  Expect<Equal<IsUnion<string | number>, true>>,
+  Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
+  Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+  Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+  Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+  Expect<Equal<IsUnion<[string | number]>, false>>,
+  // Cases where T resolves to a non-union type.
+  Expect<Equal<IsUnion<string | never>, false>>,
+  Expect<Equal<IsUnion<string | unknown>, false>>,
+  Expect<Equal<IsUnion<string | any>, false>>,
+  Expect<Equal<IsUnion<string | 'a'>, false>>,
+  Expect<Equal<IsUnion<never>, false>>,
 ]
 
-// type Diff<O, O1> = {
-//   [K in keyof (O & O1) as Exclude<K, keyof (O | O1)>] : K extends keyof O
-//   ? O[K]
-//   : K extends keyof O1
-//     ? O1[K]
-//     : never
-// }
-
-type Diff<O, O1> = {
-  [K in keyof (O & O1) as K extends keyof (O | O1) ? never : K] : (O & O1)[K]
-} 
+type IsUnion<T, A = T> = [T] extends [never]
+? false
+: T extends any
+? [A] extends [T]
+  ? false
+  : true
+: never

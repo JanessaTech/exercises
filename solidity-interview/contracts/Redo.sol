8929cc758490d4a3772b3e2be6a03afb9073e337
Janessa <lvlajzhao1983@gmail.com>
2025-05-14 14:15:01 +0800

    Redo solidity  challenge

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -5,26 +5,27 @@ pragma solidity ^0.8.20;
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC1155, Ownable {
+contract Redo is ERC1155, Ownable {
     uint256 public constant SWORD = 1;
     uint256 public constant POTION = 2;
     uint256 public constant SHEILD = 3;
-
     constructor(address initialOwner) 
         ERC1155("http://test.com/{id}.json") 
         Ownable(initialOwner) {
             _mint(msg.sender, SWORD, 1000, '');
-            _mint(msg.sender, POTION, 2000, '');
-            _mint(msg.sender, SHEILD, 3000, '');
+            _mint(msg.sender, POTION, 1000, '');
+            _mint(msg.sender, SHEILD, 1000, '');
+
     }
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
+
     function mintBatch(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public onlyOwner {
-        require(ids.length == values.length, 'ids.length != values.length');
         _mintBatch(to, ids, values, data);
     }
+
     function transferBatch(address[] memory recipients, uint256 id, uint256 amount) public {
         require(balanceOf(msg.sender, id) >= recipients.length * amount);
         for (uint256 i = 0; i < recipients.length; i++) {

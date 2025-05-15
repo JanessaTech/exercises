3755490609357790c26211b5d040c48a6a2403d3
Janessa <lvlajzhao1983@gmail.com>
2025-05-15 09:04:35 +0800

    Redo solidity  challenge

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -5,31 +5,17 @@ pragma solidity ^0.8.20;
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Redo is ERC1155, Ownable {
-    uint256 public constant SWORD = 1;
-    uint256 public constant POTION = 2;
-    uint256 public constant SHEILD = 3;
-    constructor(address initialOwner) 
-        ERC1155("http://test.com/{id}.json") 
-        Ownable(initialOwner) {
-            _mint(msg.sender, SWORD, 1000, '');
-            _mint(msg.sender, POTION, 1000, '');
-            _mint(msg.sender, SHEILD, 1000, '');
-
-    }
-
-    function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
-    }
-
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public onlyOwner {
-        _mintBatch(to, ids, values, data);
-    }
-
-    function transferBatch(address[] memory recipients, uint256 id, uint256 amount) public {
-        require(balanceOf(msg.sender, id) >= recipients.length * amount);
-        for (uint256 i = 0; i < recipients.length; i++) {
-            safeTransferFrom(msg.sender, recipients[i], id, amount, '');
+contract Redo {
+    function sumArray(uint256[] memory arr) public pure returns(uint256) {
+        uint256 sum;
+        assembly {
+            let length := mload(arr)
+            let ptr := add(arr, 0x20)
+            for { let i := 0 } lt(i, length) { i := add(i, 1)} {
+                sum := add(sum, mload(ptr))
+                ptr := add(ptr, 0x20)
+            }
         }
+        return sum;
     }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,14 +6,16 @@ const { extendEnvironment } = require("hardhat/config");
 
 describe('Redo', function () {
     async function deployRedoFixture() {
-        const [admin, Bob, nonBob, userA, userB, ...others] = await ethers.getSigners()
+        const [admin, ...others] = await ethers.getSigners()
         const Redo = await ethers.getContractFactory('Redo')
-        const redo = await Redo.deploy(admin.getAddress())
+        const redo = await Redo.deploy()
         return {redo}
     }
-    describe('init', function () {
-        it('init', async function () {
+    describe('sumArray', function () {
+        it('sumArray', async function () {
            const {redo} = await loadFixture(deployRedoFixture)
+           const sum = await redo.sumArray([1, 2, 3, 4])
+           expect(sum).to.be.equal(10)
         })
     })
     

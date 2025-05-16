210574faa9ba86a39c566cc6a3aad5517565c8b3
Janessa <lvlajzhao1983@gmail.com>
2025-05-16 11:41:35 +0800

    using aggregate

--- mongodb-interview/one-many.js
+++ mongodb-interview/one-many.js
@@ -1,57 +1,56 @@
 const {default: mongoose, Schema} = require('mongoose')
 
-const bookSchema = new mongoose.Schema({
+const bookSchema = new Schema({
     title: {
         type: String,
-        require: [true, 'title is required'],
-        index: true
+        require: [true, 'title is required']
     },
     author: {
         type: Schema.Types.ObjectId,
         ref: 'Author',
-        require: [true, 'author is required']
-    },
+        require: [true, 'author id is required']
+    }, 
     price: {
         type: Number,
         min: [0, 'price >= 0'],
         default: 0,
-        require: [true, 'price is required']
+        require: [true, 'price is rerquired']
     },
-    isbn : {
+    isbn: {
         type: String,
         validate: {
             validator: function(v) {
-                var re = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/
-                return re.test(v)
+
             },
-            message: props => `${props.value} is is invalid ISBN`
-        },
-        require: [true, 'isbn is required']
+            message: props => `${props} is an invalid isbn`
+        }, 
+        require:[true, 'isbn is required']
     }
 })
 
-const authorSchema = new mongoose.Schema({
+
+const authorSchema = new Schema({
     firstName: {
         type: String,
         require: [true, 'firstName is required']
-    },
+    }, 
     lastName: {
         type: String,
         require: [true, 'lastName is required']
     },
     age: {
         type: Number,
-        min: [18, 'min >= 18'],
-        require: [true, 'age is required']
+        min: [18, 'age >= 18'],
+        require:[true, 'age is required']
     },
     gender: {
         type: String,
-        enum: {
+        enume: {
             values: ['male', 'female'],
             message: '{VALUE} is not supported'
-        },
+        }, 
         default: 'male',
-        require: [true, 'genger is required']
+        require: [true, 'gender is required']
     }
 }, {
     virtuals: {
@@ -60,9 +59,10 @@ const authorSchema = new mongoose.Schema({
                 return this.firstName + ' ' + this.lastName
             }
         }
-    },
+    }, 
     toJSON: {virtuals: true}
 })
+
 authorSchema.virtual('books', {
     ref: 'Book',
     localField: '_id',
@@ -72,18 +72,19 @@ authorSchema.virtual('books', {
 const Book = mongoose.model('Book', bookSchema)
 const Author = mongoose.model('Author', authorSchema)
 
+
 function connect() {
     mongoose.connect('mongodb://127.0.0.1/interview')
     let db = mongoose.connection
-    db.once('open', ()  => {
+    db.once('open', () => {
         console.log('connected to database')
     })
     db.on('error', (err) => {
-        console.log()
+        console.log('database error: ', err)
     })
-}
 
-async function create() {
+}
+async function create(){
     const author1 = new Author({ firstName: 'John', lastName: 'Tomas', age: 45, gender: 'male'})
     const author2 = new Author({ firstName: 'jineffer', lastName: 'Jwong', age: 22, gender: 'female'})
     const author3 = new Author({ firstName: 'Linda', lastName: 'Huala', age: 38, gender: 'female'})
@@ -96,6 +97,7 @@ async function create() {
     const book31 = new Book({title: 'hero31', author: author3._id, price: 40, isbn: '3-56619-909-1'})
     const book32 = new Book({title: 'hero32', author: author3._id, price: 80, isbn: '3-56619-909-2'})
     const book33 = new Book({title: 'hero33', author: author3._id, price: 120, isbn: '3-56619-909-3'})
+
     try {
         await author1.save()
         await author2.save()
@@ -109,37 +111,49 @@ async function create() {
         await book31.save()
         await book32.save()
         await book33.save()
-    } catch (err) {
-        console.log(err)
+    } catch(e) {
+        console.log('Failed to save authors and books')
+        console.log(e)
+        process.exit()
     }
+    console.log('data is created')
 }
-
 async function queryAuthors() {
-    const res = await Author.find({gender: 'female', age: {$gt: 20}}).sort({age: 1}).populate('books')
+    const res = await Author.find({age: {$gt : 20}}).sort({age: 1}).populate('books')
     console.log(JSON.stringify(res, null, 2))
 }
-
 async function queryBooks() {
     const res = await Book.find({price: {$lte: 50}})
-                        .sort({price: 1})
-                        .select({title: 1, price: 1})
-                        .populate('author', 'fullName age')
+                            .sort({price: 1})
+                            .select({title: 1, price: 1})
+                            .populate('author', 'firstName lastName gender')
     console.log(JSON.stringify(res, null, 2))
 }
-
-async function aggragate() {
+async function aggregate() {
     const agg = await Book.aggregate([
         {
-            $lookup: 
+            $lookup: /**
+            * from: The target collection.
+            * localField: The local join field.
+            * foreignField: The target join field.
+            * as: The name for the results.
+            * pipeline: Optional pipeline to run on the foreign collection.
+            * let: Optional variables to use in the pipeline field stages.
+            */
            {
              from: "authors",
-             localField: "author",
+             localField:"author",
              foreignField: "_id",
              as: "R"
            }
-        },
+        }, 
         {
-            $unwind:
+            $unwind: /**
+            * path: Path to the array field.
+            * includeArrayIndex: Optional name for index.
+            * preserveNullAndEmptyArrays: Optional
+            *   toggle to unwind null and empty values.
+            */
            {
              path: "$R",
              includeArrayIndex: 'string',
@@ -147,27 +161,32 @@ async function aggragate() {
            }
         },
         {
-            $project:
+            $project: /**
+            * specifications: The fields to
+            *   include or exclude.
+            */
            {
              title: 1,
              price: 1,
-             isbn: 1,
-             author_id: "$R._id",
-             author_age:"$R.age",
-             author_gender:"$R.gender"
+             gender: "$R.gender"
            }
         },
         {
-            $match:
+            $match: /**
+            * query: The query in MQL.
+            */
            {
-             price: {$gte: 100}
+             price:{$gte: 100}
            }
-        },
+        }, 
         {
-            $group:
+            $group: /**
+            * _id: The id of the group.
+            * fieldN: The first field name.
+            */
            {
-             _id: "$author_gender",
-             sum_price: {
+             _id: "$gender",
+             totalPrice: {
                $sum: "$price"
              }
            }
@@ -178,17 +197,19 @@ async function aggragate() {
 }
 
 async function main() {
-    connect()
     try {
+        connect()
         //await create()
         //await queryAuthors()
-        //await queryBooks()
-        await aggragate()
-    } catch(err) {
+        await queryBooks()
+        await aggregate()
+    } catch (err) {
         console.log(err)
+        process.exit(1)
     }
 }
 
 main().then().catch((err) => {
     console.log(err)
 })
+

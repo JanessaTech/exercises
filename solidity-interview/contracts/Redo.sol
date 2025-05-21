7a5066f13345fe7672dd7c33bab29591c949ff3e
Janessa <lvlajzhao1983@gmail.com>
2025-05-21 20:15:09 +0800

    redo sumArray

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.20;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Redo {
     function sumArray(uint256[] memory arr) public pure returns(uint256) {
@@ -11,11 +9,11 @@ contract Redo {
         assembly {
             let length := mload(arr)
             let ptr := add(arr, 0x20)
-            for { let i := 0 } lt(i, length) { i := add(i, 1)} {
+            for { let i := 0} lt(i, length) { i := add(i, 1)} {
                 sum := add(sum, mload(ptr))
                 ptr := add(ptr, 0x20)
             }
         }
         return sum;
-    }
+    } 
 }

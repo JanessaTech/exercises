5c624253de2b6c37cb23fa16cac3407014bc0562
Janessa <lvlajzhao1983@gmail.com>
2025-05-21 16:51:34 +0800

    Redo : how to operate bulk op

--- mongodb-interview/Redo.js
+++ mongodb-interview/Redo.js
@@ -1,17 +1,10 @@
 const {default: mongoose, Schema} = require('mongoose')
 
-const childSchema = new Schema({
-    product: String,
-    score: Number
+const bulkSchema  = new Schema({
+    _id: Number,
+    data: String
 })
-const parentSchema = new Schema({
-    name: String,
-    score: [Number],
-    tags: [String],
-    children: [childSchema]
-})
-
-const MyArray = mongoose.model('MyArray', parentSchema)
+const Bulk = mongoose.model('Bulk', bulkSchema)
 
 function connect() {
     mongoose.connect('mongodb://127.0.0.1/interview')
@@ -26,65 +19,35 @@ function connect() {
 
 async function create() {
     try {
-        connect()
-        const arr1 = new MyArray({name: 'arr1', 
-                                  score: [10, 50, 100], 
-                                  tags: ["school", "book", "bag", "headphone", "appliance" ],
-                                  children: [{ "product": "abc", "score": 10 },
-                                    { "product": "xyz", "score": 5 }]
-                                })
-        const arr2 = new MyArray({name: 'arr2', 
-                                  score: [60, 90, 120], 
-                                  tags: ["book", "school"],
-                                  children: [{ "product": "abc", "score": 8 },
-                                    { "product": "xyz", "score": 7 }]
-                                })
-        const arr3 = new MyArray({name: 'arr3', 
-                                  score: [200, 300, 400], 
-                                  tags: ["electronics", "school"],
-                                  children: [{ "product": "abc", "score": 7 },
-                                    { "product": "def", "score": 8 }]})
-        await arr1.save()
-        await arr2.save()
-        await arr3.save()
+        await Bulk.insertMany([
+            {_id: 1, data: 'data1'},
+            {_id: 2, data: 'data2'},
+            {_id: 3, data: 'data3'},
+        ])
     } catch (err) {
         console.log(err)
     }
     console.log('data is created')
 }
 
-// pick up documents with tags which have "school" and "book" in the array
-async function query1() {
-    const res = MyArray.find({tags: {$all: ["school", "book"]}})
+async function bulkOp() {
+   const res = await Bulk.bulkWrite([
+    {insertOne: {document: {_id: 4, data: 'data_4'}}},
+    {updateOne: {filter: {_id: 2}, update: {$set: {data: 'data_new'}}}},
+    {deleteOne: {filter: {_id: 3}}}
+   ], {ordered: true})
 }
-// pick up documents with score in which there is at least one element which is > 40 and < 110
-async function query2() {
-    const res = await MyArray.find({score: {$elemMatch : {$gt : 40, $lt: 110}}})
-}
-// pick up documents with children in which there is at least one element whose product is 'xyz' and score > 6
-async function query3() {
-    const res = await MyArray.find({children: {$elemMatch: {product: "xyz", score: {$gt: 6}}}})
-}
-
-// pick up the first document, whose score has 60
-async function query4() {
-    const res = MyArray.find({score: 60})
-}
-
-// pick up all documents, of which score don't have 100
-async function query5() {
-    const res = MyArray.find({score: {$ne: 100}})
+async function query() {
+    const res = await Bulk.find()
+    console.log(res)
 }
 
 async function main() {
     try {
         connect()
         //await create()
-        //await query1()
-        //await query2()
-        //await query3()
-        //await query4()
-        await query5()
+        await bulkOp()
+        await query()
     } catch (err) {
         console.log(err)
     }
@@ -93,5 +56,3 @@ async function main() {
 main().then().catch((err) => {
     console.log(err)
 })
-
-

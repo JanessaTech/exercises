da257c3127c14dff16d19692dcc9e0c63edc467e
Janessa <lvlajzhao1983@gmail.com>
2025-05-22 17:43:35 +0800

    redo solution for Diff

--- typescript/practice2/src/type-challenge/re-do/ReDo.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo.ts
@@ -20,8 +20,14 @@ type cases = [
   Expect<Equal<Diff<Foo, Coo>, { age: string, gender: number }>>,
   Expect<Equal<Diff<Coo, Foo>, { age: string, gender: number }>>,
 ]
-
-
 type Diff<O, O1> = {
-  [P in keyof(O & O1) as P extends keyof(O | O1) ? never : P] : (O & O1)[P]
+  [P in keyof(O & O1) as P extends keyof(O | O1) ? never : P] : P extends keyof O
+  ? O[P]
+  : P extends keyof O1
+    ? O1[P]
+    : never
 }
+
+// type Diff<O, O1> = {
+//   [P in keyof(O & O1) as P extends keyof(O | O1) ? never : P] : (O & O1)[P]
+// }

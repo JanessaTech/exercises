8d2f210e8145c23ada93019236c81979b792cd31
Janessa <lvlajzhao1983@gmail.com>
2025-05-22 21:48:40 +0800

    redo AllCombinations

--- typescript/practice2/src/type-challenge/re-do/ReDo.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo.ts
@@ -1,22 +1,31 @@
 import { Alike, Expect, Equal, NotEqual, ExpectExtends } from "../test-utils";
 
 type cases = [
-  Expect<Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[1, '', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, 'test', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', true, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [1], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], {}, undefined, null]>, false>>,
-  Expect<Equal<AnyOf<[]>, false>>,
+  Expect<Equal<AllCombinations<''>, ''>>,
+  Expect<Equal<AllCombinations<'A'>, '' | 'A'>>,
+  Expect<Equal<AllCombinations<'AB'>, '' | 'A' | 'B' | 'AB' | 'BA'>>,
+  Expect<Equal<AllCombinations<'ABC'>, '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'>>,
+  Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
 ]
 
-type falsy = [0, '', false, [], {[P in any]: never}, undefined, null][number]
 
-type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
-? F extends falsy
-  ? AnyOf<R>
-  : true
-: false
+type StringToUnion<S> = S extends `${infer F}${infer R}`
+? F | StringToUnion<R>
+: never
+
+type ArrayToString<A> = A extends any
+? A extends [infer F, ...infer R]
+  ? `${string &F}${ArrayToString<R>}`
+  : ''
+: never
+
+type Com<U, path extends unknown[] = [], acc = never, A = U> = [U] extends [never]
+? ArrayToString<acc | path>
+: U extends any
+  ? Com<Exclude<A, U>, [...path, U], acc | path>
+  : never
+
+type AllCombinations<S extends string> = Com<StringToUnion<S>>
+
+type test = StringToUnion<'AB'>
+type test1 = Com<StringToUnion<'AB'>>

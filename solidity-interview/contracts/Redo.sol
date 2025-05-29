c55225f1f098044f42260fed80835d3bd5d52492
Janessa <lvlajzhao1983@gmail.com>
2025-05-29 08:00:25 +0800

    redo secure withdraw

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -7,16 +7,34 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Redo {
-    function sumArray(uint256[] memory arr) public pure returns(uint256) {
-        uint256 sum;
-        assembly {
-            let length := mload(arr)
-            let ptr := add(arr, 0x20)
-            for { let i := 0} lt(i, length) { i := add(i, 1)} {
-                sum := add(sum, mload(ptr))
-                ptr := add(ptr, 0x20)
-            }
-        }
-        return sum;
+    bool private locking;
+
+    event Deposit(address indexed from, uint256 amount);
+    event Withdraw(address indexed from , uint256 amount);
+
+    mapping(address => uint256) balances;
+
+    modifier reentrance() {
+        require(!locking, "reentrance");
+        locking = true;
+        _;
+        locking = false;
+    }
+
+    function deposit() public payable{
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw() public reentrance {
+        uint256 amount = balances[msg.sender];
+        require(amount > 0, 'zero balance');
+        balances[msg.sender] = 0;
+        (bool success,) = payable(msg.sender).call{
+            value: amount,
+            gas: 2300
+        }("");
+        require(success, "failed to withdraw");
+        emit Withdraw(msg.sender, amount);
     }
 }

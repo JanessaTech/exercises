a3003b857812d7bb14f538e3af2440216df3bdf8
Janessa <lvlajzhao1983@gmail.com>
2025-06-03 09:34:42 +0800

    redo upgrade logic

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -7,33 +7,53 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Redo {
-    bool private locking;
-    event Deposit(address indexed from, uint256 amount);
-    event Withraw(address indexed from, uint256 amount);
+    uint256 public value;
 
-    mapping(address => uint256) balances;
+    bytes32 private constant ADMIN_SLOT = keccak256("ADMIN_SLOT");
+    bytes32 private constant IMPLEMENTATION_SLOT = keccak256("IMPLEMENTATION_SLOT");
 
-    modifier nonReentrant() {
-        require(!locking, "reentrance");
-        locking = true;
-        _;
-        locking = false;
+    constructor(address _impelmentation) {
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            sstore(slot, _impelmentation)
+        }
+        slot = ADMIN_SLOT;
+        assembly {
+            sstore(slot, caller())
+        }
     }
 
-    function deposit() public payable{
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
+    function admin() public view returns(address adm) {
+        bytes32 slot = ADMIN_SLOT;
+        assembly {
+            adm := sload(slot)
+        }
     }
 
-   function withdraw() external nonReentrant {
-        uint256 amount = balances[msg.sender];
-        require(amount > 0, 'not enough eth to withdraw');
-        balances[msg.sender] = 0;
-        (bool success, ) = payable(msg.sender).call{
-            value: amount,
-            gas: 2300
-        }("");
-        require(success, 'failed to withdraw');
-        emit Withraw(msg.sender, amount);
+    function implementation() public view returns(address imp) {
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            imp := sload(slot)
+        }
     }
+
+    function upgradeTo(address _implementation) public {
+        require(msg.sender == admin(), 'not owner');
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            sstore(slot, _implementation)
+        }
+    }
+
+    function _delegate(address _impelmenation) private {
+        (bool success, ) = _impelmenation.delegatecall(msg.data);
+        require(success, 'failed to call delegegatecall');
+    }
+
+    fallback() external payable {
+        _delegate(implementation());
+    }
+
+    receive() external payable {}
+
 }

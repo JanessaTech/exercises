dce86097c0036acf98fa50da3a6060d53000edbc
Janessa <lvlajzhao1983@gmail.com>
2025-06-04 21:52:47 +0800

    redo 2. AnyOf

--- typescript/practice2/src/type-challenge/re-do/ReDo.ts
+++ typescript/practice2/src/type-challenge/re-do/ReDo.ts
@@ -2,12 +2,22 @@ import { Alike, Expect, Equal, NotEqual, ExpectExtends } from "../test-utils";
 
 
 type cases = [
-  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
-  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
-  Expect<Equal<TupleToNestedObject<['a', 'b', 'c'], boolean>, { a: { b: { c: boolean } } }>>,
-  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>,
+  Expect<Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[1, '', false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, 'test', false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', true, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [1], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { 1: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], {}, undefined, null]>, false>>,
+  Expect<Equal<AnyOf<[]>, false>>,
 ]
 
-type TupleToNestedObject<T, U> = T extends [infer F extends string, ...infer R]
-? {[P in F]: TupleToNestedObject<R, U>}
-: U
+type falsy = [0, '', false, [], {[P in any]: never}, undefined, null][number]
+
+type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
+? F extends falsy 
+  ? AnyOf<R>
+  : true
+: false

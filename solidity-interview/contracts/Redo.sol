3bcd0bf9595742e9a873a29a2160cef283b99d0c
Janessa <lvlajzhao1983@gmail.com>
2025-06-05 20:10:54 +0800

    Redo ERC20

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -3,21 +3,24 @@ pragma solidity ^0.8.20;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Redo {
-   function sumArray(uint256[] memory arr) public pure returns(uint256) {
-    uint256 sum;
-    assembly {
-        let length := mload(arr)
-        let ptr := add(arr, 0x20)
-        for {let i := 0} lt(i, length) { i := add(i, 1)} {
-            sum:= add(sum, mload(ptr))
-            ptr := add(ptr, 0x20)
-        }
+contract Redo is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor(address defaultAdmin, address minter, address burner) ERC20("MyToken", "MTK") {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+        _grantRole(BURNER_ROLE, burner);
     }
-    return sum;
 
-   }
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE){
+        require(balanceOf(to) >= amount, 'not enough eth to burn');
+        _burn(to, amount);
+    }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,19 +6,39 @@ const { extendEnvironment } = require("hardhat/config");
 
 describe('Redo', function () {
     async function deployRedoFixture() {
+        const [admin, minter, burner, Bob, non, ...others] = await ethers.getSigners()
         const Redo = await ethers.getContractFactory('Redo')
-        const redo = await Redo.deploy()
-        return {redo}
+        const redo = await Redo.deploy(admin, minter, burner)
+        return {redo, admin, minter, burner, Bob, non}
     }
 
-    describe('sumArray', function () {
-        it('sumArray', async function () {
-            const {redo} = await loadFixture(deployRedoFixture)
-            const sum = await redo.sumArray([1, 2, 3])
-            expect(sum).to.be.equal(6)
+    describe('mint', function () {
+        it('it minted successfull', async function () {
+            const {redo, Bob, minter} = await loadFixture(deployRedoFixture)
+            const amount = 1000
+            await redo.connect(minter).mint(Bob.getAddress(), amount)
+            const balance = await redo.balanceOf(Bob.getAddress())
+            expect(balance).to.be.equal(amount)
 
+        })   
+    })
+    describe('burn', function () {
+        it('it burned successfully', async function () {
+            const {redo, Bob, minter, burner} = await loadFixture(deployRedoFixture)
+            const amount = 1000
+            const toBurn = 300
+            await redo.connect(minter).mint(Bob.getAddress(), amount)
+            await redo.connect(burner).burn(Bob.getAddress(), toBurn)
+            const balance = await redo.balanceOf(Bob.getAddress())
+            expect(balance).to.be.equal(amount - toBurn)
         })
-        
+        it('it failed to burn when it is not burner', async function () {
+            const {redo, Bob, minter, non} = await loadFixture(deployRedoFixture)
+            const amount = 1000
+            const toBurn = 300
+            await redo.connect(minter).mint(Bob.getAddress(), amount)
+            await expect(redo.connect(non).burn(Bob.getAddress(), toBurn)).to.be.revertedWithCustomError(redo, 'AccessControlUnauthorizedAccount')
+        })
     })
 })
 

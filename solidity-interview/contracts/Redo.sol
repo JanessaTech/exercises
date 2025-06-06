f9b7a6aad8aa515c81dcad77a41b201ce9fa377c
Janessa <lvlajzhao1983@gmail.com>
2025-06-06 09:14:09 +0800

    Redo auction

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -3,24 +3,72 @@ pragma solidity ^0.8.20;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract Redo is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+contract Redo {
+    IERC721 nft;
+    uint256 nftId;
 
-    constructor(address defaultAdmin, address minter, address burner) ERC20("MyToken", "MTK") {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
-        _grantRole(BURNER_ROLE, burner);
+    address owner;
+
+    bool started;
+    uint256 endAt;
+
+    mapping(address => uint256) bids;
+    uint256 highestBid;
+    address highestBidder;
+
+    event Start(address indexed from);
+    event End(address indexed from);
+    event Bid(address indexed from, uint256 amount);
+    event Withdraw(address indexed from, uint256 amount);
+
+    constructor(address _nft, uint256 _nftId) {
+        nft = IERC721(_nft);
+        nftId = _nftId;
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+    function start() public {
+        require(msg.sender == owner, "not owner");
+        require(!started, "started");
+        started = true;
+        endAt = block.timestamp;
+        nft.transferFrom(msg.sender, address(this), nftId);
+        emit Start(msg.sender);
     }
-    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE){
-        require(balanceOf(to) >= amount, 'not enough eth to burn');
-        _burn(to, amount);
+
+    function bid() public payable {
+        require(started, "not started");
+        require(block.timestamp < endAt, "ended");
+        require(msg.value > highestBid, "msg.value <= highestBid");
+        if (highestBidder != address(0)) {
+            bids[highestBidder] += highestBid;
+        }
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+        emit Bid(msg.sender, msg.value);
+    }
+
+    function withdraw() public {
+        uint256 amount = bids[msg.sender];
+        bids[msg.sender] = 0;
+        require(amount > 0, "no eth");
+        bool success = payable(msg.sender).send(amount);
+        require(success, "failed to withdraw");
+        emit Withdraw(msg.sender, amount);
+    }
+
+    function end() public {
+        require(msg.sender == owner, "not owner");
+        require(started, "not started");
+        require(block.timestamp >= endAt, "not ended");
+        if (highestBidder != address(0)) {
+            nft.transferFrom(address(this), highestBidder, nftId);
+            bool success = payable(owner).send(highestBid);
+            require(success, "failed to pay owner");
+        } else {
+            nft.transferFrom(address(this), owner, nftId);
+        }
+        emit End(msg.sender);
     }
 }

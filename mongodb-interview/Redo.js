d201573e169b82cf9b0e9f57ffe5b8c7ce156e3f
Janessa <lvlajzhao1983@gmail.com>
2025-06-10 21:45:23 +0800

    redo - bulk ops

--- mongodb-interview/Redo.js
+++ mongodb-interview/Redo.js
@@ -1,20 +1,10 @@
 const {default: mongoose, Schema} = require('mongoose')
 
-
-const typeSchema = new Schema({
-    name: String,
-    age: Number,
-    isMale: Boolean,
-    birtheday: Date,
-    nested: {
-        addr: String,
-    },
-    lucky: [Number],
-    mixed: Schema.Types.Mixed,
-    map: Map
+const bulkSchema  = new Schema({
+    _id: Number,
+    data: String
 })
-
-const MyType = mongoose.model('MyType', typeSchema)
+const Bulk = mongoose.model('Bulk', bulkSchema)
 
 function connect() {
     mongoose.connect('mongodb://127.0.0.1/interview')
@@ -29,53 +19,40 @@ function connect() {
 
 async function create() {
     try {
-        const t1 = new MyType
-        t1.name = 'Jane'
-        t1.age = 10
-        t1.birtheday = new Date()
-        t1.nested.addr = 'xian'
-        t1.lucky = [1, 2, 3, 4]
-        t1.mixed = [{xx: 'aa'}, [12]]
-        t1.map = new Map([['1', 'value1'], ['2', 'value2']])  // the key must be string type
-
-        const t2 = new MyType
-        t2.name = 'Leo'
-        t2.age = 11
-        t2.birtheday = new Date()
-        t2.nested.addr = 'beijing'
-        t2.lucky.unshift(5, 6, 7, 8)  // pay attention to how to insert elements into array
-        t2.mixed = [{xx: 'aa'}, [12]]
-        t2.map = new Map([['3', 'value3'], ['4', 'value4']])  // the key must be string type
-
-        await t1.save()
-        await t2.save()
-    } catch(err) {
+        await Bulk.insertMany([
+            {_id: 1, data: 'data1'},
+            {_id: 2, data: 'data2'},
+            {_id: 3, data: 'data3'},
+        ])
+    } catch (err) {
         console.log(err)
     }
     console.log('data is created')
 }
 
+async function init() {
+    await Bulk.collection.drop()
+    await create()
+}
+
+async function bulkOp() {
+    await init()
+    const res = await Bulk.bulkWrite([
+        {insertOne: {document: {_id: 4, data: 'data4'}}},
+        {updateOne: {filter: {_id: 2}, update: {$set: {data: 'data2_new'}}}},
+        {deleteOne: {filter: {_id: 3}}}
+    ], {ordered: true})
+}
 async function query() {
-    const agg = await MyType.aggregate([
-        {
-            $match: {
-                birtheday: {$gte: new Date('20250501T00:00:00Z')}
-            }
-        },
-        {
-            $group: {
-                _id: {$dateToString: {format: "%Y-%m-%d", date: "$birtheday"}},
-                cnt: {$sum: 1}
-            }
-        }
-    ])
-    console.log(agg)
+    const res = await Bulk.find()
+    console.log(res)
 }
 
 async function main() {
     try {
         connect()
-        //await create()
+        await create()
+        await bulkOp()
         await query()
     } catch (err) {
         console.log(err)

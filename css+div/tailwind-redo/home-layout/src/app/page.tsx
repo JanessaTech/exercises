8d6225e3e69518ff163244b8512906e53f2c7a37
Janessa <lvlajzhao1983@gmail.com>
2025-06-11 10:10:18 +0800

    redo - layout for home

--- css+div/tailwind-redo/home-layout/src/app/page.tsx
+++ css+div/tailwind-redo/home-layout/src/app/page.tsx
@@ -2,20 +2,17 @@
 
 export default function Home() {
   return (
-   <div className="flex w-full">
-    <div className="w-[200px] h-screen sticky top-0 bg-yellow-400"></div>
-    <main className="grow">
-      <div className="h-20 bg-black text-white sticky top-0">header</div>
-      <div className="h-28 bg-purple-400"></div>
-      <div className="sticky top-20 h-12 bg-red-400">table header</div>
-      <div className="bg-green-300 h-40">item1</div>
-      <div className="bg-green-300 h-40">item2</div>
-      <div className="bg-green-300 h-40">item3</div>
-      <div className="bg-green-300 h-40">item4</div>
-      <div className="bg-green-300 h-40">item5</div>
-      <div className="bg-green-300 h-40">item6</div>
-    </main>
-
+   <div className="w-full flex">
+    <div className="w-[200px] h-screen bg-green-200 sticky top-0"></div>
+    <div className="grow bg-zinc-300">
+      <div className="h-20 text-white sticky top-0 bg-black">header</div>
+      <div className="h-20 bg-yellow-400"></div>
+      <div className="sticky top-20 bg-green-900">table header</div>
+      <div className="bg-pink-300 h-72">item1</div>
+      <div className="bg-pink-300 h-72">item2</div>
+      <div className="bg-pink-300 h-72">item3</div>
+      <div className="bg-pink-300 h-72">item4</div>
+    </div>
    </div>
   );
 }

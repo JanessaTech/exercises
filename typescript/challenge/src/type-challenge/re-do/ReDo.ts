0746d5632d9e38bfa95c8d99e00fc5708ca09fde
Janessa <lvlajzhao1983@gmail.com>
2025-06-11 10:16:54 +0800

    redo TS -diff

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,19 +1,35 @@
 import { Equal, Expect } from "../test-utils"
 
 
+type Foo = {
+  name: string
+  age: string
+}
+type Bar = {
+  name: string
+  age: string
+  gender: number
+}
+type Coo = {
+  name: string
+  gender: number
+}
+
 type cases = [
-  Expect<Equal<Permutation<'A'>, ['A']>>,
-  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
-  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
-  Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
-  Expect<Equal<Permutation<never>, []>>,
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string, gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string, gender: number }>>,
 ]
 
+type Diff<T, U> = {
+  [P in keyof (T & U) as Exclude<P, keyof(U | T)>]: P extends keyof T
+  ? T[P]
+  : P extends keyof U
+    ? U[P]
+    : never
+}
 
-
-
-type Permutation<L, path extends unknown[] = [], A = L> = [L] extends [never]
-? path
-: L extends any
-  ? Permutation<Exclude<A, L>, [...path, L]>
-  : never
+// type Diff<T, U> = {
+//   [P in keyof (T & U) as P extends keyof (T | U) ? never : P]: (T & U)[P]
+// }

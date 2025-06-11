211b0274de487eed1c18842bac67d81ea2817373
Janessa <lvlajzhao1983@gmail.com>
2025-06-11 10:25:25 +0800

    redo TS - AllCombinations

--- typescript/challenge/src/type-challenge/medium/AllCombinations.ts
+++ typescript/challenge/src/type-challenge/medium/AllCombinations.ts
@@ -8,24 +8,22 @@ type cases = [
     Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
   ]
 
-  type StringToUnion<S> = S extends `${infer F}${infer R}`
-  ? F | StringToUnion<R>
+type StringToUnion<S> = S extends `${infer F}${infer R}`
+? F | StringToUnion<R>
+: never
+
+type ArrayToString<A> = A extends any
+? A extends [infer F, ...infer R]
+  ? `${string &F}${ArrayToString<R>}`
+  : ''
+: never
+
+type Com<U, path extends unknown[] = [], acc = never, A = U> = [U] extends [never]
+? ArrayToString<acc | path>
+: U extends any
+  ? Com<Exclude<A, U>, [...path, U], acc | path>
   : never
+
+type AllCombinations<S extends string> = Com<StringToUnion<S>>
   
-  type ArrayToString<A> = A extends any
-  ? A extends [infer F, ...infer R]
-    ? `${string &F}${ArrayToString<R>}`
-    : ''
-  : never
-  
-  type Com<U, path extends unknown[] = [], acc = never, A = U> = [U] extends [never]
-  ? ArrayToString<acc | path>
-  : U extends any
-    ? Com<Exclude<A, U>, [...path, U], acc | path>
-    : never
-  
-  type AllCombinations<S extends string> = Com<StringToUnion<S>>
-  
-  
-  type test = StringToUnion<'AB'>
-  type test1 = Com<StringToUnion<'AB'>>
+
--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,35 +1,32 @@
 import { Equal, Expect } from "../test-utils"
 
 
-type Foo = {
-  name: string
-  age: string
-}
-type Bar = {
-  name: string
-  age: string
-  gender: number
-}
-type Coo = {
-  name: string
-  gender: number
-}
-
 type cases = [
-  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
-  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
-  Expect<Equal<Diff<Foo, Coo>, { age: string, gender: number }>>,
-  Expect<Equal<Diff<Coo, Foo>, { age: string, gender: number }>>,
+  Expect<Equal<AllCombinations<''>, ''>>,
+  Expect<Equal<AllCombinations<'A'>, '' | 'A'>>,
+  Expect<Equal<AllCombinations<'AB'>, '' | 'A' | 'B' | 'AB' | 'BA'>>,
+  Expect<Equal<AllCombinations<'ABC'>, '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'>>,
+  Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
 ]
 
-type Diff<T, U> = {
-  [P in keyof (T & U) as Exclude<P, keyof(U | T)>]: P extends keyof T
-  ? T[P]
-  : P extends keyof U
-    ? U[P]
-    : never
-}
+type StringToUnion<S> = S extends `${infer F}${infer R}`
+? F | StringToUnion<R>
+: never
 
-// type Diff<T, U> = {
-//   [P in keyof (T & U) as P extends keyof (T | U) ? never : P]: (T & U)[P]
-// }
+type ArrayToString<A> = A extends any
+? A extends [infer F, ...infer R]
+  ? `${string &F}${ArrayToString<R>}`
+  : ''
+: never
+
+type Com<U, path extends unknown[] = [], acc = never, A = U> = [U] extends [never]
+? ArrayToString<acc | path>
+: U extends any
+  ? Com<Exclude<A, U>, [...path, U], acc | path>
+  : never
+
+type AllCombinations<S extends string> = Com<StringToUnion<S>>
+
+
+type test = StringToUnion<'AB'>
+type test1 = Com<StringToUnion<'AB'>>

cf390d9d41d481d48dbd2ddc24c91af5b3359e64
Janessa <lvlajzhao1983@gmail.com>
2025-06-11 09:45:57 +0800

    redo - solidity array sum by assembly

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -3,39 +3,18 @@ pragma solidity ^0.8.20;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
 contract Redo {
-    struct Person {
-        uint256 id;
-        string name;
-    }
-    uint256 idx;
-    Person[] people;
-    mapping(uint256 => uint256) idxMapping;
-    mapping(uint256 => bool) inserted;
-
-    function create(string memory _name) public {
-        uint256 _id = idx++;
-        people.push(Person({id: _id, name: _name}));
-        idxMapping[_id] = people.length - 1;
-        inserted[_id] = true;
-    }
-    function remove(uint256 _id) public {
-        require(inserted[_id], "invalid id");
-        uint256 _idx = idxMapping[_id];
-        Person storage last = people[people.length - 1];
-        people[_idx] = last;
-        idxMapping[last.id] = _idx;
-        delete idxMapping[_id];
-        delete inserted[_id];
-        people.pop();
-    }
-    function get(
-        uint256 _id
-    ) public view returns (uint256 id, string memory name) {
-        require(inserted[_id], "invalid id");
-        Person storage person = people[idxMapping[_id]];
-        return (person.id, person.name);
+    function sumArray(uint256[] memory arr) public pure returns(uint256) {
+        uint256 sum;
+        assembly {
+            let length := mload(arr)
+            let ptr := add(arr, 0x20)
+            for { let i := 0} lt(i, length) { i := add(i, 1)} {
+                sum := add(sum, mload(ptr))
+                ptr := add(ptr, 0x20)
+            }
+            
+        }
+        return sum;
     }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -10,35 +10,13 @@ describe('Redo', function () {
         const redo = await Redo.deploy();
         return {redo}
     }
-    describe('create', function () {
-        it('create', async function () {
+    describe('addSum', function () {
+        it('adddSum', async function () {
             const {redo} = await loadFixture(deployRedoFixture)
-            await redo.create('person0') 
-            await redo.create('person1')
-            const person0 = await redo.get(0)
-            const person1 = await redo.get(1)
-            expect(person0.name).to.be.equal('person0')
-            expect(person1.name).to.be.equal('person1')
+            const sum = await redo.sumArray([1, 2, 3])
+            expect(sum).to.be.equal(sum)
         })
     })
-    describe('remove', function () {
-        it('remove', async function () {
-            const {redo} = await loadFixture(deployRedoFixture)
-            await redo.create('person0')
-            await redo.create('person1')
-            await redo.create('person2')
-
-            await redo.remove(1)
-
-            const person0 = await redo.get(0)
-            const person2 = await redo.get(2)
-            await expect(redo.get(1)).to.be.revertedWith('invalid id')
-            expect(person0.name).to.be.equal('person0')
-            expect(person2.name).to.be.equal('person2')
-        })
-    })
-
-    
 })
 
 

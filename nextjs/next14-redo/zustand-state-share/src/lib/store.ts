0831fa359db78a6e7bd9387de6117f7f0ca2b12f
Janessa <lvlajzhao1983@gmail.com>
2025-06-13 09:52:11 +0800

    redo zustand state sharing

--- nextjs/next14-redo/zustand-state-share/src/lib/store.ts
+++ nextjs/next14-redo/zustand-state-share/src/lib/store.ts
@@ -5,32 +5,29 @@ import {persist, createJSONStorage} from 'zustand/middleware'
 interface NoteState {
     notes: string[];
     isDone: boolean;
-    setIsDone: (done: boolean) => void;
     addNote: (note: string) => void;
+    setIsDone: (done: boolean) => void;
     clear: () => void
 }
 
-const useAddNote = create<NoteState>()(
-    persist(
-        (set) => ({
-            notes: [],
-            isDone: true,
-            addNote: (note) => set((state) => ({notes: [...state.notes, note]})),
-            setIsDone: (done) => set({isDone: done}),
-            clear: () => {
-                localStorage.removeItem('note_state')
-                set({notes: []})
-            }
-        }),
-        {
-            name: '',
-            storage: createJSONStorage(() => localStorage),
-            onRehydrateStorage: () => (state) => {
-                state?.setIsDone(true)
-            }
+const useAddNote = create<NoteState>()(persist(
+    (set) => ({
+        notes: [],
+        isDone: false,
+        addNote : (note) => set((state) => ({notes: [...state.notes, note]})),
+        setIsDone: (done) => set({isDone: done}),
+        clear: () => {
+            localStorage.removeItem('note_state')
+            set({notes: []})
         }
-    )
-    
-)
+    }),
+    {
+        name: 'note_state',
+        storage: createJSONStorage(() => localStorage),
+        onRehydrateStorage: () => (state) => {
+            state?.setIsDone(true)
+        }
+    }
+))
 
 export default useAddNote

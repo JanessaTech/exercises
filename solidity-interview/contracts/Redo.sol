f295dae137fe91d05805814fd9ca3cc505711f35
Janessa <lvlajzhao1983@gmail.com>
2025-06-15 19:35:23 +0800

    redo solidity checks-affects-interactions pattern

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -5,30 +5,26 @@ pragma solidity ^0.8.20;
 
 contract Redo {
     bool private locking;
-    mapping(address => uint256) balances;
-
     event Deposit(address indexed from, uint256 amount);
     event Withdraw(address indexed from, uint256 amount);
-
-    modifier non_reentrance() {
-        require(!locking, 'enentrance');
+    modifier non_reEntrant() {
+        require(!locking, 'reentrant');
         locking = true;
         _;
         locking = false;
     }
-
-    function deposit()  public payable{
+    mapping(address => uint256) balances;
+    function deposit() public payable{
         balances[msg.sender] += msg.value;
         emit Deposit(msg.sender, msg.value);
     }
-
-    function withdraw() public non_reentrance {
+    function withdraw() public payable{
         uint256 amount = balances[msg.sender];
-        require(amount > 0, 'no eth');
+        require(amount > 0, 'no eth left');
+        balances[msg.sender] = 0;
         (bool success, ) = payable(msg.sender).call{
             value: amount,
             gas: 2300
-        
         }("");
         require(success, 'failed to withdraw');
         emit Withdraw(msg.sender, amount);

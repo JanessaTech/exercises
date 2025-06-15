63463599d4b46de0ff106a6b36ea37dd8ba41bab
Janessa <lvlajzhao1983@gmail.com>
2025-06-15 17:36:24 +0800

    redo use $set and $inc

--- mongodb-interview/Redo.js
+++ mongodb-interview/Redo.js
@@ -1,15 +1,13 @@
 const {default: mongoose, Schema} = require('mongoose')
-const gradeSchema = new Schema({
-    grade: Number,
-    mean: Number,
-    std: Number
-})
-const examSchema = new Schema({
-    rating: [Number],
-    grades: [gradeSchema]
+
+const studentSchema = new Schema({
+    name: String,
+    age: Number,
+    addr: String
 })
 
-const Exam = mongoose.model('Exam', examSchema)
+const Student = mongoose.model('Student', studentSchema)
+
 
 function connect() {
     mongoose.connect('mongodb://127.0.0.1/interview')
@@ -23,207 +21,37 @@ function connect() {
 }
 
 async function create() {
+    const stu1 = new Student({name: 'stu1', age: 10, addr: 'xian'})
+    const stu2 = new Student({name: 'stu2', age: 20, addr: 'xian'})
+    const stu3 = new Student({name: 'stu3', age: 30, addr: 'xian'})
     try {
-        await Exam.insertMany([
-            {rating: [85, 80, 80], grades:[{grade: 80, mean: 75, std:8}, 
-                                           {grade: 85, mean: 90, std:5}, 
-                                           {grade: 85, mean: 85, std:8}]},
-
-            {rating: [88, 90, 92], grades:[{grade: 180, mean: 75, std:8}, 
-                                           {grade: 185, mean: 90, std:5}, 
-                                           {grade: 185, mean: 85, std:8}]},
-
-            {rating: [80, 100, 90], grades:[{grade: 280, mean: 75, std:8}, 
-                                            {grade: 285, mean: 90, std:5}, 
-                                            {grade: 285, mean: 85, std:8}]}
-        ])
+        await stu1.save()
+        await stu2.save()
+        await stu3.save()
     } catch(err) {
         console.log(err)
     }
-    console.log('data is created')
+    console.log('data is created!')
 }
 
-async function init() {
-    await Exam.collection.drop()
-    await create()
+// for stu1, increment age by 2
+async function update_inc() {
+   const res = await Student.findOneAndUpdate({name: 'stu1'}, {$inc: {age: 2}}, {new: true})
+    console.log(JSON.stringify(res, null, 2))
 }
-// pick up the first document with rating which has 80 in it. update the the first 80 to 100
-// pay more attention: the array field must appear as part of the query document
-async function update_$() {
-    await init()
-    await Exam.updateOne({rating: 80}, {$set: {"rating.$": 100}})
+// for stu2, set age=40 and add=shanghai
+async function update_set() {
+   const res  = await Student.findOneAndUpdate({name: 'stu2'}, {$set: {age: 40, addr: 'shanghai'}}, {new: true})
+    console.log(JSON.stringify(res, null, 2))
 }
-// pick up the fist document with grades which has grade being 185, update  std to 20 for the first matched element in grades
-async function update_$_embbeded() {
-    await init()
-    await Exam.updateOne(
-        {"grades.grade": 185},
-        {$set: {"grades.$.std": 20}}
-    )
-}
-    
-// pick up the first document with grades, 
-// in which there is at least one element, of which the grade > 200 and mean < 90, 
-// update std to 200 for the first matched element in grades
-async function update_$_multiple() {
-    await init()
-    await Exam.updateOne(
-        {grades: {$elemMatch: {grade: {$gt: 200}, mean: {$lt: 90}}}},
-        {$set: {"grades.$.std": 200}}
-    )
-}
-
-// pick up the the first document in which rating has 88 in it, update all elements in the array to 888 
-async function update_$_all(){
-    await init()
-    await Exam.updateOne(
-        {rating: 88},
-        {$set: {"rating.$[]": 888}}
-    )
-    
-}
-
-// pick up the the first document in which rating has 88 in it, update std to 20 in all elements in the grades for the matched document
-async function update_$_all_embbeded() {
-    await init()
-    await Exam.updateOne(
-        {rating: 88},
-        {$set: {"grades.$[].std": 20}}
-    )
-}
-
-// pick up the the first document in which rating has 88 in it, update elements greater or equal to 90 in it to 100
-async function update_$_identifier() {
-    await init()
-    await Exam.updateOne(
-        {rating: 88},
-        {$set: {"grades.$[elem]": 100}},
-        {arrayFilters: [{"elem":{$gte: 99}}]}
-    )
-}
-
-//pick up the first document in which rating has 88 in it, update std to 20 for the all elements whose grade == 185 and mean >=80 in the document
-async function update_$_identifier_embbeded() {
-    await init()
-    await Exam.updateOne(
-        {rating :88},
-        {$set: {"grades.$[elem].std": 20}},
-        {arrayFilters: [{"elem.grade": {$eq: 185}, "elem.mean": {$gte: 85}}]}
-    )
-}
-
-//pick up the first document in which rating has 88 in it, add 93 into rating array(92 will be ignored)
-async function update_$_addToSet() {
-    await init()
-    await Exam.updateOne({rating: 88},  {$addToSet: {rating: [92, 93]}})
-}
-//pick up the first document in which rating has 88 in it, 
-// remove the first element in grades ({grades: 1} means remove the last element in grades)
-async function update_$_pop() {
-    await init()
-    await Exam.updateOne({rating: 88}, {$pop: {grades: -1}})
-}
-
-//pick up the first document in which rating has 88 in it,  
-// append 100, 200, 300 to rating array
-async function update_$_push() {
-    await init()
-    await Exam.updateOne(
-        {rating: 88}, 
-        {$push: {rating: {$each: [100, 200, 300]}}})
-}
-//pick up the first document in which rating has 88 in it,  
-// insert 100, 200, 300 to rating at position 1
-async function update_$_position() {
-    await init()
-    await Exam.updateOne(
-        {rating: 88},
-        {$push: {
-            rating: {
-                $each: [100, 200, 300],
-                $position: 1
-            }
-        }}
-    )
-}
-//pick up the first document in which rating has 88 in it,  
-// insert 100, 200, 300 to rating at position 1
-// once insertion is done, slice 3 elements from the head as the rating
-async function update_$_slice() {
-    await init()
-    await Exam.updateOne(
-        {rating: 88},
-        {$push: {
-            rating: {
-                $each: [100, 200, 300],
-                $position: 1,
-                $slice: 3
-            }
-        }}
-    )
-}
-
-// pick up the first document in which rating has 88 in it,
-// insert {grade: 190, mean: 92, std: 7}, {grade: 160, mean: 92, std: 5}, {grade: 181, mean: 92, std: 9} to grades
-// sort grades by grade in ascending order
-// slice 3 elements from grades as the result
-async function update_$_sort() {
-    await init()
-    await Exam.updateOne(
-        {rating: 88}, 
-        {$push: {
-            grades: {
-                $each: [{grade: 190, mean: 92, std: 7}, {grade: 160, mean: 92, std: 5}, {grade: 181, mean: 92, std: 9}],
-                $slice: 3,
-                $sort: {grade: 1}
-            }
-        }}
-    )
-}
-
-// first
-// pick up the first document in which rating has 88 in it, remove elements >= 90 from rating
-// second
-// pick up the first document in which rating has 88 in it, remove elements in grades in which grade =185 and mean>=90
-async function update_$_pull() {
-    await init()
-    await Exam.updateOne(
-        {rating: 88},
-        {$pull : {
-            rating: {$gte: 90}
-        }}
-    )
-}
-
-//pick up the first document in which rating has 88 in it, remove elements specified by [90, 92, 93] from rating
-async function update_$_pullAll() {
-    await init()
-    await Exam.updateOne(
-        {rating: 88},
-        {$pullAll: {rating: [90, 92, 93]}}
-    )
-}
-
 async function main() {
     try {
         connect()
         //await create()
-        //await update_$()
-        //await update_$_embbeded()
-        //await update_$_multiple()
-        //await update_$_all()
-        //await update_$_all_embbeded()
-        //await update_$_identifier()
-        //await update_$_identifier_embbeded()
-        //await update_$_addToSet()
-        //await update_$_pop()
-        //await update_$_push()
-        //await update_$_position()
-        //await update_$_slice()
-        //await update_$_sort()
-        //await update_$_pull()
-        //await update_$_pullAll()
-    } catch (err) {
+        //await update_inc()
+        await update_set()
+        //await query()
+    } catch(err) {
         console.log(err)
     }
 }

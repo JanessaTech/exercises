9af8fe8328dbaac0ba09fc7d1c6651fcb825bce6
Janessa <lvlajzhao1983@gmail.com>
2025-06-15 22:24:18 +0800

    redo upgrade proxy

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -5,71 +5,48 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "hardhat/console.sol";
 
 contract Redo {
-    IERC721 nft;
-    uint256 nftId;
+    uint256 public value;
+    
+    bytes32 private constant ADMIN_SLOT = keccak256('ADMIN_SLOT');
+    bytes32 private constant IMPLEMENTATION_SLOT = keccak256('IMPLEMENTATION_SLOT');
 
-    address owner;
-    bool started;
-    uint256 endAt;
-
-    address highestBidder;
-    uint256 highestBid;
-    mapping(address => uint256) bids;
-
-    event Start(address indexed from);
-    event End(address indexed from);
-    event Bid(address indexed from, uint256 amount);
-    event Withdraw(address indexed from, uint256 amount);
-
-    constructor(address _nft, uint256 _nftId) {
-        nft = IERC721(_nft);
-        nftId = _nftId;
-        owner = msg.sender;
-    }
-
-    function start() public {
-        require(msg.sender == owner, 'not owner');
-        require(!started, 'started');
-        started = true;
-        endAt = block.timestamp + 7 days;
-        nft.transferFrom(msg.sender, address(this), nftId);
-
-    }
-
-    function bid() public payable{
-        require(started, 'not started');
-        require(block.timestamp < endAt, 'ended');
-        require(msg.value > highestBid, 'msg.value <= highestBid');
-        if (highestBidder != address(0)) {
-            bids[highestBidder] += highestBid;
+    constructor(address _impelmentation) {
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            sstore(slot, _impelmentation)
         }
-        highestBid = msg.value;
-        highestBidder = msg.sender;
-        emit Bid(msg.sender, msg.value);
-    }
-
-    function withdraw() public {
-        uint256 amount = bids[msg.sender];
-        bids[msg.sender] = 0;
-        if (amount > 0) {
-            bool sent = payable(msg.sender).send(amount);
-            require(sent, 'failed to withdraw');
+        slot = ADMIN_SLOT;
+        assembly {
+            sstore(slot, caller())
         }
-        emit Withdraw(msg.sender, amount);
     }
-
-    function end() public {
-        require(started, 'not started');
-        require(owner == msg.sender, 'not owner');
-        require(block.timestamp >= endAt, 'not ended');
-        if (highestBidder != address(0)) {
-            nft.transferFrom(address(this), highestBidder, nftId);
-            bool success = payable(owner).send(highestBid);
-            require(success, 'failed to pay owner');
-        } else {
-            nft.transferFrom(address(this), owner, nftId);
+    function admin() public view returns(address adm) {
+        bytes32 slot = ADMIN_SLOT;
+        assembly {
+            adm := sload(slot)
         }
-
-        emit End(msg.sender);
     }
+    function implememtation() public view returns(address impl) {
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            impl := sload(slot)
+        }
+    }
+    function toUpgrade(address _implemenation) public {
+        require(admin() == msg.sender, 'not owner');
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            sstore(slot, _implemenation)
+        }
+    }
+
+    function _delegate(address _implementation) private {
+        (bool success, ) = _implementation.delegatecall(msg.data);
+        require(success, 'failed to call delegatecall');
+    }
+
+    fallback() external payable{
+        _delegate(implememtation());
+    }
+    receive() external payable{}
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,130 +6,51 @@ const { extendEnvironment } = require("hardhat/config");
 
 describe('Redo', function () {
     async function deployRedoFixture() {
-        const [admin, Bob, nonBob, userA, userB, ...others] = await ethers.getSigners()
-        const MyERC721 = await ethers.getContractFactory('MyERC721')
-        const name = 'MyERC721'
-        const symbol = 'MyERC721'
-        const erc721 = await MyERC721.deploy(name, symbol)
-        const nftId = 1
-        erc721.mint(Bob.getAddress(), nftId)
+        const [admin, ...others] = await ethers.getSigners()
+        const LogicV1 = await ethers.getContractFactory('LogicV1')
+        const logicV1 = await LogicV1.deploy()
+        const LogicV2 = await ethers.getContractFactory('LogicV2')
+        const logicV2 = await LogicV2.deploy()
 
-        const Redo = await ethers.getContractFactory('Redo', Bob)
-        const redo = await Redo.deploy(erc721.getAddress(), nftId);
-        await erc721.connect(Bob).approve(redo.getAddress(), nftId)
-        const seven_days = 60 * 60 * 24 * 7
-        return {redo, Bob, nonBob, erc721, nftId, userA, userB, seven_days}
+        const Proxy = await ethers.getContractFactory('Redo')
+        const proxy = await Proxy.deploy(logicV1.getAddress())
+        return {proxy, admin, logicV1, logicV2}
     }
-    describe('init', function () {
-        it('init', async function () {
-            const {redo, erc721, Bob, nftId} = await loadFixture(deployRedoFixture)
-            const owner = await erc721.ownerOf(nftId)
-            const spender = await erc721.getApproved(nftId)
-            expect(owner).to.be.equal(await Bob.getAddress())
-            expect(spender).to.be.equal(await redo.getAddress())
-        })
-    })
-    describe('start', function() {
-        it('it failed to start when it is not owner', async function () {
-            const {redo, Bob, nonBob} = await loadFixture(deployRedoFixture)
-            await expect(redo.connect(nonBob).start()).to.be.revertedWith('not owner')
-        })
-        it('it started successfully', async function () {
-            const {redo, Bob, erc721, nftId} = await loadFixture(deployRedoFixture)
-            await redo.start()
-            const owner = await erc721.ownerOf(nftId)
-            expect(owner).to.be.equal(await redo.getAddress())
-        })
-        it('it failed to start whhen it is already started', async function () {
-            const {redo} = await loadFixture(deployRedoFixture)
-            await redo.start()
-            await expect(redo.start()).to.be.revertedWith('started')
-        })
-    })
-    describe('bid', function () {
-        it('it failed to start when it is not started', async function () {
-            const {redo, userA} = await loadFixture(deployRedoFixture)
-            const amount1 = 1000
-            await expect(redo.connect(userA).bid({value: amount1})).to.be.revertedWith('not started')
+
+    describe('LogicV1 & LogicV2', function () {
+        it('LogicV1', async function () {
+            const {proxy, admin} = await loadFixture(deployRedoFixture)
+            const abi = ['function setValue(uint256) external']
+            const iface = new ethers.Interface(abi)
+            const amount = 1000
+            const cdata = iface.encodeFunctionData('setValue(uint256)', [amount])
+            const tx = {
+                to: proxy.getAddress(),
+                data: cdata
+            }
+            await admin.sendTransaction(tx)
+            const val = await proxy.value()
+            expect(val).to.be.equal(amount)
 
         })
-        it('it failed to start when it is ended', async function () {
-            const {redo, userA, seven_days} = await loadFixture(deployRedoFixture)
-            const amount1 = 1000
-            await redo.start()
-            const latest = await time.latest()
-            await time.setNextBlockTimestamp(latest + seven_days)
-            await expect(redo.connect(userA).bid({value: amount1})).to.be.revertedWith('ended')
-
-        })
-        it('it failed to bid when msg.value <= highestBid', async function () {
-            const {redo, userA, userB}  = await loadFixture(deployRedoFixture)
-            const amount1 = 1000
-            const amount2 = 700
-            await redo.start()
-            await redo.connect(userA).bid({value: amount1})
-            await expect(redo.connect(userB).bid({value: amount2})).to.be.revertedWith('msg.value <= highestBid')
-        })
-        it('it bidded successfully', async function () {
-            const {redo, userA, userB}  = await loadFixture(deployRedoFixture)
-            const amount1 = 1000
-            const amount2 = 2000
-            await redo.start()
-            await redo.connect(userA).bid({value: amount1})
-            await redo.connect(userB).bid({value: amount2})
-        })
-    })
-    describe('withdraw', function () {
-        it('it withdrawn successffully', async function () {
-            const {redo, userA, userB} = await loadFixture(deployRedoFixture)
-            const amount1 = 1000
-            const amount2 = 2000
-            await redo.start()
-            await redo.connect(userA).bid({value: amount1})
-            await redo.connect(userB).bid({value: amount2})
-            await expect(redo.connect(userA).withdraw()).to.emit(redo, 'Withdraw').withArgs(userA.getAddress(), amount1)
-        })
-    })
-    describe('end', async function ( ){
-        it('it failed to end when it is not started', async function () {
-            const {redo} = await loadFixture(deployRedoFixture)
-            await expect(redo.end()).to.be.revertedWith('not started')
-        })
-        it('it failed to end when it is not owner', async function () {
-            const {redo, nonBob} = await loadFixture(deployRedoFixture)
-            await redo.start()
-            await expect(redo.connect(nonBob).end()).to.be.revertedWith('not owner')
-        })
-        it('it failed to end when it is not ended', async function () {
-            const {redo} = await loadFixture(deployRedoFixture)
-            await redo.start()
-            await expect(redo.end()).to.be.revertedWith('not ended')
-        }
-        )
-        it('it ended successfully when there is no bidder at all', async function () {
-            const {redo, seven_days, erc721, nftId, Bob}  = await loadFixture(deployRedoFixture)
-            await redo.start()
-            const latest = await time.latest()
-            await time.setNextBlockTimestamp(latest + seven_days)
-            await redo.end()
-            const owner = await erc721.ownerOf(nftId)
-            expect(owner).to.be.equal(await Bob.getAddress())
-        })
-        it('it ended successfully when there is at least 1 bidder', async function () {
-            const {redo, seven_days, userA, userB, erc721, nftId} = await loadFixture(deployRedoFixture)
-            await redo.start()
-            const latest = await time.latest()
-            const amount1 = 1000
-            const amount2 = 2000
-            await redo.connect(userA).bid({value: amount1})
-            await redo.connect(userB).bid({value: amount2})
-            await time.setNextBlockTimestamp(latest + seven_days)
-            await redo.end()
-            const owner = await erc721.ownerOf(nftId)
-            expect(owner).to.be.equal(await userB.getAddress())
+        it('LogicV2', async function () {
+            const {proxy, admin, logicV2} = await loadFixture(deployRedoFixture)
+            await proxy.toUpgrade(logicV2.getAddress())
+            const abi = ['function setValue(uint256) external']
+            const iface = new ethers.Interface(abi)
+            const amount = 1000
+            const cdata = iface.encodeFunctionData('setValue(uint256)', [amount])
+            const tx = {
+                to: proxy.getAddress(),
+                data: cdata
+            }
+            await admin.sendTransaction(tx)
+            const val = await proxy.value()
+            expect(val).to.be.equal(amount * 2)
 
         })
     })
+    
 })
 
 

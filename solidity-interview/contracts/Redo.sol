db84885fd2b4c5681fb705e338d51e1707b19811
Janessa <lvlajzhao1983@gmail.com>
2025-06-16 22:36:29 +0800

    redo - ERC721

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -4,26 +4,44 @@ pragma solidity ^0.8.20;
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "hardhat/console.sol";
 
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Redo is ERC20, ERC20Permit, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+contract Redo is ERC721, ERC721URIStorage, Ownable {
+    uint256 private nextTokenId;
+    constructor(address initialOwner)
+        ERC721("MyToken", "MTK")
+        Ownable(initialOwner)
+    {}
 
-    constructor(address defaultAdmin, address minter, address burner)
-        ERC20("MyToken", "MTK")
-        ERC20Permit("MyToken")
+    function _baseURI() internal pure override returns (string memory) {
+        return "http://test.com/";
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
     {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
-        _grantRole(BURNER_ROLE, burner);
+        return super.tokenURI(tokenId);
     }
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
-    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE) {
-        _burn(to, amount);
+
+    function mint(address to, string memory _tokenURI) public onlyOwner {
+        uint256 tokenId = nextTokenId++;
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
     }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,22 +6,22 @@ const { extendEnvironment } = require("hardhat/config");
 
 describe('Redo', function () {
     async function deployRedoFixture() {
-        const [admin, minter, burner, non, Bob, ...others] = await ethers.getSigners()
+        const [admin, Bob, ...others] = await ethers.getSigners()
         const Redo = await ethers.getContractFactory('Redo')
-        const redo = await Redo.deploy(admin, minter, burner)
-        return {redo, admin, minter, burner, non, Bob}
+        const redo = await Redo.deploy(admin.getAddress())
+        return {redo, admin, Bob}
     }
 
     describe('mint', function () {
-        it('it failed mint when it is not minter', async function () {
-            const {redo, non, Bob} = await loadFixture(deployRedoFixture)
-            const amount = 1000
-            await expect(redo.connect(non).mint(Bob.getAddress(), amount)).to.be.revertedWithCustomError(redo, 'AccessControlUnauthorizedAccount')
-        })
+       
         it('it minted successfully', async function () {
-            const {redo, minter, Bob} = await loadFixture(deployRedoFixture)
-            const amount = 1000
-            await expect(redo.connect(minter).mint(Bob.getAddress(), amount)).not.reverted
+            const {redo, admin, Bob} = await loadFixture(deployRedoFixture)
+            const uri = 'aaaa'
+            await redo.mint(Bob.getAddress(), uri)
+            const fullURI = await redo.tokenURI(0)
+            console.log(fullURI)
+            const owner = await redo.ownerOf(0)
+            expect(owner).to.be.equal(await Bob.getAddress())
         })
     })
 })

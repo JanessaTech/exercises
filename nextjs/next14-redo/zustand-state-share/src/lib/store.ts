92bc15130d9b98ea9f931c05e5e38e02dbc43b2f
Janessa <lvlajzhao1983@gmail.com>
2025-06-25 21:44:20 +0800

    redo state-sharing

--- nextjs/next14-redo/zustand-state-share/src/lib/store.ts
+++ nextjs/next14-redo/zustand-state-share/src/lib/store.ts
@@ -2,13 +2,13 @@
 import {create } from 'zustand'
 import {persist, createJSONStorage} from 'zustand/middleware'
 
+
 interface NoteState {
     notes: string[];
-    isDone: boolean;
     addNote: (note: string) => void;
+    isDone: boolean;
     setIsDone: (done: boolean) => void;
     clear: () => void
-
 }
 
 const useAddNote = create<NoteState>()(persist(
@@ -19,10 +19,9 @@ const useAddNote = create<NoteState>()(persist(
         setIsDone: (done) => set({isDone: done}),
         clear: () => {
             localStorage.removeItem('note_state')
-            set({notes:[]})
+            set({notes: []})
         }
-    }),
-    {
+    }), {
         name: 'note_state',
         storage: createJSONStorage(() => localStorage),
         onRehydrateStorage: () => (state) => {

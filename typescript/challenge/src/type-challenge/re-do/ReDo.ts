8cf35a7d539087f497a17ffb96ce6106a7552bc6
Janessa <lvlajzhao1983@gmail.com>
2025-06-27 09:43:46 +0800

    redo - Unique

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,26 +1,21 @@
 import { Alike, Equal, Expect } from "../test-utils"
 
-  
-
 type cases = [
-  Expect<Equal<MapTypes<{ stringToArray: string }, { mapFrom: string, mapTo: [] }>, { stringToArray: [] }>>,
-  Expect<Equal<MapTypes<{ stringToNumber: string }, { mapFrom: string, mapTo: number }>, { stringToNumber: number }>>,
-  Expect<Equal<MapTypes<{ stringToNumber: string, skipParsingMe: boolean }, { mapFrom: string, mapTo: number }>, { stringToNumber: number, skipParsingMe: boolean }>>,
-  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date } | { mapFrom: string, mapTo: null }>, { date: null | Date }>>,
-  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date | null }>, { date: null | Date }>>,
-  Expect<Equal<MapTypes<{ fields: Record<string, boolean> }, { mapFrom: Record<string, boolean>, mapTo: string[] }>, { fields: string[] }>>,
-  Expect<Equal<MapTypes<{ name: string }, { mapFrom: boolean, mapTo: never }>, { name: string }>>,
-  Expect<Equal<MapTypes<{ name: string, date: Date }, { mapFrom: string, mapTo: boolean } | { mapFrom: Date, mapTo: string }>, { name: boolean, date: string }>>,
+  Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
+  Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
+  Expect<Equal<Unique<[1, 'a', 2, 'b', 2, 'a']>, [1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>, [string, number, 1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[unknown, unknown, any, any, never, never]>, [unknown, any, never]>>,
 ]
 
+type Include<L, E> = L extends [infer F, ...infer R]
+? Equal<F, E> extends true
+  ? true
+  : Include<R, E>
+: false
 
-
-type MapTypes<T, M extends {[P in 'mapFrom' | 'mapTo'] : unknown}> = {
-  [K in keyof T] : T[K] extends M['mapFrom']
-    ? M extends any
-      ? M extends {mapFrom: T[K]}
-        ? M['mapTo']
-        : never
-      : never
-    : T[K]
-}
+type Unique<T, acc extends unknown[] = []> = T extends [infer F, ...infer R]
+? Include<acc, F> extends true
+  ? Unique<R, acc>
+  : Unique<R, [...acc, F]>
+: acc

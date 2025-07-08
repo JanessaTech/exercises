5692acb9aa4837c75f341d797023f5c6857f9445
Janessa <lvlajzhao1983@gmail.com>
2025-07-08 08:31:01 +0800

    redo- TupleToNestedObject

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,36 +1,12 @@
 import { Alike, Equal, Expect } from "../test-utils"
 
-type Foo = {
-  name: string
-  age: string
-}
-type Bar = {
-  name: string
-  age: string
-  gender: number
-}
-type Coo = {
-  name: string
-  gender: number
-}
-
 type cases = [
-  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
-  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
-  Expect<Equal<Diff<Foo, Coo>, { age: string, gender: number }>>,
-  Expect<Equal<Diff<Coo, Foo>, { age: string, gender: number }>>,
+  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b', 'c'], boolean>, { a: { b: { c: boolean } } }>>,
+  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>,
 ]
 
-
-
-// type Diff<T, U> = {
-//   [P in keyof (T & U) as P extends keyof (T | U) ? never : P] : (T & U)[P]
-// }
-
-type Diff<T, U> = {
-  [P in keyof(T & U) as P extends keyof(T | U) ? never : P] : P extends keyof T
-  ? T[P]
-  : P extends keyof U
-    ? U[P]
-    : never
-}
+type TupleToNestedObject<T extends unknown[], U> = T extends [infer F extends string, ...infer R]
+? { [P in F]: TupleToNestedObject<R, U>}
+: U

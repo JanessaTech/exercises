9d4a649fddddbb451ff7f2fc610dee2934525a7d
Janessa <lvlajzhao1983@gmail.com>
2025-07-14 10:38:09 +0800

    redo- upgrade

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -8,11 +8,9 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Redo {
     uint256 public value;
-    
-    bytes32 private constant ADMIN_SLOT = keccak256('ADMIN_SLOT');
-    bytes32 private constant IMPLEMENTATION_SLOT = keccak256('IMPLEMENTATION_SLOT');
-
-    constructor(address _implementation) {
+     bytes32 private constant ADMIN_SLOT = keccak256('ADMIN_SLOT');
+     bytes32 private constant IMPLEMENTATION_SLOT = keccak256('IMPLEMENTATION_SLOT');
+     constructor(address _implementation) {
         bytes32 slot = IMPLEMENTATION_SLOT;
         assembly {
             sstore(slot, _implementation)
@@ -21,36 +19,35 @@ contract Redo {
         assembly {
             sstore(slot, caller())
         }
-    }
-
-    function admin() public view returns(address adm) {
+     }
+     function admin() public view returns(address adm) {
         bytes32 slot = ADMIN_SLOT;
         assembly {
             adm := sload(slot)
         }
-    }
-
-    function implementation() public view returns(address impl) {
+     }
+     function implementation() public view returns(address impl) {
         bytes32 slot = IMPLEMENTATION_SLOT;
         assembly {
             impl := sload(slot)
         }
-    }
+     }
 
-    function upgradeTo(address _implementation) public {
+     function upgradeTo(address _implementation) public {
+        require(msg.sender == admin(), 'not owner');
         bytes32 slot = IMPLEMENTATION_SLOT;
         assembly {
             sstore(slot, _implementation)
         }
-    }
+     }
 
-    function _delegate(address _implementation) private {
+     function _deletegate(address _implementation) private {
         (bool success, ) = _implementation.delegatecall(msg.data);
         require(success, 'failed to call delegatecall');
-    }
+     }
 
-    fallback() external payable {
-        _delegate(implementation());
-    }
-    receive() external payable {}
+     fallback() external payable {
+        _deletegate(implementation());
+     }
+     receive() external payable {}
 }

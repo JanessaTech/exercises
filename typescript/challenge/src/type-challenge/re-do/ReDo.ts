35376fa8359ef9b738151bdbdfb75e46bf9c5124
Janessa <lvlajzhao1983@gmail.com>
2025-07-17 10:55:31 +0800

    redo - Permutation

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,74 +1,16 @@
 import { Alike, Equal, Expect } from "../test-utils"
 
 type cases = [
-  Expect<Equal<DeepReadonly<X1>, Expected1>>,
-  Expect<Equal<DeepReadonly<X2>, Expected2>>,
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
+  Expect<Equal<Permutation<never>, []>>,
 ]
 
-type X1 = {
-  a: () => 22
-  b: string
-  c: {
-    d: boolean
-    e: {
-      g: {
-        h: {
-          i: true
-          j: 'string'
-        }
-        k: 'hello'
-      }
-      l: [
-        'hi',
-        {
-          m: ['hey']
-        },
-      ]
-    }
-  }
-}
 
-type X2 = { a: string } | { b: number }
-
-type Expected1 = {
-  readonly a: () => 22
-  readonly b: string
-  readonly c: {
-    readonly d: boolean
-    readonly e: {
-      readonly g: {
-        readonly h: {
-          readonly i: true
-          readonly j: 'string'
-        }
-        readonly k: 'hello'
-      }
-      readonly l: readonly [
-        'hi',
-        {
-          readonly m: readonly ['hey']
-        },
-      ]
-    }
-  }
-}
-
-type Expected2 = { readonly a: string } | { readonly b: number }
-
-type DeepArray<L> = L extends [infer F, ...infer R]
-? [DeepReadonly<F>, ...DeepArray<R>]
-: []
-
-
-type DeepReadonly<T> = T extends any
-? T extends {[P in any]: unknown}
-  ? {readonly [K in keyof T]: DeepReadonly<T[K]>}
-  : T extends unknown[]
-    ? readonly [...DeepArray<T>]
-    : T
-: never 
-
-
-
-// for test
-type test = DeepReadonly<X1>
+type Permutation<T, path extends unknown[] = [], A = T> = [T] extends [never]
+? path
+: T extends any
+  ? Permutation<Exclude<A, T>, [...path, T]>
+  : never

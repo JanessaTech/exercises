24f75e51a8665acedc671318ec813a1cfad885e4
Janessa <lvlajzhao1983@gmail.com>
2025-07-26 11:28:45 +0800

    redo - ERC1155

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -6,18 +6,36 @@ import "hardhat/console.sol";
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Redo {
-   function sumArray(uint256[] memory arr) public pure returns(uint256) {
-      uint256 sum;
-      assembly {
-         let len := mload(arr)
-         let ptr:= add(arr, 0x20)
-         for {let i := 0} lt(i, len) { i := add(i, 1)} {
-            sum := add(sum, mload(ptr))
-            ptr := add(ptr, 0x20)
-         }
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract Redo is ERC1155, Ownable {
+   uint256 public constant SWORD = 1;
+   uint256 public constant POTION = 2;
+   uint256 public constant SHEILD = 3;
+
+
+   constructor(address initialOwner) ERC1155("http://test.com/{id}.json") Ownable(initialOwner) {
+      _mint(msg.sender, SWORD, 1000, '');
+      _mint(msg.sender, POTION, 1000, '');
+      _mint(msg.sender, SHEILD, 1000, '');
+   }
+
+   function setURI(string memory newuri) public onlyOwner {
+      _setURI(newuri);
+   }
+
+   function batchMint(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public onlyOwner {
+      require(ids.length == values.length, 'ids.length != values.length');
+      _mintBatch(to, ids, values, data);
+   }
+   function batchTransfer(address[] memory recipients, uint256 id, uint256 amount) public {
+      require(balanceOf(msg.sender, id) >= recipients.length * amount);
+      for (uint256 i = 0; i < recipients.length; i++) {
+      _safeTransferFrom(msg.sender, recipients[i], id, amount, '');
       }
-      return sum;
    }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,17 +6,35 @@ const { extendEnvironment } = require("hardhat/config");
 
 describe('Redo', function () {
     async function deployRedoFixture() {
-        const Redo = await ethers.getContractFactory('Redo')
-        const redo = await Redo.deploy()
-        return {redo}
+        const [admin, bob, alice, ...others] = await ethers.getSigners()
+        const Redo = await ethers.getContractFactory('Redo', admin)
+        const redo = await Redo.deploy(admin)
+        return {redo, admin, bob, alice}
 
     }
-    describe('sumArray', function () {
-        it('sumArray', async function () {
-            const {redo} = await loadFixture(deployRedoFixture)
-            const sum = await redo.sumArray([1, 2, 3])
-            expect(sum).to.be.equal(6)
-        }) 
+    describe('batchMint', function () {
+        it('batchMint', async function () {
+            const {redo, bob} = await loadFixture(deployRedoFixture)
+            const ids = [1, 2]
+            const values = [100, 200]
+            await redo.batchMint(bob.getAddress(), ids, values, '0x123456')
+            const balance1 = await redo.balanceOf(bob.getAddress(), 1)
+            const balance2 = await redo.balanceOf(bob.getAddress(), 2)
+            expect(balance1).to.be.equal(100)
+            expect(balance2).to.be.equal(200)
+        })
+    })
+    describe('batchTransfer', function () {
+        it('batchTransfer', async function () {
+            const {redo, admin, bob, alice} = await loadFixture(deployRedoFixture)
+            const recepients = [await bob.getAddress(), await alice.getAddress()]
+            const id = 1
+            const amount = 200
+            await redo.batchTransfer(recepients, id, amount)
+            const left = await redo.balanceOf(admin.getAddress(), 1)
+            expect(left).to.be.equal(600)
+
+        })
     })
     
 })

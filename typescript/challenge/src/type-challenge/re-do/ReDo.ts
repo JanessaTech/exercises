1f3fefc71a35944c572c083af823fbaeeca6cb2d
Janessa <lvlajzhao1983@gmail.com>
2025-07-27 17:33:10 +0800

    redo - MapType

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,23 +1,17 @@
 import { Alike, Equal, Expect } from "../test-utils"
 
-type cases = [
-  Expect<Equal<MapTypes<{ stringToArray: string }, { mapFrom: string, mapTo: [] }>, { stringToArray: [] }>>,
-  Expect<Equal<MapTypes<{ stringToNumber: string }, { mapFrom: string, mapTo: number }>, { stringToNumber: number }>>,
-  Expect<Equal<MapTypes<{ stringToNumber: string, skipParsingMe: boolean }, { mapFrom: string, mapTo: number }>, { stringToNumber: number, skipParsingMe: boolean }>>,
-  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date } | { mapFrom: string, mapTo: null }>, { date: null | Date }>>,
-  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date | null }>, { date: null | Date }>>,
-  Expect<Equal<MapTypes<{ fields: Record<string, boolean> }, { mapFrom: Record<string, boolean>, mapTo: string[] }>, { fields: string[] }>>,
-  Expect<Equal<MapTypes<{ name: string }, { mapFrom: boolean, mapTo: never }>, { name: string }>>,
-  Expect<Equal<MapTypes<{ name: string, date: Date }, { mapFrom: string, mapTo: boolean } | { mapFrom: Date, mapTo: string }>, { name: boolean, date: string }>>,
-]
-
-
-type MapTypes<T, M extends {[P in 'mapFrom' | 'mapTo'] : unknown}> = {
-  [K in keyof T] : T[K] extends M['mapFrom']
-    ? M extends any
-      ? M extends {mapFrom: T[K]}
-        ? M['mapTo']
-        : never
-      : never
-    : T[K]
+type User = {
+  id: number;
+  name: string;
+  age ?: number;
+  phone ?: string
 }
+
+type RequiredKeys<T> = {
+  [K in keyof T]-? : T extends Record<K, T[K]> ? K : never
+}[keyof T]
+
+type OptionalKeys<T> = Exclude<keyof T, RequiredKeys<T>>
+
+type test = RequiredKeys<User>
+type test2 = OptionalKeys<User>

67d7d047d6fd325220e1acc75c2d8669918baa08
Janessa <lvlajzhao1983@gmail.com>
2025-07-27 17:28:24 +0800

    redo - MapTypes

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,29 +1,23 @@
 import { Alike, Equal, Expect } from "../test-utils"
 
 type cases = [
-  Expect<Equal<AllCombinations<''>, ''>>,
-  Expect<Equal<AllCombinations<'A'>, '' | 'A'>>,
-  Expect<Equal<AllCombinations<'AB'>, '' | 'A' | 'B' | 'AB' | 'BA'>>,
-  Expect<Equal<AllCombinations<'ABC'>, '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'>>,
-  Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
+  Expect<Equal<MapTypes<{ stringToArray: string }, { mapFrom: string, mapTo: [] }>, { stringToArray: [] }>>,
+  Expect<Equal<MapTypes<{ stringToNumber: string }, { mapFrom: string, mapTo: number }>, { stringToNumber: number }>>,
+  Expect<Equal<MapTypes<{ stringToNumber: string, skipParsingMe: boolean }, { mapFrom: string, mapTo: number }>, { stringToNumber: number, skipParsingMe: boolean }>>,
+  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date } | { mapFrom: string, mapTo: null }>, { date: null | Date }>>,
+  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date | null }>, { date: null | Date }>>,
+  Expect<Equal<MapTypes<{ fields: Record<string, boolean> }, { mapFrom: Record<string, boolean>, mapTo: string[] }>, { fields: string[] }>>,
+  Expect<Equal<MapTypes<{ name: string }, { mapFrom: boolean, mapTo: never }>, { name: string }>>,
+  Expect<Equal<MapTypes<{ name: string, date: Date }, { mapFrom: string, mapTo: boolean } | { mapFrom: Date, mapTo: string }>, { name: boolean, date: string }>>,
 ]
 
-type StringToUnion<S> = S extends `${infer F}${infer R}`
-? F | StringToUnion<R>
-: never
-type ArrayToString<A> = A extends any
-? A extends [infer F, ...infer R]
-  ? `${string &F}${ArrayToString<R>}`
-  : ''
-: never
 
-type Com<U, path extends unknown[] = [], acc = never, A = U> = [U] extends [never]
-? ArrayToString<acc | path>
-: U extends any
-  ? Com<Exclude<A, U>, [...path, U], acc | path>
-  : never
-
-type AllCombinations<S extends string> = Com<StringToUnion<S>>
-
-type test = StringToUnion<'AB'>
-type test1 = Com<StringToUnion<'AB'>>
+type MapTypes<T, M extends {[P in 'mapFrom' | 'mapTo'] : unknown}> = {
+  [K in keyof T] : T[K] extends M['mapFrom']
+    ? M extends any
+      ? M extends {mapFrom: T[K]}
+        ? M['mapTo']
+        : never
+      : never
+    : T[K]
+}

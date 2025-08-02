a9940d414dcebcbe32825d11fd57587261b5d7ae
Janessa <lvlajzhao1983@gmail.com>
2025-08-02 16:25:28 +0800

    redo - ReplaceAll

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,17 +1,23 @@
 import { Alike, Equal, Expect } from "../test-utils"
 
-type User = {
-  id: number;
-  name: string;
-  age ?: number;
-  phone ?: string
-}
+type cases = [
+  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
+  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
+  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
+  Expect<Equal<ReplaceAll<'', '', ''>, ''>>,
+]
 
-type RequiredKeys<T> = {
-  [K in keyof T]-? : T extends Record<K, T[K]> ? K : never
-}[keyof T]
 
-type OptionalKeys<T> = Exclude<keyof T, RequiredKeys<T>>
-
-type test = RequiredKeys<User>
-type test2 = OptionalKeys<User>
+type test = ReplaceAll<'foobar', 'bar', 'foo'>
+type ReplaceAll<S extends string, From extends string, To extends string, acc extends string= ''> = From extends ''
+? S
+: S extends ''
+  ? acc
+  : S extends `${infer L}${From}${infer R}`
+    ? ReplaceAll<R, From, To, `${acc}${L}${To}`>
+    : ReplaceAll<'', From, To, `${acc}${S}`>

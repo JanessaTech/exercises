0755eafa95223804a99bcccdcac70f6897b7008e
Janessa <lvlajzhao1983@gmail.com>
2025-08-14 11:53:09 +0800

    redo - secure withdraw- checks-affects-interations

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -13,70 +13,28 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Redo {
-   IERC721 nft;
-   uint256 tokenId;
+   bool private locked;
+   mapping(address => uint256) balances;
 
-   address owner;
-
-   bool started;
-   uint256 endAt;
-
-   address highestBidder;
-   uint256 highestBid;
-   mapping(address => uint256) bids;
-
-   event Start(address indexed from);
-   event End(address indexed from);
-   event Bid(address indexed from, uint256 amount);
+   modifier non_reentrant() {
+      require(!locked, 'reentrant');
+      locked = true;
+      _;
+      locked = false;
+   }
+   event Deposit(address indexed from, uint256 amount);
    event Withdraw(address indexed from, uint256 amount);
 
-   constructor(address _nft, uint256 _tokenId) {
-      nft = IERC721(_nft);
-      tokenId = _tokenId;
-      owner = msg.sender;
+   function deposit() public payable {
+      balances[msg.sender] += msg.value;
+      emit Deposit(msg.sender, msg.value);
    }
-
-   function start() public {
-      require(msg.sender == owner, 'not owner');
-      require(!started, 'started');
-      started = true;
-      endAt = block.timestamp + 7 days;
-      nft.transferFrom(msg.sender, address(this), tokenId);
-      emit Start(msg.sender);
-   }
-
-   function bid() public payable{
-      require(started, 'not started');
-      require(block.timestamp < endAt, 'ended');
-      require(msg.value > highestBid, 'msg.value <= highestBid');
-      if (highestBidder != address(0)) {
-         bids[highestBidder] += highestBid;
-      }
-      highestBidder = msg.sender;
-      highestBid = msg.value;
-      emit Bid(msg.sender, msg.value);
-   }
-
-   function withdraw() public {
-      uint256 amount = bids[msg.sender];
+   function withdraw() public payable non_reentrant {
+      uint256 amount = balances[msg.sender];
       require(amount > 0, 'no eth');
-      bids[msg.sender] = 0;
-      bool sent = payable(msg.sender).send(amount);
-      require(sent, 'failed to withdraw');
+      balances[msg.sender] = 0;
+      (bool success, ) = payable(msg.sender).call{gas: 2300, value: amount}('');
+      require(success, 'failed to withdraw');
       emit Withdraw(msg.sender, amount);
-   }
-
-   function end() public {
-      require(msg.sender == owner, 'not owner');
-      require(started, 'not started');
-      require(block.timestamp >= endAt, 'not end');
-      if (highestBidder != address(0)) {
-         nft.transferFrom(address(this), highestBidder, tokenId);
-         bool sent = payable(owner).send(highestBid);
-         require(sent, 'failed to pay nft owner');
-      } else {
-         nft.transferFrom(address(this), owner, tokenId);
-      }
-      emit End(msg.sender);
    }
 }

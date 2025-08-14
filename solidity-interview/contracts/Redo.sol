2be5abd31c4a14ee117862266fbb50c0aad93982
Janessa <lvlajzhao1983@gmail.com>
2025-08-14 18:21:13 +0800

    redo - ERC1155

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -12,21 +12,31 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Redo is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+contract Redo is ERC1155, Ownable {
+   uint256 private constant SWORD = 1;
+   uint256 private constant POTION = 2;
+   uint256 private constant SHEILD = 3;
 
-    constructor(address defaultAdmin, address minter, address burner) ERC20("MyToken", "MTK") {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
-        _grantRole(BURNER_ROLE, burner);
+    constructor(address initialOwner) 
+      ERC1155("http://test.com/{id}.json") 
+      Ownable(initialOwner) {
+         _mint(msg.sender, SWORD, 1000, '');
+         _mint(msg.sender, POTION, 1000, '');
+         _mint(msg.sender, SHEILD, 1000, '');
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
     }
-    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE) {
-      require(balanceOf(to) >= amount, 'not enough to burn');
-      _burn(to, amount);
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public onlyOwner {
+      require(ids.length == values.length, 'ids.length != values.length');
+      _mintBatch(to, ids, values, data);
+    }
+    function transferBatch(address[] memory recipients, uint256 id, uint256 amount) public {
+      require(balanceOf(msg.sender, id) >= recipients.length * amount, 'no enough');
+      for (uint256 i = 0; i < recipients.length; i++) {
+         _safeTransferFrom(msg.sender, recipients[i], id, amount, '');
+      }
     }
 }

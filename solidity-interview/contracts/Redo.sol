befc95ef0d5bbecb98a581bcccfd7e16f8d66046
Janessa <lvlajzhao1983@gmail.com>
2025-08-14 16:54:07 +0800

    redo - ArrayPerson

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -13,28 +13,36 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Redo {
-   bool private locked;
-   mapping(address => uint256) balances;
-
-   modifier non_reentrant() {
-      require(!locked, 'reentrant');
-      locked = true;
-      _;
-      locked = false;
+   struct Person {
+      uint256 id;
+      string name;
    }
-   event Deposit(address indexed from, uint256 amount);
-   event Withdraw(address indexed from, uint256 amount);
+   uint256 idx;
+   Person[] people;
+   mapping(uint256 => uint256) idxMapping;
+   mapping(uint256 => bool) inserted;
 
-   function deposit() public payable {
-      balances[msg.sender] += msg.value;
-      emit Deposit(msg.sender, msg.value);
+   function create(string memory _name) public {
+      uint256 _id = idx++;
+      people.push(Person({id:_id, name:_name}));
+      idxMapping[_id] = people.length - 1;
+      inserted[_id] = true;
    }
-   function withdraw() public payable non_reentrant {
-      uint256 amount = balances[msg.sender];
-      require(amount > 0, 'no eth');
-      balances[msg.sender] = 0;
-      (bool success, ) = payable(msg.sender).call{gas: 2300, value: amount}('');
-      require(success, 'failed to withdraw');
-      emit Withdraw(msg.sender, amount);
+
+   function remove(uint256 _id) public {
+      require(inserted[_id], 'invalid id');
+      uint256 _idx = idxMapping[_id];
+      Person storage last = people[people.length - 1];
+      people[_idx] = last;
+      idxMapping[last.id] = _idx;
+      delete idxMapping[_id];
+      delete inserted[_id];
+      people.pop();
+   }
+
+   function get(uint256 _id) public view returns(uint256 id, string memory name) {
+      require(inserted[_id], 'invalid id');
+      Person storage person = people[idxMapping[_id]];
+      return (person.id, person.name);
    }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -11,11 +11,31 @@ describe('Redo', function () {
         const redo = await Redo.deploy()
         return {redo, Bob}
     }  
-    describe('withdraw', function () {
-        it('withdraw', async function () {
+    describe('create & remove', function () {
+        it('create', async function () {
             const {redo, Bob} = await loadFixture(deployRedoFixture)
-            await redo.connect(Bob).deposit({value: 1000})
-            await expect(redo.connect(Bob).withdraw()).to.emit(redo, 'Withdraw').withArgs(Bob.getAddress(), 1000)
+            await redo.create('person0')
+            await redo.create('person1')
+            await redo.create('person2')
+            const person0 = await redo.get(0);
+            const person1 = await redo.get(1);
+            const person2 = await redo.get(2);
+            expect(person0.name).to.be.equal('person0')
+            expect(person1.name).to.be.equal('person1')
+            expect(person2.name).to.be.equal('person2')
+        })
+        it('remove', async function () {
+            const {redo} = await loadFixture(deployRedoFixture)
+            await redo.create('person0')
+            await redo.create('person1')
+            await redo.create('person2')
+            await redo.remove(1)
+            const person0 = await redo.get(0);
+            const person2 = await redo.get(2);
+            expect(person0.name).to.be.equal('person0')
+            expect(person2.name).to.be.equal('person2')
+            await expect(redo.get(1)).to.be.revertedWith('invalid id')
+
         })
     })  
     

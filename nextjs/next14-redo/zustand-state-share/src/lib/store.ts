af9a3eaf2606b7ac432954e91ae8b32a8f3e2418
Janessa <lvlajzhao1983@gmail.com>
2025-08-23 22:50:27 +0800

    redo - zustand state shring

--- nextjs/next14-redo/zustand-state-share/src/lib/store.ts
+++ nextjs/next14-redo/zustand-state-share/src/lib/store.ts
@@ -8,22 +8,24 @@ interface NoteState {
     addNote: (note: string) => void;
     isDone: boolean;
     setIsDone: (done: boolean) => void;
-    clear : () => void
+    clear: () => void
 }
+
 const useAddNote = create<NoteState>()(persist(
     (set) => ({
         notes: [],
+        addNote: (note) => set((state) =>({notes: [...state.notes, note]})),
         isDone: false,
-        addNote: (note) => set((state) => ({notes: [...state.notes, note]})),
         setIsDone: (done) => set({isDone: done}),
         clear: () => {
-            localStorage.removeItem('note_state')
-            set({notes:[]})
+            localStorage.removeItem('state_note')
+            set({notes: []})
         }
-    }), {
-        name: 'note_state',
+    }),
+    {
+        name: 'state_note',
         storage: createJSONStorage(() => localStorage),
-        onRehydrateStorage: () => (state) => {
+        onRehydrateStorage: () =>(state) => {
             state?.setIsDone(true)
         }
     }

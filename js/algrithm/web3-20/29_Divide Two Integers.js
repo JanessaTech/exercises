c7cfd4c9fac5f2ccc621b8b0873f9410c182be93
Janessa <lvlajzhao1983@gmail.com>
2025-08-23 22:31:25 +0800

    solution for 29

---
+++ js/algrithm/web3-20/29_Divide Two Integers.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function(dividend, divisor) {
+    
+};
+
+//check https://leetcode.com/problems/divide-two-integers/solutions/6773203/easy-solution-100-beats-javascript/

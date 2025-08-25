1cbe63f1b4943600c88d385cd0afa79647d59a1e
Janessa <lvlajzhao1983@gmail.com>
2025-08-25 14:13:12 +0800

    reponsive menu

--- css+div/tailwind-redo/responsive-menu/src/app/page.tsx
+++ css+div/tailwind-redo/responsive-menu/src/app/page.tsx
@@ -31,16 +31,18 @@ export default function Home() {
             </svg>
           </div>
           <ul className={`${showMenu ? '' : 'hidden'}`}>
-            <li className="cursor-pointer group my-3">
-              <div className="flex justify-end border-r-[4px] border-white group-hover:border-red-500 group-hover:text-red-500">
+            <li className="cursor-pointer group my-3" onClick={() => setSelect(0)}>
+              <div className={`flex justify-end border-r-[4px] border-white 
+                group-hover:border-red-500 group-hover:text-red-500 ${select === 0 ? 'border-red-500 text-red-500' : ''}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
                 <span className="font-semibold px-2">Home</span>
               </div>
             </li>
-            <li className="cursor-pointer group my-3">
-              <div className="flex justify-end border-r-[4px] border-white group-hover:border-red-500 group-hover:text-red-500">
+            <li className="cursor-pointer group my-3" onClick={() => setSelect(1)}>
+              <div className={`flex justify-end border-r-[4px] border-white 
+                group-hover:border-red-500 group-hover:text-red-500 ${select === 1 ? 'border-red-500 text-red-500' : ''}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>

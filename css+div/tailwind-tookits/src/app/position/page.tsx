ed2fc4972373bd6d5c5340a96e0d931d8c2184c4
Janessa <lvlajzhao1983@gmail.com>
2025-08-26 10:40:22 +0800

    tailwind position

--- css+div/tailwind-tookits/src/app/position/page.tsx
+++ css+div/tailwind-tookits/src/app/position/page.tsx
@@ -1,7 +1,12 @@
 
 const Position: React.FC<{}> = () => {
     return (
-        <div></div>
+        <div className="w-full h-screen bg-zinc-300 flex justify-center items-center">
+            <div className="w-[300px] h-0 relative bg-blue-700">
+                <div className="bg-orange-600 h-[50px] w-[300px] absolute top-[-50px]"></div>
+                <div className="bg-pink-600 h-[20px] w-[300px] absolute top-0"></div>
+            </div>
+        </div>
     )
 }
 

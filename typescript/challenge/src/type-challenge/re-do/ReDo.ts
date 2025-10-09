d03be79ffb1acc7673430ba10f4c10233532797d
Janessa <lvlajzhao1983@gmail.com>
2025-10-09 17:47:42 +0800

    redo TS - UnpackPromise

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,25 +1,9 @@
 import { Alike, Equal, Expect, ExpectExtends, NotAny } from "../test-utils"
 
-type cases = [
-  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
-  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
-  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
-  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
-  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
-  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
-  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
-  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
-  Expect<Equal<ReplaceAll<'', '', ''>, ''>>,
-]
+type UnpackPromise<T> = T extends Promise<infer U> 
+                      ? UnpackPromise<U>
+                      : T
 
-
-
-type test = ReplaceAll<'foobar', 'bar', 'foo'>
-
-type ReplaceAll<S extends string, From extends string, To extends string, acc extends string= ''> = From extends ''
-? S
-: S extends ''
-  ? acc
-  : S extends `${infer L}${From}${infer R}`
-    ? ReplaceAll<R, From, To, `${acc}${L}${To}`>
-    : ReplaceAll<'', From, To, `${acc}${S}`>
+// 测试
+type NestedPromise = Promise<Promise<string>>;
+type ResolvedType = UnpackPromise<NestedPromise>; // string

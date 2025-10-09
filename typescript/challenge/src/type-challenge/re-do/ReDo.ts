ef7c5e9ab9393bf3f61d52a6c21d1e4be7dcc1b0
Janessa <lvlajzhao1983@gmail.com>
2025-10-09 08:35:59 +0800

    redo TS- Unique

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,23 +1,21 @@
 import { Alike, Equal, Expect, ExpectExtends, NotAny } from "../test-utils"
 
-
 type cases = [
-  Expect<Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[1, '', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, 'test', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', true, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [1], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], {}, undefined, null]>, false>>,
-  Expect<Equal<AnyOf<[]>, false>>,
-]
+    Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
+    Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
+    Expect<Equal<Unique<[1, 'a', 2, 'b', 2, 'a']>, [1, 'a', 2, 'b']>>,
+    Expect<Equal<Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>, [string, number, 1, 'a', 2, 'b']>>,
+    Expect<Equal<Unique<[unknown, unknown, any, any, never, never]>, [unknown, any, never]>>,
+  ]
 
-type falsy = [0, '', false, [], {[P in any]: never}, undefined, null][number]
-  
-type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
-? F extends falsy
-  ? AnyOf<R>
-  : true
+type Contains<L extends unknown[], E>  = L extends [infer F, ...infer R]
+? Equal<F, E> extends true
+  ? true
+  : Contains<R, E>
 : false
+
+type Unique<T extends unknown[], acc extends unknown[] = []> = T extends [infer F, ...infer R]
+? Contains<acc, F> extends true 
+  ? Unique<R, acc>
+  : Unique<R, [...acc, F]>
+: acc

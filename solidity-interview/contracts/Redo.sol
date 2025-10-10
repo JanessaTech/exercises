825d153435ab40329fa685d993b69dadedd9b928
Janessa <lvlajzhao1983@gmail.com>
2025-10-10 14:36:36 +0800

    redo solidity - erc1155

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -12,21 +12,30 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Redo is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+contract Redo is ERC1155, Ownable {
+    uint public constant SWORD = 1;
+    uint public constant POTION = 2;
+    uint public constant SHEILD = 3;
 
-    constructor(address defaultAdmin, address minter, address burner) ERC20("MyToken", "MTK") {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
-        _grantRole(BURNER_ROLE, burner);
+    constructor(address initialOwner) ERC1155("http://test.com/{id}.json") Ownable(initialOwner) {
+      _mint(msg.sender, SWORD, 1000, '');
+      _mint(msg.sender, POTION, 1000, '');
+      _mint(msg.sender, POTION, 1000, '');
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
     }
-    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE) {
-      require(balanceOf(to) > amount, 'not enough to burn');
-      _burn(to, amount);
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public onlyOwner {
+      require(ids.length == values.length, 'ids.length != values.length');
+      _mintBatch(to, ids, values, data);
+    }
+
+    function transferBatch(address[] memory recipents, uint id, uint amount) public {
+      require(balanceOf(address(this), id) >= recipents.length * amount);
+      for (uint i = 0; i < recipents.length; i++) {
+        safeTransferFrom(address(this), recipents[i], id, amount, '');
+      }
     }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,36 +6,22 @@ const { extendEnvironment } = require("hardhat/config");
 
 describe('Redo', function () {
    async function deployRedoFixture() {
-    const [admin, minter, burner, bob, non, ...others] = await ethers.getSigners()
+    const [owner, bob, ...others] = await ethers.getSigners()
     const Redo = await ethers.getContractFactory('Redo')
-    const redo = await Redo.deploy(admin, minter, burner)
-    return {redo, minter, burner, bob, non}
+    const redo = await Redo.deploy(owner)
+    return {redo, owner, bob}
    }
 
-   describe('mint & burn', function () {
-    it('it failed to mint when it is not minter', async function () {
-        const {redo, minter, bob, non} = await loadFixture(deployRedoFixture)
-        await expect(redo.connect(non).mint(bob.getAddress(), 1000)).to.be.revertedWithCustomError(redo, 'AccessControlUnauthorizedAccount')
+   describe('mint', function () {
+    it('it minted successfully', async function () {
+        const {redo, owner, bob} = await loadFixture(deployRedoFixture)
+        const tokenURI = 'aaaa'
+        await redo.connect(owner).mint(bob.getAddress(), tokenURI)
+        const url = await redo.tokenURI(0)
+        console.log(url)
         
     })
-    it('it minted successfully', async function () {
-        const {redo, minter, bob} = await loadFixture(deployRedoFixture)
-        const amount = 1000
-        await expect(redo.connect(minter).mint(bob.getAddress(), amount)).not.to.be.reverted
-        const balance = await redo.balanceOf(bob.getAddress())
-        expect(balance).to.be.equal(amount)
-    })
-    it('it failed burn when there is not enough to burn', async function () {
-        const {redo, burner, bob, minter} = await loadFixture(deployRedoFixture)
-        await expect(redo.connect(burner).burn(bob.getAddress(), 1000)).to.be.revertedWith('not enough to burn')
-    })
-    it('it burned successfully', async function () {
-        const {redo, burner, bob, minter} = await loadFixture(deployRedoFixture)
-        await redo.connect(minter).mint(bob.getAddress(), 1000)
-        await expect(redo.connect(burner).burn(bob.getAddress(), 300)).not.to.be.reverted
-        const balance = await redo.balanceOf(bob.getAddress())
-        expect(balance).to.be.equal(700)
-    })
+  
    })
 })
 

db9be7a1cf3d9546fa5127828dcf693c3f91955a
Janessa <lvlajzhao1983@gmail.com>
2025-10-10 16:42:14 +0800

    redo nextjs-zustand

--- nextjs/next14-redo/zustand-state-share/src/lib/store.ts
+++ nextjs/next14-redo/zustand-state-share/src/lib/store.ts
@@ -1,15 +1,12 @@
-
-import { notEqual } from 'assert';
-import { BlobOptions } from 'buffer';
 import {create } from 'zustand'
 import {persist, createJSONStorage} from 'zustand/middleware'
 
-interface NoteState {
+export interface NoteState {
     notes: string[];
     addNote: (note: string) => void;
     isDone: boolean;
     setIsDone: (done: boolean) => void;
-    clear: () => void
+    clear: () => void;
 }
 
 const useAddNote = create<NoteState>()(persist(
@@ -17,18 +14,17 @@ const useAddNote = create<NoteState>()(persist(
         notes: [],
         isDone: false,
         addNote: (note) => set((state) => ({notes: [...state.notes, note]})),
-        setIsDone: (done)=> set({isDone: done}),
+        setIsDone: (done) => set({isDone: done}), 
         clear: () => {
-            localStorage.removeItem('')
             set({notes: []})
+            localStorage.removeItem('note_state')
         }
-    }),
-    {
+    }), {
         name: 'note_state', 
-        storage: createJSONStorage(() => localStorage),
-       onRehydrateStorage: () => (state) => {
-        state?.setIsDone(true)
-       }
+        storage: createJSONStorage(() =>localStorage),
+        onRehydrateStorage: () => (state) => {
+            state?.setIsDone(true)
+        }
     }
 ))
 

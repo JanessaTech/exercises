26bf5da7f9695cfe7b45582b9502b12c781dadb0
Janessa <lvlajzhao1983@gmail.com>
2025-10-29 11:43:36 +0800

    redo: TS - Diff

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,21 +1,27 @@
 import { Alike, Equal, Expect, ExpectExtends, NotAny } from "../test-utils"
 
+type Foo = {
+  name: string
+  age: string
+}
+type Bar = {
+  name: string
+  age: string
+  gender: number
+}
+type Coo = {
+  name: string
+  gender: number
+}
+
 type cases = [
-  Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
-  Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
-  Expect<Equal<Unique<[1, 'a', 2, 'b', 2, 'a']>, [1, 'a', 2, 'b']>>,
-  Expect<Equal<Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>, [string, number, 1, 'a', 2, 'b']>>,
-  Expect<Equal<Unique<[unknown, unknown, any, any, never, never]>, [unknown, any, never]>>,
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string, gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string, gender: number }>>,
 ]
 
-type Include<L extends unknown[], E> = L extends [infer F, ...infer R]
-? Equal<E, F> extends true
-  ? true
-  : Include<R, E>
-: false
 
-type Unique<T extends unknown[], acc extends unknown[] = []> =  T extends [infer F, ...infer R]
-? Include<acc, F> extends true  
-  ? Unique<R, acc>
-  : Unique<R, [...acc, F]>
-: acc
+type Diff<T, U> = {
+  [K in keyof (T & U) as K extends keyof (T | U) ? never : K]: (T & U)[K]
+}

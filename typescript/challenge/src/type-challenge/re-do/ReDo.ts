eb7130e3830d137e48d2bb752ee843772f02b083
Janessa <lvlajzhao1983@gmail.com>
2025-10-29 11:40:46 +0800

    redo: solidity - Unique

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,13 +1,21 @@
 import { Alike, Equal, Expect, ExpectExtends, NotAny } from "../test-utils"
 
-type User = {
-  id : string;
-  name: string;
-  age ?: number;
-  phone ?: string
-}
+type cases = [
+  Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
+  Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
+  Expect<Equal<Unique<[1, 'a', 2, 'b', 2, 'a']>, [1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>, [string, number, 1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[unknown, unknown, any, any, never, never]>, [unknown, any, never]>>,
+]
 
-type RequiredTypes<T> = {
-  [K in keyof T] -?: T extends Record<K, T[K]> ? K : never
-}[keyof T]
-type test = RequiredTypes<User>
+type Include<L extends unknown[], E> = L extends [infer F, ...infer R]
+? Equal<E, F> extends true
+  ? true
+  : Include<R, E>
+: false
+
+type Unique<T extends unknown[], acc extends unknown[] = []> =  T extends [infer F, ...infer R]
+? Include<acc, F> extends true  
+  ? Unique<R, acc>
+  : Unique<R, [...acc, F]>
+: acc

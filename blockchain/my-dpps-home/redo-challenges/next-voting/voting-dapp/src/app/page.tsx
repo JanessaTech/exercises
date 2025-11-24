18f36d83a49cf2be5a9012f969df49b14e83aad7
Janessa <lvlajzhao1983@gmail.com>
2025-11-24 16:00:20 +0800

    redo voting

--- blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/page.tsx
+++ blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/page.tsx
@@ -1,14 +1,11 @@
 'use client'
 
-import { IWeb3Context, useWeb3Context } from "@/components/providers/Web3ContextProvider"
-import { authState } from "@/lib/Atoms"
+import { IWeb3Context, useWeb3Context } from "@/components/providers/Web3Provider"
 import { useRouter } from "next/navigation"
 import React from "react"
-import { useRecoilState } from "recoil"
 
 type LoginProps = {}
 const Login: React.FC<LoginProps> = () => {
-    const [auth, setAuth] = useRecoilState(authState)
     const router = useRouter()
     const {connectWallet} = useWeb3Context() as IWeb3Context
 
@@ -16,7 +13,6 @@ const Login: React.FC<LoginProps> = () => {
         await connectWallet()
         router.push('/home')
     }
-
     return (
         <div>
             <button className="bg-zinc-600 rounded-3xl px-3 py-2" onClick={onClick}>Collect wallet </button>

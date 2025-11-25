5b18aa9ce30a105651f88f4cff2671869c9cf500
Janessa <lvlajzhao1983@gmail.com>
2025-11-25 18:06:51 +0800

    redo solidity - erc1155

--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,39 +6,24 @@ const { extendEnvironment, extendProvider } = require("hardhat/config");
 
 describe('Redo', function () {
    async function deployRedoFxiture() {
-    const [admin, minter, burner, bob, non, ...other] = await ethers.getSigners()
+    const [admin, bob, ...other] = await ethers.getSigners()
     const Redo = await ethers.getContractFactory('Redo')
-    const redo = await Redo.deploy(admin.getAddress(), minter.getAddress(), burner.getAddress())
-    return {redo, minter, burner, bob, non}
+    const redo = await Redo.deploy(admin.getAddress())
+    return {redo, admin, bob}
    }
 
    describe('mint', function () {
-    it('it failed to mint when it is not minter', async function () {
-       const {redo, bob, non} = await loadFixture(deployRedoFxiture)
-       await expect(redo.connect(non).mint(bob.getAddress(), 1000)).to.be.revertedWithCustomError(redo, 'AccessControlUnauthorizedAccount')
-       
-    })
-    it('it minted successfully', async function () {
-      const {redo, minter, bob} = await loadFixture(deployRedoFxiture)
-      await expect(redo.connect(minter).mint(bob.getAddress(), 1000)).not.to.be.revertedWith
-    })
-   })
-   describe('burn', function () {
-    it('it failed to burn when it is not burner', async function () {
-        const {redo, non, bob} = await loadFixture(deployRedoFxiture)
-        await expect(redo.connect(non).burn(bob.getAddress(), 1000)).to.be.revertedWithCustomError(redo, 'AccessControlUnauthorizedAccount')
-    })
-    it('it burned successfully', async function () {
-        const {redo, minter, burner, bob} = await loadFixture(deployRedoFxiture)
-        await redo.connect(minter).mint(bob.getAddress(), 1000)
-        await expect(redo.connect(burner).burn(bob.getAddress(), 300))
-        const balance = await redo.balanceOf(bob.getAddress())
-        expect(balance).to.be.equal(700)
+    it('it minted', async function () {
+       const {redo, bob} = await loadFixture(deployRedoFxiture)
+       const uri = 'aaa'
+       await redo.mint(bob.getAddress(), uri)
+       const url = await redo.tokenURI(0)
+       console.log(url)
     })
     
-   })
 
    
+})
 })
 
 

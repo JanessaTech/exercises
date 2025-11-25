bd9c3704ee8f136aae1ff3a57410f3216c92e48a
Janessa <lvlajzhao1983@gmail.com>
2025-11-25 10:06:40 +0800

    redo TS - sumArray with assembly

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -13,49 +13,16 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Redo {
-  uint256 public value;
-  bytes32 private constant IMPLEMENTATION_SLOT = keccak256('IMPLEMENTATION_SLOT');
-  bytes32 private constant ADMIN_SLOT = keccak256('ADMIN_SLOT');
-
-  constructor(address _implementation) {
-    bytes32 slot = IMPLEMENTATION_SLOT;
+  function sumArray(uint[] memory arr) public pure returns(uint) {
+    uint sum;
     assembly {
-      sstore(slot, _implementation)
+      let len := mload(arr)
+      let ptr := add(arr, 0x20)
+      for { let i := 0} lt(i, len) { i := add(i, 1)} {
+        sum := add(sum, mload(ptr))
+        ptr := add(ptr, 0x20)
+      }
     }
-    slot = ADMIN_SLOT;
-    assembly {
-      sstore(slot, caller())
-    }
+    return sum;
   }
-
-  function admin() public view returns(address adm) {
-    bytes32 slot = ADMIN_SLOT;
-    assembly {
-      adm := sload(slot)
-    }
-  }
-  function implemenation() public view returns(address impl) {
-    bytes32 slot = IMPLEMENTATION_SLOT;
-    assembly {
-      impl := sload(slot)
-    }
-  }
-
-  function upgradeTo(address newImplementation) public {
-    require(msg.sender == admin(), 'not adim');
-    bytes32 slot = IMPLEMENTATION_SLOT;
-    assembly {
-      sstore(slot, newImplementation)
-    }
-  }
-
-  function _delegate(address _implemenation) private {
-    (bool success, ) = _implemenation.delegatecall(msg.data);
-    require(success, 'Failed to call delegatecall');
-  }
-
-  fallback() external payable {
-    _delegate(implemenation());
-  }
-  receive() external payable {}
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -5,45 +5,16 @@ const { boolean, any } = require("hardhat/internal/core/params/argumentTypes");
 
 describe('Redo', function () {
    async function deployRedoFxiture() {
-    const [admin, nonadmin, ...others] = await ethers.getSigners()
-    const LogicV1 = await ethers.getContractFactory('LogicV1')
-    const logicV1 = await LogicV1.deploy()
-    const LogicV2 = await ethers.getContractFactory('LogicV2')
-    const logicV2 = await LogicV2.deploy()
-    const Proxy = await ethers.getContractFactory('Redo')
-    const proxy = await Proxy.deploy(logicV1.getAddress())
-    return {proxy, admin, logicV1, logicV2}
+    const Redo = await ethers.getContractFactory('Redo')
+    const redo = await Redo.deploy()
+    return {redo}
    }
 
-   describe('logicV1 & logicV1', function () {
-    it('logicV1', async function () {
-        const {proxy, admin} = await loadFixture(deployRedoFxiture)
-        const abi = ['function setValue(uint256 _value) external']
-        const iface = new ethers.Interface(abi)
-        const value = 100
-        const cdata = iface.encodeFunctionData('setValue(uint256)', [value])
-        const tx = {
-            to: await proxy.getAddress(),
-            data: cdata
-        }
-        await admin.sendTransaction(tx)
-        const val = await proxy.value()
-        expect(val).to.be.equal(value)
-    })
-    it('logicV2', async function () {
-        const {proxy, admin, logicV2} = await loadFixture(deployRedoFxiture)
-        await proxy.upgradeTo(logicV2.getAddress())
-        const abi = ['function setValue(uint256 _value) external']
-        const iface = new ethers.Interface(abi)
-        const value = 100
-        const cdata = iface.encodeFunctionData('setValue(uint256)', [value])
-        const tx = {
-            to: await proxy.getAddress(),
-            data: cdata
-        }
-        await admin.sendTransaction(tx)
-        const val = await proxy.value()
-        expect(val).to.be.equal(value * 2)
+   describe('sumArray', function () {
+    it('sumArray', async function () {
+        const {redo} = await loadFixture(deployRedoFxiture)
+        const res = await redo.sumArray([1, 2, 3, 4])
+        expect(res).to.be.equal(10)
     })
    })
 

92f41cf8ceea5053a9b2c22d80f71f815a0df02e
Janessa <lvlajzhao1983@gmail.com>
2025-12-18 17:37:26 +0800

    redo - solidity-sum by assembly

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -4,54 +4,16 @@ pragma solidity ^0.8.20;
 import "hardhat/console.sol";
 
 contract Redo {
-    uint256 public value;
-    bytes32 private constant IMPLEMENATION_SLOT = keccak256('IMPLEMENATION_SLOT');
-    bytes32 private constant ADMIN_SLOT = keccak256('ADMIN_SLOT');
-
-    constructor(address _implementation) {
-        bytes32 slot = IMPLEMENATION_SLOT;
+    function sumArray(uint256[] memory arr) public pure returns(uint256) {
+        uint256 sum;
         assembly {
-            sstore(slot, _implementation)
+            let len := mload(arr)
+            let ptr := add(arr, 0x20)
+            for {let i := 0} lt(i, len) { i :=add(i, 1)}{
+                sum := add(mload(ptr), sum)
+                ptr := add(ptr, 0x20)
+            }
         }
-        slot = ADMIN_SLOT;
-        assembly {
-            sstore(slot, caller())
-        }
-
-    }
-
-    function admin() public view returns(address adm) {
-        bytes32 slot = ADMIN_SLOT;
-        assembly {
-            adm := sload(slot)
-        }
-    }
-
-    function implementation() public view returns(address imp) {
-        bytes32 slot = IMPLEMENATION_SLOT;
-        assembly {
-            imp := sload(slot)
-        }
-    }
-
-    function upgradeTo(address _implementation) public {
-        require(msg.sender == admin(), 'not admin');
-        bytes32 slot = IMPLEMENATION_SLOT;
-        assembly {
-            sstore(slot, _implementation)
-        }
-    }
-
-    function _delegate(address _implementation) private {
-        (bool success, ) = _implementation.delegatecall(msg.data);
-        require(success, 'failed to call delegatecall');
-    }
-
-    fallback() external payable {
-        _delegate(implementation());
-    }
-
-    receive() external payable {
-
+        return sum;
     }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,45 +6,16 @@ const { extendEnvironment, extendProvider } = require("hardhat/config");
 
 describe('Redo', function () {
     async function deployFixture() {
-        const [admin, ...others] = await ethers.getSigners()
-        const LogicV1 = await ethers.getContractFactory('LogicV1')
-        const logicV1 = await LogicV1.deploy()
-        const LogicV2 = await ethers.getContractFactory('LogicV2')
-        const logicV2 = await LogicV2.deploy()
-        const Proxy = await ethers.getContractFactory('Redo')
-        const proxy = await Proxy.deploy(logicV1.getAddress())
-        return {proxy, logicV1, logicV2,admin}
+        const Redo = await ethers.getContractFactory('Redo')
+        const redo = await Redo.deploy()
+        return {redo}
 
     }
-    describe('logicV1 & logicV2', function () {
-        it('logicV1', async function () {
-           const {proxy, logicV1, admin} = await loadFixture(deployFixture)
-           const abi = ['function setValue(uint256 _value) external']
-           const iface = new ethers.Interface(abi)
-           const value = 100
-           const cdata = iface.encodeFunctionData('setValue(uint256)', [value])
-           const tx = {
-            to: proxy.getAddress(),
-            data: cdata
-           }
-           await admin.sendTransaction(tx)
-           const val = await proxy.value()
-           expect(val).to.be.equal(value)
-        })
-        it('logicV2', async function () {
-            const {proxy, logicV2, admin} = await loadFixture(deployFixture)
-            await proxy.upgradeTo(logicV2.getAddress())
-            const abi = ['function setValue(uint256 _value) external']
-            const iface = new ethers.Interface(abi)
-            const value = 100
-            const cdata = iface.encodeFunctionData('setValue(uint256)', [value])
-            const tx = {
-             to: proxy.getAddress(),
-             data: cdata
-            }
-            await admin.sendTransaction(tx)
-            const val = await proxy.value()
-            expect(val).to.be.equal(value * 2)
+    describe('sumArray', function () {
+        it('sumArray', async function () {
+           const {redo} = await loadFixture(deployFixture)
+           const res = await redo.sumArray([1, 2, 3])
+           expect(res).to.be.equal(6)
         })
     })
 })

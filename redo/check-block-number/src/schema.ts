16cbf0a2a5cf20868835c16621525595ad6d7ec0
Janessa <lvlajzhao1983@gmail.com>
2025-12-21 12:33:08 +0800

    redo: check block number

--- redo/check-block-number/src/schema.ts
+++ redo/check-block-number/src/schema.ts
@@ -4,7 +4,7 @@ import * as yup from 'yup'
 export const txSchema = {
     getTxDetails: yup.object({
         params: yup.object({
-            hash: yup.string().required('hash is required').matches(/^0x[0-9a-zA-F]{64}$/, 'hash is invalid')
+            hash: yup.string().required('hash is required').matches(/^0x[0-9a-fA-F]{64}$/, 'hash is invalid')
         })
     })
 }

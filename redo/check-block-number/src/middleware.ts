b1b296a68496ce9061247872bce6cc0cf0a32b21
Janessa <lvlajzhao1983@gmail.com>
2025-12-21 10:13:51 +0800

    redo-clean: check block number

--- redo/check-block-number/src/middleware.ts
+++ redo/check-block-number/src/middleware.ts
@@ -1,12 +1,1 @@
-import * as yup from "yup"
-import {Request, Response, NextFunction} from 'express'
-export const validate = (schema: yup.AnySchema) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        try {
-            schema.validateSync({body: req.body, params: req.params, query: req.query}, {abortEarly: true, stripUnknown: true})
-            next()
-        } catch(error) {
-            next(error)
-        }
-    }
-}
+
--- redo/check-block-number/src/schema.ts
+++ redo/check-block-number/src/schema.ts
@@ -1,10 +1,1 @@
 
-import * as yup from "yup"
-
-export const txSchema = {
-    getDetails: yup.object({
-        params: yup.object({
-            hash: yup.string().required('hash is required').matches(/^0x[0-9a-fA-F]{64}$/, 'hash is invalid')
-        })
-    })
-}

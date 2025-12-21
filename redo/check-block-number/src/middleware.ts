2ce5f33c10905794a6acf73af9954758ef1f2e50
Janessa <lvlajzhao1983@gmail.com>
2025-12-21 12:29:03 +0800

    redo: check block number

--- redo/check-block-number/src/middleware.ts
+++ redo/check-block-number/src/middleware.ts
@@ -1,12 +1,13 @@
-import { NextFunction, Request, Response } from 'express'
-import * as yup from 'yup'
+import { AnySchema } from "yup";
+import {Request, Response, NextFunction} from 'express'
 
-export const validate = (schema: yup.AnySchema) => {
-    return (req:Request, res: Response, next: NextFunction) => {
+
+export const validate = (schema: AnySchema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
         try {
             schema.validateSync({body: req.body, params: req.params, query: req.query}, {abortEarly: true, stripUnknown: true})
             next()
-        } catch(error) {
+        } catch (error) {
             next(error)
         }
     }

47738caa705bd88febbd91719dee19cf38b53541
Janessa <lvlajzhao1983@gmail.com>
2025-12-29 15:49:09 +0800

    redo-backend: erc20-records

--- redo/erc20-records/src/app/page.tsx
+++ redo/erc20-records/src/app/page.tsx
@@ -2,53 +2,56 @@
 
 import useERC20Hook, { TransferLogType } from "@/hooks/useERC20Hook"
 import { useEffect, useState } from "react"
+import { isNullOrUndefined } from "util"
 
 const tokenAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 const walletAddress = '0x3A9b127e87295D90Ac0136eBa0aaA39DB185d341'
 
 export default function Home() {
-  const [balance, setBalance] = useState('')
-  const [transfers, setTransfers] = useState<TransferLogType[]>([])
-  const [isLoading, setIsLoading] = useState(false)
+
   const {getBalance, getRecentTransfers} = useERC20Hook(tokenAddress, walletAddress)
 
-  const format = (address: string) => `${address.slice(0, 10)}...${address.slice(-5)}`
+  const [balance, setBlance] = useState('')
+  const [transfers, setTransfers] = useState<TransferLogType[]>([])
+  const [loading, setLoading] = useState(false)
+
+  const format = (address: string) => `${address.slice(0,5)}...${address.slice(-5)}`
 
   useEffect(() => {
     (async () => {
-      setIsLoading(true)
+      setLoading(true)
       const balance = await getBalance()
-      const transfers = await getRecentTransfers()
-      setBalance(balance)
-      setTransfers(transfers)
-      setIsLoading(false)
+      const recentTransfer = await getRecentTransfers()
+      setBlance(balance)
+      setTransfers(recentTransfer)
+      setLoading(false)
     })()
   }, [])
-
   return (
     <div>
       {
-        isLoading 
-        ? <div>Loading...</div>
+        loading 
+        ? <div>Loading</div>
         : 
         <>
-        <div>Balance: {balance}</div>
-        <div className="grid grid-cols-6 gap-3">
-          <div>from</div> <div>to</div> <div>value</div> <div>blockNumber</div> <div>txHash</div> <div>logIndex</div>
-          {transfers.map((transfer) => (
-            <>
-              <div>{format(transfer.from)}</div>
-              <div>{format(transfer.to)}</div>
-              <div>{transfer.value}</div>
-              <div>{transfer.blockNumber}</div>
-              <div>{format(transfer.txHash)}</div>
-              <div>{transfer.logIndex}</div>
-            </>
-          ))}
-        </div>
+          <div>Balance: {balance}</div>
+          <div className="grid grid-cols-6 gap-5">
+            <div>from</div> <div>to</div>  <div>value</div>  <div>blockNumber</div>  <div>transactionHash</div>  <div>logIndex</div>
+            {
+              transfers.map((transfer) => (
+                <>
+                <div>{format(transfer.from)}</div>
+                <div>{format(transfer.to)}</div>
+                <div>{transfer.value}</div>
+                <div>{transfer.blockNumber}</div>
+                <div>{format(transfer.transactionHash)}</div>
+                <div>{transfer.logIndex}</div>
+                </>
+              ))
+            }
+          </div>
         </>
       }
-      
     </div>
   )
 }
--- redo/erc20-records/src/hooks/useERC20Hook.ts
+++ redo/erc20-records/src/hooks/useERC20Hook.ts
@@ -1,18 +1,18 @@
 import { erc20abi } from "@/config/abi"
 import { ethers } from "ethers"
+import { off } from "process"
 
-export type TransferLogType = {
-    from: string;
-    to: string;
-    value: string;
-    blockNumber: number;
-    txHash: string;
-    logIndex: number;
+export type TransferLogType  = {
+    from: string,
+    to: string,
+    value: string,
+    blockNumber: number,
+    transactionHash: string,
+    logIndex: number
 }
 
 const useERC20Hook = (tokenAddress: `0x${string}`, walletAddress: `0x${string}`) => {
     const provider = new ethers.JsonRpcProvider('https://eth.llamarpc.com')
-    //const provider = new ethers.JsonRpcProvider('https://eth.rpc.blxrbdn.com')
     const contract = new ethers.Contract(tokenAddress, erc20abi, provider)
 
     const getBalance = async () => {
@@ -21,12 +21,12 @@ const useERC20Hook = (tokenAddress: `0x${string}`, walletAddress: `0x${string}`)
             const decimals = await contract.decimals()
             return ethers.formatUnits(balance, decimals)
         } catch(error) {
-            console.log('failed to get balance', error)
+            console.log('failed to get balance')
             return ''
         }
     }
 
-    const getRecentTransfers  = async (offset: number = 2) => {
+    const getRecentTransfers = async (offset: number = 2) => {
         try {
             const curBlock = await provider.getBlockNumber()
             const fromBlock = curBlock - offset
@@ -35,19 +35,19 @@ const useERC20Hook = (tokenAddress: `0x${string}`, walletAddress: `0x${string}`)
                 fromBlock, curBlock
             )
             const parsedLogs = rawLogs.map((log) => {
-                if (!('args' in log) || !(log?.args)) return null
+                if (!('args' in log) || (!(log?.args))) return null
                 const [from, to, value] = log.args
                 return {
                     from: from,
                     to: to,
                     value: value.toString(),
                     blockNumber: log.blockNumber,
-                    txHash: log.blockHash,
+                    transactionHash: log.transactionHash,
                     logIndex: log.index
                 } as TransferLogType
-            }).filter((t): t is TransferLogType => t !== null)
-            return parsedLogs.slice(-10)
-        } catch (error) {
+            }).filter((t): t is  TransferLogType => t !== null).slice(-10)
+            return parsedLogs
+        } catch(error) {
             console.log('failed to get recent transfers', error)
             return []
         }

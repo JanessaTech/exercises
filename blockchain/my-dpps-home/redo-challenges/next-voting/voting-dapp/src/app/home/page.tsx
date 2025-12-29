398f8586b6db75050c34af1bf5f2676e983e2ffb
Janessa <lvlajzhao1983@gmail.com>
2025-12-29 22:22:27 +0800

    redo-voting

--- blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/home/page.tsx
+++ blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/home/page.tsx
@@ -1,65 +1,10 @@
+
 'use client'
 
-import { IWeb3Context, useWeb3Context } from "@/components/providers/Web3ContextProvider";
-import { authState } from "@/lib/Atoms";
-import { ethers } from "ethers";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react"
-import { useRecoilState } from "recoil";
-
-type Candidate  = {
-    id: number;
-    name: string;
-    votedBy: string;
-}
-
-type HomeState = {
-    isEnded: boolean;
-    candidates: Candidate[];
-}
-const defaultHomeState: HomeState = {
-    isEnded: false,
-    candidates: []
-}
-const Home: React.FC<{}> = () => {
-    const [state, setState] = useState(defaultHomeState)
-    const [auth, setAuth] = useRecoilState(authState)
-    const {disconnectWallet, connectWallet, state:{contract, address}} = useWeb3Context() as IWeb3Context
-    const router = useRouter()
-
-    useEffect(() => {
-        (async () => {
-            if (auth.connected) {
-                if (contract) {
-                    await updateState(contract)
-                } else {
-                    await connectWallet()
-                }
-            } else {
-                router.push('/')
-            }
-        })()
-    }, [contract])
-
-    const updateState = async (contract: ethers.Contract) => {
-        const raw: any[] = await contract.getCandidates()
-        const rows:Candidate[] = []
-        raw.forEach((e) => {
-            rows.push({id: Number(e[0]), name: e[1], votedBy: e[2]})
-        })
-        const isEnded = await contract.isEnded()
-        setState({isEnded: isEnded, candidates: rows})
-    }
-
-    console.log(state)
-
+const Home:React.FC<{}> = () => {
     return (
-        <div>
-            <div>Address:{address}</div>
-        </div>
+        <div></div>
     )
-
-    
 }
 
 export default Home
--- blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/page.tsx
+++ blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/page.tsx
@@ -1,26 +1,15 @@
 'use client'
 
-import { IWeb3Context, useWeb3Context } from "@/components/providers/Web3ContextProvider"
-import { authState } from "@/lib/Atoms"
-import { useRouter } from "next/navigation"
+
 import React from "react"
-import { useRecoilState } from "recoil"
+
 
 type LoginProps = {}
 const Login: React.FC<LoginProps> = () => {
-    const {connectWallet} = useWeb3Context() as IWeb3Context
-    const [auth, setAuth] = useRecoilState(authState)
-    const router = useRouter()
-
-    const onClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
-        await connectWallet()
-        setAuth({connected: true})
-        router.push('/home')
-    }
-
+    
     return (
         <div>
-            <button className="bg-zinc-600 rounded-3xl px-3 py-2" onClick={onClick}>Collect wallet </button>
+            <button className="bg-zinc-600 rounded-3xl px-3 py-2">Collect wallet </button>
         </div>
     )
 }

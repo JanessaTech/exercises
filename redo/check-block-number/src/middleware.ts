ffc707aea236d8a4cbc93a88ceef9d1e08c6780e
Janessa <lvlajzhao1983@gmail.com>
2025-12-29 14:59:33 +0800

    redo-backend: check-block-number

---
+++ redo/check-block-number/src/middleware.ts
@@ -0,0 +1,13 @@
+import { AnySchema } from "yup";
+import {Request, Response, NextFunction} from 'express'
+
+export const validate = (schema: AnySchema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            schema.validateSync({body: req.body, params: req.params, query: req.query}, {abortEarly: true, stripUnknown: true})
+            next()
+        } catch(error) {
+            next(error)
+        }
+    }
+}

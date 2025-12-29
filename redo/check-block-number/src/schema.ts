4bee7950d88ba818f0c72dd277e162a4c427fc02
Janessa <lvlajzhao1983@gmail.com>
2025-12-29 14:40:58 +0800

    redo-backend: clean check-block-number

--- redo/check-block-number/src/schema.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup'
-
-export const txSchema = {
-    getTxDetails: yup.object({
-        params: yup.object({
-            hash: yup.string().required('hash is required').matches(/^0x[0-9a-fA_F]{64}$/, 'invalid hash')
-        })
-    })
-}

9f4446d34c2db0cafdf6145b718426debce5d822
Janessa <lvlajzhao1983@gmail.com>
2026-01-08 09:40:58 +0800

    redo solidity - array insert

--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,16 +6,40 @@ const { extendEnvironment, extendProvider } = require("hardhat/config");
 
 describe('Redo', function () {
     async function deployFixture() {
-       const [admin, ...others] = await ethers.getSigners()
        const Redo = await ethers.getContractFactory('Redo')
        const redo = await Redo.deploy()
        return {redo}
     }
-    describe('sumArray', function () {
-        it('sumArray', async function () {
+    describe('create', function () {
+        it('create', async function () {
            const {redo} = await loadFixture(deployFixture)
-           const res = await redo.sumArray([1, 2, 3])
-           expect(res).to.be.equal(6)
+           await redo.create('person0')
+           await redo.create('person1')
+           await redo.create('person2')
+
+           const person0 = await redo.get(0)
+           const person1 = await redo.get(1)
+           const person2 = await redo.get(2)
+
+           expect(person0.name).to.be.equal('person0')
+           expect(person1.name).to.be.equal('person1')
+           expect(person2.name).to.be.equal('person2')
+        })
+    })
+
+    describe('remove', function () {
+        it('remove', async function () {
+            const {redo} = await loadFixture(deployFixture)
+            await redo.create('person0')
+            await redo.create('person1')
+            await redo.create('person2')
+            await redo.remove(1)
+
+            const person0 = await redo.get(0)
+            const person2 = await redo.get(2)
+            await expect(redo.get(1)).to.be.revertedWith('invalid id')
+            expect(person0.name).to.be.equal('person0')
+            expect(person2.name).to.be.equal('person2')
         })
     })
 })

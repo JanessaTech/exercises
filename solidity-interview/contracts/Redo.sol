d6ecc6a138a958aa9ec9a6ceae36d8c564109b46
Janessa <lvlajzhao1983@gmail.com>
2026-01-08 09:13:29 +0800

    redo solidity - sumArray

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -5,32 +5,16 @@ import "hardhat/console.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract Redo {
-    struct Person {
-        uint256 id;
-        string name;
-    }
-    Person[] people;
-    uint256 idx;
-    mapping(uint256 => uint256) idxMapping;
-    mapping(uint256 => bool) inserted;
-
-    function create(string memory _name) public {
-        uint256 _idx = idx++;
-        people.push(Person({id: _idx, name: _name}));
-        idxMapping[_idx] = people.length - 1;
-        inserted[_idx] = true;
-    }
-
-    function remove(uint256 _id) public {
-        require(inserted[_id], 'invalid id');
-        uint256 _idx = idxMapping[_id];
-        Person storage last = people[people.length - 1];
-        if (last.id != _id) {
-            people[_idx] = last;
-            idxMapping[last.id] = _idx;
+    function sumArray(uint[] memory arr) public pure returns (uint) {
+        uint sum;
+        assembly {
+            let len := mload(arr)
+            let ptr := add(arr, 0x20)
+            for { let i := 0} lt(i, len) { i := add(i, 1)} {
+                sum := add(mload(ptr), sum)
+                ptr := add(ptr, 0x20)
+            }
         }
-        people.pop();
-        delete idxMapping[_id];
-        delete inserted[_id];
+        return sum;
     }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,25 +6,17 @@ const { extendEnvironment, extendProvider } = require("hardhat/config");
 
 describe('Redo', function () {
     async function deployFixture() {
-        const [admin, bob, user1, user2, ...others] = await ethers.getSigners()
-        const MyERC721 = await ethers.getContractFactory('MyERC721')
-        const erc721 = await MyERC721.deploy('MyERC721', 'MyERC721')
-        const nftId = 1
-        await erc721.mint(bob.getAddress(), nftId)
-        const Aution = await ethers.getContractFactory('Redo')
-        const auction = await Aution.deploy(erc721.getAddress(), nftId)
-        await erc721.connect(bob).approve(auction.getAddress(), nftId)
-        return {auction, bob, erc721, user1, user2, nftId}
+       const [admin, ...others] = await ethers.getSigners()
+       const Redo = await ethers.getContractFactory('Redo')
+       const redo = await Redo.deploy()
+       return {redo}
     }
-    describe('init', function () {
-        it('init', async function () {
-           const {auction, erc721, nftId, bob} = await loadFixture(deployFixture)
-           const owner = await erc721.ownerOf(nftId)
-           const spender = await erc721.getApproved(nftId)
-           expect(owner).to.be.equal(await bob.getAddress())
-           expect(spender).to.be.equal(await auction.getAddress())
+    describe('sumArray', function () {
+        it('sumArray', async function () {
+           const {redo} = await loadFixture(deployFixture)
+           const res = await redo.sumArray([1, 2, 3])
+           expect(res).to.be.equal(6)
         })
-        
     })
 })
 

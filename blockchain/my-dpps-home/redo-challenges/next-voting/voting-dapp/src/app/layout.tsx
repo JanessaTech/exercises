00890533a341402e9f29969b51e80ad627cbfbde
Janessa <lvlajzhao1983@gmail.com>
2026-01-13 09:49:51 +0800

    redo : redo voting

--- blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/layout.tsx
+++ blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import RecoilProvider from "@/components/providers/RecoilProvider";
+import Web3ContextProvider from "@/components/providers/Web3ContextProvider";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -27,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
+        <RecoilProvider>
+          <Web3ContextProvider>
+            {children}
+          </Web3ContextProvider>
+          
+        </RecoilProvider>
       </body>
     </html>
   );
--- blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/page.tsx
+++ blockchain/my-dpps-home/redo-challenges/next-voting/voting-dapp/src/app/page.tsx
@@ -1,13 +1,22 @@
 'use client'
 
+import { IWeb3Context, useWeb3Context } from "@/components/providers/Web3ContextProvider"
+import { useRouter } from "next/navigation"
 import React from "react"
 
 type LoginProps = {}
 const Login: React.FC<LoginProps> = () => {
-    
+    const router = useRouter()
+    const {connect} = useWeb3Context() as IWeb3Context
+
+    const onClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        await connect()
+        router.push('/home')
+    }
+
     return (
         <div>
-            <button className="bg-zinc-600 rounded-3xl px-3 py-2">Collect wallet </button>
+            <button className="bg-zinc-600 rounded-3xl px-3 py-2" onClick={onClick}>Collect wallet </button>
         </div>
     )
 }

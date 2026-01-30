985375ebeb43b2d3e6cc867f2197b86e61b3397a
Janessa <lvlajzhao1983@gmail.com>
2026-01-30 20:48:24 +0800

    solidity redo: array ops

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -11,33 +11,33 @@ contract Redo {
     }
     uint idx;
     Person[] people;
-    mapping(uint=> uint) idxMapping;
+    mapping(uint => uint) idxMappping;
     mapping(uint => bool) inserted;
 
     function create(string memory _name) public {
         uint _idx = idx;
         idx++;
         people.push(Person({id: _idx, name: _name}));
-        idxMapping[_idx] = people.length - 1;
+        idxMappping[_idx] = people.length - 1;
         inserted[_idx] = true;
     }
 
     function remove(uint _id) public {
         require(inserted[_id], 'invalid id');
-        uint _idx = idxMapping[_id];
+        uint _idx = idxMappping[_id];
         Person storage last = people[people.length - 1];
         if (last.id != _id) {
             people[_idx] = last;
-            idxMapping[last.id] = _idx;
+            idxMappping[last.id] = _idx;
         }
         people.pop();
-        delete idxMapping[_id];
+        delete idxMappping[_id];
         delete inserted[_id];
-
     }
+
     function get(uint _id) public view returns(uint id, string memory name) {
         require(inserted[_id], 'invalid id');
-        Person storage person = people[idxMapping[_id]];
+        Person storage person = people[idxMappping[_id]];
         return (person.id, person.name);
     }
 }

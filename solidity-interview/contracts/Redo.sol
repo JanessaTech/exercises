c33bbb4f8c1185adc923c6f5d618b389122fdbd5
Janessa <lvlajzhao1983@gmail.com>
2026-01-30 10:23:45 +0800

    solidity redo: array ops

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -6,25 +6,25 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract Redo {
     struct Person {
-        uint256 id;
+        uint id;
         string name;
     }
+    uint idx;
     Person[] people;
-    uint256 idx;
-    mapping(uint256 => uint256) idxMapping;
-    mapping(uint256 => bool) inserted;
+    mapping(uint => uint) idxMapping;
+    mapping(uint => bool) inserted;
 
     function create(string memory _name) public {
-        uint256 _idx = idx;
+        uint _idx = idx;
         idx++;
         people.push(Person({id: _idx, name: _name}));
         idxMapping[_idx] = people.length - 1;
         inserted[_idx] = true;
     }
 
-    function remove(uint256 _id) public {
+    function remove(uint _id) public {
         require(inserted[_id], 'invalid id');
-        uint256 _idx = idxMapping[_id];
+        uint _idx = idxMapping[_id];
         Person storage last = people[people.length - 1];
         if (last.id != _id) {
             people[_idx] = last;
@@ -33,9 +33,9 @@ contract Redo {
         people.pop();
         delete idxMapping[_id];
         delete inserted[_id];
-    }
 
-    function get(uint256 _id) public view returns(uint256 id, string memory name) {
+    }
+    function get(uint _id) public view returns(uint id, string memory name) {
         require(inserted[_id], 'invalid id');
         Person storage person = people[idxMapping[_id]];
         return (person.id, person.name);

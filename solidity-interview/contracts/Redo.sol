ae5a8c15aa812932999caad4f0d27f938f131f16
Janessa <lvlajzhao1983@gmail.com>
2026-01-31 14:40:22 +0800

    solidity redo: erc1155

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -2,42 +2,34 @@
 pragma solidity ^0.8.20;
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Redo {
-    struct Person {
-        uint id;
-        string name;
-    }
-    uint idx;
-    Person[] people;
-    mapping(uint => uint) idxMapping;
-    mapping(uint => bool) inserted;
+contract Redo is ERC1155, Ownable {
+    uint256 constant public SWORD = 1;
+    uint256 constant public POTION = 2;
+    uint256 constant public SHEILD = 3;
 
-    function create(string memory _name) public {
-        uint _idx = idx;
-        idx++;
-        people.push(Person({id: _idx, name: _name}));
-        idxMapping[_idx] = people.length - 1;
-        inserted[_idx] = true;
+    constructor(address initialOwner) ERC1155("http://test.com/{id}.json") Ownable(initialOwner) {
+        _mint(msg.sender, SWORD, 1000, '');
+        _mint(msg.sender, POTION, 2000, '');
+        _mint(msg.sender, SHEILD, 3000, '');
     }
 
-    function remove(uint _id) public {
-        require(inserted[_id], 'invalid id');
-        uint _idx = idxMapping[_id];
-        Person storage last = people[people.length - 1];
-        if (last.id != _id) {
-            people[_idx] = last;
-            idxMapping[last.id] = _idx;
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public onlyOwner {
+        require(ids.length == values.length, 'ids.length != values.length');
+        _mintBatch(to, ids, values, data);
+    }
+
+    function transferBatch(address[] memory recipients, uint256 id, uint256 amount) public {
+        require(balanceOf(msg.sender, id) >= recipients.length * amount);
+        for (uint i = 0; i < recipients.length; i++) {
+            _safeTransferFrom(msg.sender, recipients[i], id, amount, '');
         }
-        people.pop();
-        delete idxMapping[_id];
-        delete inserted[_id];
     }
 
-    function get(uint _id) public view returns(uint id, string memory name) {
-        require(inserted[_id], 'invalid id');
-        Person storage person = people[idxMapping[_id]];
-        return (person.id, person.name);
-    }
 }

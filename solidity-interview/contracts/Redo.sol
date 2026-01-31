addbeb465556eea01bc580101afa2b2282808c5f
Janessa <lvlajzhao1983@gmail.com>
2026-01-31 17:44:14 +0800

    solidity redo: array ops

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -5,31 +5,40 @@ import "hardhat/console.sol";
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Redo is ERC1155, Ownable {
-    uint256 constant public SWORD = 1;
-    uint256 constant public POTION = 2;
-    uint256 constant public SHEILD = 3;
+contract Redo {
+    struct Person{
+        uint id;
+        string name;
+    }
+    uint idx;
+    Person[] people;
+    mapping(uint => uint) idxMapping;
+    mapping(uint => bool) inserted;
 
-    constructor(address initialOwner) ERC1155("http://test.com/{id}.json") Ownable(initialOwner) {
-        _mint(msg.sender, SWORD, 1000, '');
-        _mint(msg.sender, POTION, 2000, '');
-        _mint(msg.sender, SHEILD, 3000, '');
+    function create(string memory _name) public {
+        uint _idx = idx;
+        idx++;
+        people.push(Person({id: _idx, name: _name}));
+        idxMapping[_idx] = people.length - 1;
+        inserted[_idx] = true;
     }
 
-    function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
-    }
-
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory values, bytes memory data) public onlyOwner {
-        require(ids.length == values.length, 'ids.length != values.length');
-        _mintBatch(to, ids, values, data);
-    }
-
-    function transferBatch(address[] memory recipients, uint256 id, uint256 amount) public {
-        require(balanceOf(msg.sender, id) >= recipients.length * amount);
-        for (uint i = 0; i < recipients.length; i++) {
-            _safeTransferFrom(msg.sender, recipients[i], id, amount, '');
+    function remove(uint _id) public {
+        require(inserted[_id], 'invalid id');
+        uint _idx = idxMapping[_id];
+        Person storage last = people[people.length - 1];
+        if (last.id != _id) {
+            people[_idx] = last;
+            idxMapping[last.id] = _idx;
         }
+        people.pop();
+        delete idxMapping[_id];
+        delete inserted[_id];
     }
 
+    function get(uint _id) public view returns(uint id, string memory name) {
+        require(inserted[_id], 'invalid id');
+        Person storage person = people[idxMapping[_id]];
+        return (person.id, person.name);
+    }
 }
--- solidity-interview/test/Redo.js
+++ solidity-interview/test/Redo.js
@@ -6,48 +6,40 @@ const { extendEnvironment, extendProvider } = require("hardhat/config");
 
 describe('Redo', function () {
     async function deployRedoFixture() {
-        const [admin, bob, alice, ...others] = await ethers.getSigners()
         const Redo = await ethers.getContractFactory('Redo')
-        const redo = await Redo.deploy(admin.getAddress())
-        return {redo, admin, bob, alice}
+        const redo = await Redo.deploy()
+        return {redo}
     }
-    describe('init', function () {
-        it('init', async function () {
-           const {redo, admin} = await loadFixture(deployRedoFixture)
-           const balance1 = await redo.balanceOf(admin.getAddress(), 1)
-           const balance2 = await redo.balanceOf(admin.getAddress(), 2)
-           const balance3 = await redo.balanceOf(admin.getAddress(), 3)
-           expect(balance1).to.be.equal(1000)
-           expect(balance2).to.be.equal(2000)
-           expect(balance3).to.be.equal(3000)
+    describe('create/remove', function () {
+        it('create', async function () {
+           const {redo} = await loadFixture(deployRedoFixture)
+           await redo.create('person0')
+           await redo.create('person1')
+           await redo.create('person2')
+
+           const person0 = await redo.get(0)
+           const person1 = await redo.get(1)
+           const person2 = await redo.get(2)
+           expect(person0.name).to.be.equal('person0')
+           expect(person1.name).to.be.equal('person1')
+           expect(person2.name).to.be.equal('person2')
         })
-    })
+        it('remove', async function () {
+            const {redo} = await loadFixture(deployRedoFixture)
+            await redo.create('person0')
+            await redo.create('person1')
+            await redo.create('person2')
+            await redo.create('person3')
 
-    describe('mintBatch', function () {
-        it('mintBatch', async function () {
-            const {redo, bob} = await loadFixture(deployRedoFixture)
-            const ids = [2, 3]
-            const values = [200, 300]
-            await redo.mintBatch(bob.getAddress(), ids, values, '0x123456')
-            const balance2 = await redo.balanceOf(bob.getAddress(), 2)
-            const balance3 = await redo.balanceOf(bob.getAddress(), 3)
+            await redo.remove(3)
+            await redo.remove(1)
 
-            expect(balance2).to.be.equal(200)
-            expect(balance3).to.be.equal(300)
-
-        })
-    })
-
-    describe('transferBatch', function () {
-        it('transferBatch', async function () {
-            const {redo, bob, alice} = await loadFixture(deployRedoFixture)
-            const recipients = [bob.getAddress(), alice.getAddress()]
-            const id = 1, amount = 300
-            await redo.transferBatch(recipients, id, amount)
-            const balanceBob = await redo.balanceOf(bob.getAddress(), 1)
-            const balanceAlice = await redo.balanceOf(alice.getAddress(), 1)
-            expect(balanceBob).to.be.equal(300)
-            expect(balanceAlice).to.be.equal(300)
+            const person0 = await redo.get(0)
+            const person2 = await redo.get(2)
+            expect(person0.name).to.be.equal('person0')
+            expect(person2.name).to.be.equal('person2')
+            await expect(redo.get(1)).to.be.revertedWith('invalid id')
+            await expect(redo.get(3)).to.be.revertedWith('invalid id')
         })
     })
 })

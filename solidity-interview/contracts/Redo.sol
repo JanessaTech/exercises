c7fb98f26d63c721627bdc6b47e0571471c7c5e9
Janessa <lvlajzhao1983@gmail.com>
2026-02-02 11:22:26 +0800

    solidity redo: array ops

--- solidity-interview/contracts/Redo.sol
+++ solidity-interview/contracts/Redo.sol
@@ -6,7 +6,7 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Redo {
-    struct Person{
+    struct Person {
         uint id;
         string name;
     }
@@ -16,16 +16,14 @@ contract Redo {
     mapping(uint => bool) inserted;
 
     function create(string memory _name) public {
-        uint _idx = idx;
-        idx++;
+        uint _idx = idx++;
         people.push(Person({id: _idx, name: _name}));
         idxMapping[_idx] = people.length - 1;
         inserted[_idx] = true;
     }
-
     function remove(uint _id) public {
         require(inserted[_id], 'invalid id');
-        uint _idx = idxMapping[_id];
+        uint _idx  = idxMapping[_id];
         Person storage last = people[people.length - 1];
         if (last.id != _id) {
             people[_idx] = last;
@@ -35,7 +33,6 @@ contract Redo {
         delete idxMapping[_id];
         delete inserted[_id];
     }
-
     function get(uint _id) public view returns(uint id, string memory name) {
         require(inserted[_id], 'invalid id');
         Person storage person = people[idxMapping[_id]];

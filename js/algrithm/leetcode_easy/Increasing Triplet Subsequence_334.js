9f2545376b254ef2016b948846b6e5d5367e9606
Janessa <lvlajzhao1983@gmail.com>
2026-02-13 11:08:29 +0800

    solution to 334

---
+++ js/algrithm/leetcode_easy/Increasing Triplet Subsequence_334.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let small = Infinity, big = Infinity
+    for (let num of nums) {
+        if (num <= small) {
+            small = num
+        } else if (num <= big) {
+            big  = num
+        } else {
+            return true
+        }
+    }
+    return false
+};

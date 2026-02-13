08aad9695d0c2ed8f90a7657d4db4a9f0fc9289a
Janessa <lvlajzhao1983@gmail.com>
2026-02-13 10:59:58 +0800

    solution to 605

---
+++ js/algrithm/leetcode_easy/Can Place Flowers_605.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let cnt = 0
+    for (let i = 0; i < flowerbed.length && cnt < n; i++) {
+        if (flowerbed[i] === 1) continue
+        const pre = i === 0 ? 0 : flowerbed[i - 1]
+        const next = i === flowerbed.length - 1 ? 0 : flowerbed[i + 1]
+        if (pre === 0 && next == 0) {
+            flowerbed[i] = 1
+            cnt++
+        }
+    }
+
+    return cnt === n
+};

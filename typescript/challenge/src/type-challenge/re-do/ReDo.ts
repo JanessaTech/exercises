b61ba94baae2b50599983f2a5a80962f8628b2df
Janessa <lvlajzhao1983@gmail.com>
2026-02-17 08:13:16 +0800

    TS redo - TupleToNestedObject

--- typescript/challenge/src/type-challenge/re-do/ReDo.ts
+++ typescript/challenge/src/type-challenge/re-do/ReDo.ts
@@ -1,22 +1,17 @@
 import { Alike, Equal, Expect, ExpectExtends, NotAny } from "../test-utils"
 
-const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
-const tupleNumber = [1, 2, 3, 4] as const
-const sym1 = Symbol(1)
-const sym2 = Symbol(2)
-const tupleSymbol = [sym1, sym2] as const
-const tupleMix = [1, '2', 3, '4', sym1] as const
 
 type cases = [
-  Expect<Equal<TupleToObject<typeof tuple>, { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y' }>>,
-  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1, 2: 2, 3: 3, 4: 4 }>>,
-  Expect<Equal<TupleToObject<typeof tupleSymbol>, { [sym1]: typeof sym1, [sym2]: typeof sym2 }>>,
-  Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1, '2': '2', 3: 3, '4': '4', [sym1]: typeof sym1 }>>,
+  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b', 'c'], boolean>, { a: { b: { c: boolean } } }>>,
+  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>,
 ]
-// @ts-expect-error
-type error = TupleToObject<[[1, 2], {}]>
 
+// type TupleToNestedObject<T extends unknown[], U> = T extends [infer F extends string, ... infer R]
+// ? {[P in F]: TupleToNestedObject<R, U>}
+// : U
 
-type TupleToObject<T extends readonly (string | number | symbol)[]> = {
-  [K in T[number]]: K
-}
+type TupleToNestedObject<T extends unknown[], U> = T extends [infer F extends string, ...infer R]
+? Record<F, TupleToNestedObject<R, U>>
+: U
